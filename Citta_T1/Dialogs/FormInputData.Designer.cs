3d3958e28feed96eebc61e0cc1080b3cc0a24d6d
卢琪 <kfc.skywalker@163.com>
2020-01-17 17:50:55 +0800

    修改新建模型窗体属性，添加模型关闭图标

--- Citta_T1/Dialogs/FormInputData.Designer.cs
+++ Citta_T1/Dialogs/FormInputData.Designer.cs
@@ -272,6 +272,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "FormInputData";
             this.ShowIcon = false;
             this.Text = "导入数据";
