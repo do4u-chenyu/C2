1c96134a3d752cf2dd058d116208e36ffd129c6a
卢琪 <kfc.skywalker@163.com>
2020-01-17 18:16:37 +0800

    修改图标文件

--- Citta_T1/Controls/ModelTitleControl.Designer.cs
+++ Citta_T1/Controls/ModelTitleControl.Designer.cs
@@ -28,8 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // ModelTitleControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "ModelTitleControl";
+            this.Size = new System.Drawing.Size(150, 36);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
