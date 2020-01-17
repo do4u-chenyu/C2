9bda0d2657076516ca0a036c1cf27a953a9aac27
x3933 <470058433@qq.com>
2020-01-17 14:37:47 +0800

    修改FormInputData初始化位置

---
+++ Citta_T1/DataGridView0.Designer.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+using Citta_T1;
+
+namespace Citta_T1
+{
+    partial class DataGridView0
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this._InitializeColumns();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(1011, 137);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // DataGridView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "DataGridView";
+            this.Size = new System.Drawing.Size(1011, 137);
+            this.Load += new System.EventHandler(this.DataGridView_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+        private void _InitializeColumns()
+        {
+            /*
+             * 初始化列
+             */
+            System.Windows.Forms.DataGridViewTextBoxColumn[] ColumnList = new System.Windows.Forms.DataGridViewTextBoxColumn[numOfRows];
+            for (int i = 0; i < this.numOfCols; i++)
+            {
+                ColumnList[i] = new System.Windows.Forms.DataGridViewTextBoxColumn();
+                ColumnList[i].HeaderText = "Col" + i.ToString();
+                ColumnList[i].Name = "列" + i.ToString();
+            }
+            this.dataGridView1.Columns.AddRange(ColumnList);
+        }
+
+        private void _InitializeRowsFromFile(string fileName)
+        {
+            /*
+             * 初始化行
+             * TODO
+             * 使用样例数据
+             */
+            for (int i = 0; i < this.numOfRows; i=this.dataGridView1.Rows.Add())
+            {   
+                //this.dataGridView1.Rows.Add();
+                for (int j = 0; j < this.numOfCols; j++)
+                {
+                    this.dataGridView1.Rows[i].Cells[j].Value = i.ToString() + "_" + j.ToString();
+                }
+            }            
+        }
+        #endregion
+
+        //private List<List<string>> OverViewFile(string fileName, int maxNumOfFile = 50, char sep='\t')
+        //{
+        //    System.IO.StreamReader file = new System.IO.StreamReader(fileName);
+        //    int rowCounter = 0;
+        //    List<List<string>> datas;
+        //    string line;
+        //    char[] seps = new char[sep];
+        //    while(((line = file.ReadLine()) != null) && (rowCounter < maxNumOfFile))
+        //    {
+        //        List<string> eles = new List<string>(line.Split(seps));
+        //        datas.Add(eles);
+        //    }
+        //    return datas;
+            
+        //}
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private int numOfCols = 20;
+        private int numOfRows = 20;
+    }
+}
---
+++ Citta_T1/DataGridView0.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Citta_T1
+{
+    public partial class DataGridView0 : UserControl
+    {
+        public DataGridView0()
+        {
+            InitializeComponent();
+        }
+
+        private void DataGridView_Load(object sender, EventArgs e)
+        {
+            //_InitializeRows();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
