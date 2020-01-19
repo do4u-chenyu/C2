b08a35f2def97011e521bfd06b86d2f66b7b4e6b
王龙楷 <x6421@njsecnet.com>
2020-01-19 10:23:16 +0800

    修改data/text.txt为相对路径

--- Citta_T1/DataGridView0.cs
+++ Citta_T1/DataGridView0.cs
@@ -12,7 +12,8 @@ namespace Citta_T1
 {
     public partial class DataGridView0 : UserControl
     {
-        private string overViewFilePath = "D:/work/Citta_T1/datas/text.txt";
+        //private string overViewFilePath = "D:/work/Citta_T1/datas/text.txt";
+        private string overViewFilePath = "../../datas/text.txt"; //相对于bin/Debug/Citta_T1.exe的上两层目录
         public DataGridView0()
         {
             InitializeComponent();
