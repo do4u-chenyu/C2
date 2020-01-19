13a5565469d38933b987eb010f9945a4c3438343
王龙楷 <x6421@njsecnet.com>
2020-01-19 09:03:06 +0800

    删除FormCreateModel.cs

--- Citta_T1/Dialogs/CreateNewModel.cs
+++ Citta_T1/Dialogs/CreateNewModel.cs
@@ -16,5 +16,10 @@ namespace Citta_T1.Dialogs
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
