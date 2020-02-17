6b49f0943e4fae4c555e7fdecf857fe49609a14e
Small White <1309744250@qq.com>
2020-02-17 16:03:04 +0800

    '美化重命名效果'

--- Citta_T1/Controls/MoveOpControl.Designer.cs
+++ Citta_T1/Controls/MoveOpControl.Designer.cs
@@ -54,9 +54,10 @@
             // rightPictureBox
             // 
             this.rightPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("rightPictureBox.Image")));
-            this.rightPictureBox.Location = new System.Drawing.Point(214, 19);
+            this.rightPictureBox.Location = new System.Drawing.Point(321, 28);
+            this.rightPictureBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rightPictureBox.Name = "rightPictureBox";
-            this.rightPictureBox.Size = new System.Drawing.Size(19, 21);
+            this.rightPictureBox.Size = new System.Drawing.Size(28, 32);
             this.rightPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.rightPictureBox.TabIndex = 1;
             this.rightPictureBox.TabStop = false;
@@ -67,9 +68,10 @@
             // leftPicture
             // 
             this.leftPicture.Image = ((System.Drawing.Image)(resources.GetObject("leftPicture.Image")));
-            this.leftPicture.Location = new System.Drawing.Point(34, 19);
+            this.leftPicture.Location = new System.Drawing.Point(51, 28);
+            this.leftPicture.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.leftPicture.Name = "leftPicture";
-            this.leftPicture.Size = new System.Drawing.Size(24, 30);
+            this.leftPicture.Size = new System.Drawing.Size(36, 45);
             this.leftPicture.TabIndex = 0;
             this.leftPicture.TabStop = false;
             this.leftPicture.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MoveOpControl_MouseDown);
@@ -79,9 +81,10 @@
             // leftPinPictureBox
             // 
             this.leftPinPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.leftPinPictureBox.Location = new System.Drawing.Point(19, 26);
+            this.leftPinPictureBox.Location = new System.Drawing.Point(28, 39);
+            this.leftPinPictureBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.leftPinPictureBox.Name = "leftPinPictureBox";
-            this.leftPinPictureBox.Size = new System.Drawing.Size(10, 10);
+            this.leftPinPictureBox.Size = new System.Drawing.Size(14, 14);
             this.leftPinPictureBox.TabIndex = 3;
             this.leftPinPictureBox.TabStop = false;
             this.leftPinPictureBox.MouseEnter += new System.EventHandler(this.PinOpPictureBox_MouseEnter);
@@ -90,9 +93,10 @@
             // rightPinPictureBox
             // 
             this.rightPinPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.rightPinPictureBox.Location = new System.Drawing.Point(243, 26);
+            this.rightPinPictureBox.Location = new System.Drawing.Point(364, 39);
+            this.rightPinPictureBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rightPinPictureBox.Name = "rightPinPictureBox";
-            this.rightPinPictureBox.Size = new System.Drawing.Size(10, 10);
+            this.rightPinPictureBox.Size = new System.Drawing.Size(14, 14);
             this.rightPinPictureBox.TabIndex = 4;
             this.rightPinPictureBox.TabStop = false;
             this.rightPinPictureBox.MouseEnter += new System.EventHandler(this.PinOpPictureBox_MouseEnter);
@@ -109,7 +113,7 @@
             this.异常日志ToolStripMenuItem,
             this.删除ToolStripMenuItem});
             this.contextMenuStrip.Name = "contextMenuStrip1";
-            this.contextMenuStrip.Size = new System.Drawing.Size(134, 148);
+            this.contextMenuStrip.Size = new System.Drawing.Size(163, 208);
             // 
             // 菜单1ToolStripMenuItem
             // 
@@ -119,7 +123,7 @@
             this.菜单1ToolStripMenuItem.Font = new System.Drawing.Font("微软雅黑", 10F, System.Drawing.FontStyle.Bold);
             this.菜单1ToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(155)))), ((int)(((byte)(213)))));
             this.菜单1ToolStripMenuItem.Name = "菜单1ToolStripMenuItem";
-            this.菜单1ToolStripMenuItem.Size = new System.Drawing.Size(133, 24);
+            this.菜单1ToolStripMenuItem.Size = new System.Drawing.Size(162, 34);
             this.菜单1ToolStripMenuItem.Text = "设置";
             // 
             // 菜单2ToolStripMenuItem
@@ -128,7 +132,7 @@
             this.菜单2ToolStripMenuItem.Font = new System.Drawing.Font("微软雅黑", 10F, System.Drawing.FontStyle.Bold);
             this.菜单2ToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(155)))), ((int)(((byte)(213)))));
             this.菜单2ToolStripMenuItem.Name = "菜单2ToolStripMenuItem";
-            this.菜单2ToolStripMenuItem.Size = new System.Drawing.Size(133, 24);
+            this.菜单2ToolStripMenuItem.Size = new System.Drawing.Size(162, 34);
             this.菜单2ToolStripMenuItem.Text = "重命名";
             this.菜单2ToolStripMenuItem.Click += new System.EventHandler(this.重命名ToolStripMenuItem_Click_1);
             // 
@@ -138,7 +142,7 @@
             this.备注ToolStripMenuItem.Font = new System.Drawing.Font("微软雅黑", 10F, System.Drawing.FontStyle.Bold);
             this.备注ToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(155)))), ((int)(((byte)(213)))));
             this.备注ToolStripMenuItem.Name = "备注ToolStripMenuItem";
-            this.备注ToolStripMenuItem.Size = new System.Drawing.Size(133, 24);
+            this.备注ToolStripMenuItem.Size = new System.Drawing.Size(162, 34);
             this.备注ToolStripMenuItem.Text = "备注";
             this.备注ToolStripMenuItem.TextDirection = System.Windows.Forms.ToolStripTextDirection.Horizontal;
             // 
@@ -148,7 +152,7 @@
             this.运行到此ToolStripMenuItem.Font = new System.Drawing.Font("微软雅黑", 10F, System.Drawing.FontStyle.Bold);
             this.运行到此ToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(155)))), ((int)(((byte)(213)))));
             this.运行到此ToolStripMenuItem.Name = "运行到此ToolStripMenuItem";
-            this.运行到此ToolStripMenuItem.Size = new System.Drawing.Size(133, 24);
+            this.运行到此ToolStripMenuItem.Size = new System.Drawing.Size(162, 34);
             this.运行到此ToolStripMenuItem.Text = "运行到此";
             this.运行到此ToolStripMenuItem.ToolTipText = "将执行模型到本阶段为止";
             // 
@@ -158,7 +162,7 @@
             this.异常日志ToolStripMenuItem.Font = new System.Drawing.Font("微软雅黑", 10F, System.Drawing.FontStyle.Bold);
             this.异常日志ToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(155)))), ((int)(((byte)(213)))));
             this.异常日志ToolStripMenuItem.Name = "异常日志ToolStripMenuItem";
-            this.异常日志ToolStripMenuItem.Size = new System.Drawing.Size(133, 24);
+            this.异常日志ToolStripMenuItem.Size = new System.Drawing.Size(162, 34);
             this.异常日志ToolStripMenuItem.Text = "异常日志";
             // 
             // 删除ToolStripMenuItem
@@ -167,18 +171,19 @@
             this.删除ToolStripMenuItem.Font = new System.Drawing.Font("微软雅黑", 10F, System.Drawing.FontStyle.Bold);
             this.删除ToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(155)))), ((int)(((byte)(213)))));
             this.删除ToolStripMenuItem.Name = "删除ToolStripMenuItem";
-            this.删除ToolStripMenuItem.Size = new System.Drawing.Size(133, 24);
+            this.删除ToolStripMenuItem.Size = new System.Drawing.Size(162, 34);
             this.删除ToolStripMenuItem.Text = "删除";
             this.删除ToolStripMenuItem.Click += new System.EventHandler(this.删除ToolStripMenuItem_Click);
             // 
             // textBox1
             // 
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox1.Font = new System.Drawing.Font("楷体", 9F);
-            this.textBox1.Location = new System.Drawing.Point(64, 22);
+            this.textBox1.Location = new System.Drawing.Point(96, 33);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(144, 14);
+            this.textBox1.Size = new System.Drawing.Size(216, 28);
             this.textBox1.TabIndex = 5;
             this.textBox1.Text = "连接算子";
             this.textBox1.Visible = false;
@@ -189,16 +194,17 @@
             this.textLabel.AutoSize = true;
             this.textLabel.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.textLabel.Font = new System.Drawing.Font("楷体", 9F);
-            this.textLabel.Location = new System.Drawing.Point(64, 22);
+            this.textLabel.Location = new System.Drawing.Point(96, 33);
+            this.textLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.textLabel.Name = "textLabel";
-            this.textLabel.Size = new System.Drawing.Size(53, 12);
+            this.textLabel.Size = new System.Drawing.Size(80, 18);
             this.textLabel.TabIndex = 6;
             this.textLabel.Text = "连接算子";
             // 
             // MoveOpControl
             // 
             this.AllowDrop = true;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
@@ -212,8 +218,9 @@
             this.Controls.Add(this.leftPicture);
             this.DoubleBuffered = true;
             this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "MoveOpControl";
-            this.Size = new System.Drawing.Size(274, 64);
+            this.Size = new System.Drawing.Size(411, 96);
             this.Load += new System.EventHandler(this.MoveOpControl_Load);
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MoveOpControl_MouseDown);
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MoveOpControl_MouseMove);
