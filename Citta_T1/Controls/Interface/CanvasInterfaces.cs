c4c04d1d06a6261eacccd2886cdfd604c83ed15f
Darkxiete <470058433@qq.com>
2020-03-10 15:59:17 +0800

    重构代码

---
+++ Citta_T1/Controls/Interface/CanvasInterfaces.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Citta_T1.Utils
+{
+
+    // 缩放接口
+    public interface IScalable
+    {
+        void SetTag(Control cons);
+        void SetControlsBySize(float fx, float fy, Control cons);
+    }
+
+    // 绘制接口
+    public interface IDrawObject
+    {
+        // 判断是否鼠标在当前目标中
+        bool PointInObject(Point p);
+    }
+}
