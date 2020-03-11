88d22209f98650bf7f6af9b52020e249bd10a032
刘晓凤 <lxfjiangnan@163.com>
2020-03-11 16:29:45 +0800

    写了个数据源的类

---
+++ Citta_T1/Business/DataSource.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Citta_T1.Business
+{
+    class DataSource
+    {
+        private string path;
+        private string UserInfoPath;
+        public DataSource()
+        {
+            this.path = Directory.GetCurrentDirectory().ToString() + "\\cittaModelDocument\\";
+            this.UserInfoPath = path + "\\UserInformation.xml";
+        }
+        public void WriteUserInfo(string userName)
+        {
+         
+        }
+    }
+}
