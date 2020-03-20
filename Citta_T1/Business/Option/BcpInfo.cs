de7954ca04dc817595676b868292ea382e2690f3
lu-qi123 <18705158378@163.com>
2020-03-20 16:50:36 +0800

    开始算子配置模块的编码

--- Citta_T1/Business/Option/BcpInfo.cs
+++ Citta_T1/Business/Option/BcpInfo.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Citta_T1.Business.Model;
 namespace Citta_T1.Business.Option
 {
     class BcpInfo
     {
-        private string fullBcpPath;
+        private string fullBcpPath;  //BCP文件全路径
+        private string fileName;     //BCP文件名
+        private string name;         //对应的数据源名称
+        private ElementType type;    //对应的类型:数据源 或 Result
         private List<ColumnInfo> columnInfos;
     }
 }
