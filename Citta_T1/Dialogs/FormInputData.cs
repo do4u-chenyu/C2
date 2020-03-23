e634d39e568376948e8e14e38341013dc4f57db0
lu-qi123 <18705158378@163.com>
2020-03-23 10:09:42 +0800

    清理FormInputData无用代码

--- Citta_T1/Dialogs/FormInputData.cs
+++ Citta_T1/Dialogs/FormInputData.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Drawing;
 using System.IO;
-using System.Security.Cryptography;
-using System.Text;
 using System.Windows.Forms;
 using Citta_T1.Utils;
 
@@ -17,8 +13,8 @@ namespace Citta_T1.Dialogs
         private bool m_isUTF8 = false;
         private string m_filePath;
         private int m_maxNumOfRow = 100;
-        private System.Drawing.Font bold_font = new System.Drawing.Font("微软雅黑", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-        private System.Drawing.Font font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+        private Font bold_font = new Font("微软雅黑", 12F, (FontStyle.Bold | FontStyle.Underline), GraphicsUnit.Point, 134);
+        private Font font = new Font("微软雅黑", 12F, FontStyle.Underline, GraphicsUnit.Point, 134);
         private bool textboxHasText = false;
 
         public FormInputData()
@@ -40,32 +36,16 @@ namespace Citta_T1.Dialogs
         
         private void textBox1_Leave(object sender, EventArgs e)
         {
+            textboxHasText = true;
+
             if (this.textBox1.Text == "")
             {
                 this.textBox1.Text = "请输入数据名称";
-                this.textBox1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+                this.textBox1.ForeColor = SystemColors.ActiveCaption;
                 textboxHasText = false;
             }
-            else
-            {
-                textboxHasText = true;
-            }
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -86,30 +66,6 @@ namespace Citta_T1.Dialogs
 
         }
 
-        private void label6_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label9_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label8_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label10_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label7_Click(object sender, EventArgs e)
-        {
-
-        }
 
         // 添加按钮
         public event delegateInputData InputDataEvent;
@@ -134,11 +90,6 @@ namespace Citta_T1.Dialogs
             }
         }
 
-        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
-        {
-
-        }
-
         private void button3_Click(object sender, EventArgs e)
         {
             // 关闭按钮
@@ -146,25 +97,12 @@ namespace Citta_T1.Dialogs
             Close();
         }
 
-        private void label4_Click(object sender, EventArgs e)
+        private void UTF8Lable_Click(object sender, EventArgs e)
         {
-            this.label4.Font = bold_font;
-            this.label5.Font = font;
-            this.m_isUTF8 = false;
-            PreViewFile();
-        }
-
-        private void label5_Click(object sender, EventArgs e)
-        {
-            this.label4.Font = font;
-            this.label5.Font = bold_font;
+            this.gbkLable.Font = font;
+            this.utf8Lable.Font = bold_font;
             this.m_isUTF8 = true;
             PreViewFile();
-        }
-
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-
         }
 
         private void PreViewFile()
@@ -187,13 +125,13 @@ namespace Citta_T1.Dialogs
             String header = sr.ReadLine();
             String[] headers = header.Split('\t');
             int numOfCol = header.Split('\t').Length;
-            System.Windows.Forms.DataGridViewTextBoxColumn[] ColumnList = new System.Windows.Forms.DataGridViewTextBoxColumn[numOfCol];
+            DataGridViewTextBoxColumn[] ColumnList = new DataGridViewTextBoxColumn[numOfCol];
             try
             {
                 // 初始化表头
                 for (int i = 0; i < numOfCol; i++)
                 {
-                    ColumnList[i] = new System.Windows.Forms.DataGridViewTextBoxColumn();
+                    ColumnList[i] = new DataGridViewTextBoxColumn();
                     ColumnList[i].HeaderText = headers[i];
                     ColumnList[i].Name = "Col " + i.ToString();
                 }
@@ -206,7 +144,7 @@ namespace Citta_T1.Dialogs
                     // 读取数据
                     String line = sr.ReadLine();
                     String[] eles = line.Split('\t');
-                    System.Windows.Forms.DataGridViewRow dr = new System.Windows.Forms.DataGridViewRow();
+                    DataGridViewRow dr = new DataGridViewRow();
                     this.dataGridView1.Rows.Add(dr);
                     for (int col = 0; col < numOfCol; col++)
                     {
@@ -228,6 +166,13 @@ namespace Citta_T1.Dialogs
             this.dataGridView1.Rows.Clear();
             this.dataGridView1.Columns.Clear();
         }
-  
+
+        private void GBKLable_Click(object sender, EventArgs e)
+        {
+            this.gbkLable.Font = bold_font;
+            this.utf8Lable.Font = font;
+            this.m_isUTF8 = false;
+            PreViewFile();
+        }
     }
 }
