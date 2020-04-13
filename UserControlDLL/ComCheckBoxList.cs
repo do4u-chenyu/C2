28779aabd806150ffc150d2319922e67a6bc4dc2
phoenix1214 <273048269@qq.com>
2020-04-13 10:45:30 +0800

    新增下拉复选框的模糊搜索功能

--- UserControlDLL/ComCheckBoxList.cs
+++ UserControlDLL/ComCheckBoxList.cs
@@ -19,11 +19,13 @@ namespace UserControlDLL
         private LabelS lbGrip;//此LABEL用于设置可以拖动下拉窗体变化
 
         private CheckedListBox checkListBox;
+        private CheckedListBox checkListBoxTmp;
         private Label lbSelectAll;//全选
         private Label lbSelectNo;//取消
 
         private Form frmCheckList;
 
+        private TextBox textBox1;
         private Panel pnlBack;
         private Panel pnlCheck;
 
@@ -37,7 +39,7 @@ namespace UserControlDLL
         {
             InitializeComponent();
             this.Name = "comBoxCheckBoxList";
-            this.Layout+=new LayoutEventHandler(ComCheckBoxList_Layout);
+            this.Layout += new LayoutEventHandler(ComCheckBoxList_Layout);
 
             //生成控件
             tbSelectedValue = new TextBox();
@@ -48,7 +50,7 @@ namespace UserControlDLL
             //下拉箭头
             this.btnSelect = new ButtonS();
             btnSelect.FlatStyle = FlatStyle.Flat;
-            btnSelect.Click+=new EventHandler(btnSelect_Click);
+            btnSelect.Click += new EventHandler(btnSelect_Click);
 
             //全选
             this.lbSelectAll = new Label();
@@ -56,9 +58,9 @@ namespace UserControlDLL
             lbSelectAll.Text = "全选";
             lbSelectAll.Size = new Size(40, 20);
             lbSelectAll.ForeColor = Color.Blue;
-            lbSelectAll.Cursor = Cursors.Hand ;
+            lbSelectAll.Cursor = Cursors.Hand;
             lbSelectAll.TextAlign = ContentAlignment.MiddleCenter;
-            lbSelectAll.Click+=new EventHandler(lbSelectAll_Click);
+            lbSelectAll.Click += new EventHandler(lbSelectAll_Click);
 
             //取消
             lbSelectNo = new Label();
@@ -68,16 +70,26 @@ namespace UserControlDLL
             lbSelectNo.ForeColor = Color.Blue;
             lbSelectNo.Cursor = Cursors.Hand;
             lbSelectNo.TextAlign = ContentAlignment.MiddleCenter;
-            lbSelectNo.Click+=new EventHandler(lbSelectNo_Click);
+            lbSelectNo.Click += new EventHandler(lbSelectNo_Click);
 
             //生成checkboxlist
             this.checkListBox = new CheckedListBox();
             checkListBox.BorderStyle = BorderStyle.None;
-            checkListBox.Location = new Point(0,0);
+            checkListBox.Location = new Point(0, 25);
             checkListBox.CheckOnClick = true;
             checkListBox.ScrollAlwaysVisible = true;
-            checkListBox.LostFocus +=new EventHandler(checkListBox_LostFocus);
-            checkListBox.ItemCheck+=new ItemCheckEventHandler(checkListBox_ItemCheck);
+            //checkListBox.LostFocus += new EventHandler(checkListBox_LostFocus);
+            checkListBox.ItemCheck += new ItemCheckEventHandler(checkListBox_ItemCheck);
+
+            //生成checkListBoxTmp
+            this.checkListBoxTmp = new CheckedListBox();
+            checkListBoxTmp.BorderStyle = BorderStyle.None;
+            checkListBoxTmp.Location = new Point(0, 25);
+            checkListBoxTmp.CheckOnClick = true;
+            checkListBoxTmp.ScrollAlwaysVisible = true;
+            //checkListBoxTmp.LostFocus += new EventHandler(checkListBox_LostFocus);
+            checkListBoxTmp.ItemCheck += new ItemCheckEventHandler(checkListBoxTmp_ItemCheck);
+            checkListBoxTmp.Hide();
 
             //窗体
             frmCheckList = new Form();
@@ -85,10 +97,13 @@ namespace UserControlDLL
             frmCheckList.StartPosition = FormStartPosition.Manual;
             frmCheckList.BackColor = SystemColors.Control;
             frmCheckList.ShowInTaskbar = false;
+            frmCheckList.Activated += new System.EventHandler(this.frmCheckList_Activated);
+            frmCheckList.Deactivate += new System.EventHandler(this.frmCheckList_Deactivate);
+
 
             //可拖动窗体大小变化的LABEL
             lbGrip = new LabelS();
-            lbGrip.Size = new Size(9,18);
+            lbGrip.Size = new Size(9, 18);
             lbGrip.BackColor = Color.Transparent;
             lbGrip.Cursor = Cursors.SizeNWSE;
             //lbGrip.MouseDown+=new MouseEventHandler(lbGrip_MouseDown);
@@ -99,11 +114,21 @@ namespace UserControlDLL
             pnlBack.BorderStyle = BorderStyle.Fixed3D;
             pnlBack.BackColor = Color.White;
             pnlBack.AutoScroll = false;
+            //pnlBack.LostFocus += new EventHandler(checkListBox_LostFocus);
+            // 
+            // textBox1
+            // 
+            textBox1 = new TextBox();
+            textBox1.Size = new System.Drawing.Size(50, 25);
+            textBox1.LostFocus += new EventHandler(textBox1_LostFocus); //失去焦点后发生事件
+            textBox1.GotFocus += new EventHandler(textBox1_GotFocus);  //获取焦点前发生事件
+            textBox1.TextChanged += new EventHandler(textBox1_TextChanged);
 
             //
             pnlCheck = new Panel();
             pnlCheck.BorderStyle = BorderStyle.FixedSingle;
             pnlCheck.BackColor = Color.White; ;
+
 
             SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
             SetStyle(ControlStyles.ResizeRedraw, true);
@@ -113,35 +138,141 @@ namespace UserControlDLL
 
             this.Controls.Add(pnlBack);
 
+
             pnlCheck.Controls.Add(checkListBox);
+            pnlCheck.Controls.Add(checkListBoxTmp);
+            pnlCheck.Controls.Add(textBox1);
             pnlCheck.Controls.Add(lbSelectAll);
             pnlCheck.Controls.Add(lbSelectNo);
             pnlCheck.Controls.Add(lbGrip);
             this.frmCheckList.Controls.Add(pnlCheck);
 
-          
+
+        }
+
+        private void frmCheckList_Deactivate(object sender, EventArgs e)
+        {
+            if (!this.btnSelect.RectangleToScreen(this.btnSelect.ClientRectangle).Contains(Cursor.Position))
+            {
+                frmCheckList.Hide();
+            }
+        }
+
+        private void frmCheckList_Activated(object sender, EventArgs e)
+        {
+            checkListBox.Focus();
+        }
+
+        private void checkListBoxTmp_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            if (ItemClick != null)
+            {
+                ItemClick(sender, e);
+            }
+
+
+            if (GetItemText(this.checkListBoxTmp.Items[e.Index]) == "未找到结果")
+            {
+                e.NewValue = CheckState.Indeterminate;
+                return;
+            }
+
+
+
+            //获取选中的数量
+            int nCount = this.checkListBox.CheckedItems.Count;
+
+            //找到改变状态的checkbox
+            for (int i = 0; i < checkListBox.Items.Count; i++)
+            {
+                if (GetItemText(checkListBox.Items[i]) == GetItemText(this.checkListBoxTmp.Items[e.Index]))
+                {
+                    checkListBox.SetItemChecked(i, e.NewValue == CheckState.Checked ? true : false);
+                }
+            }
+
+            if (this.checkListBoxTmp.CheckedItems.Contains(this.checkListBoxTmp.Items[e.Index]))
+            {
+
+                if (e.NewValue != CheckState.Checked)
+                {
+                    nCount--;
+                }
+            }
+            else
+            {
+                if (e.NewValue == CheckState.Checked)
+                {
+                    nCount++;
+                }
+            }
+
+            tbSelectedValue.Text = "已选择" + nCount.ToString() + "项";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (this.textBox1.Text == "")
+            {
+                checkListBoxTmp.Hide();
+                checkListBox.Show();
+                return;
+            }
+
+            checkListBox.Hide();
+            checkListBoxTmp.Show();
+            int count = 0;
+            this.checkListBoxTmp.Items.Clear();
+            checkListBoxTmp.ItemCheck -= checkListBoxTmp_ItemCheck;
+
+            for (int i = 0; i < checkListBox.Items.Count; i++)
+            {
+                if (GetItemText(checkListBox.Items[i]).Contains(this.textBox1.Text))
+                {
+                    checkListBoxTmp.Items.Add(GetItemText(checkListBox.Items[i]), checkListBox.GetItemChecked(i) ? CheckState.Checked : CheckState.Unchecked);
+                    count++;
+                }
+            }
+
+            if (count == 0)
+            {
+                checkListBoxTmp.Items.Add("未找到结果", CheckState.Indeterminate);
+            }
+
+            checkListBoxTmp.ItemCheck += checkListBoxTmp_ItemCheck;
+        }
+
+        private void textBox1_GotFocus(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_LostFocus(object sender, EventArgs e)
+        {
+
         }
 
         private void ReloationGrip()
         {
+            textBox1.Width = this.frmCheckList.Width - 20;
 
             lbGrip.Top = this.frmCheckList.Height - lbGrip.Height - 1;
             lbGrip.Left = this.frmCheckList.Width - lbGrip.Width - 1;
 
-            lbSelectAll.Left =5;
+            lbSelectAll.Left = 5;
             lbSelectAll.Top = frmCheckList.Height - lbSelectAll.Height;
 
             lbSelectNo.Left = 50;
             lbSelectNo.Top = frmCheckList.Height - lbSelectNo.Height;
-      
+
 
         }
 
         #region 事件
-        
-      
+
+
         //布局
-        private void ComCheckBoxList_Layout(object sender,LayoutEventArgs e)
+        private void ComCheckBoxList_Layout(object sender, LayoutEventArgs e)
         {
             this.Height = tbSelectedValue.Height + 6;
             this.pnlBack.Size = new Size(this.Width, this.Height - 2);
@@ -153,15 +284,16 @@ namespace UserControlDLL
             this.tbSelectedValue.Location = new Point(2, 2);
             this.tbSelectedValue.Width = this.Width - btnSelect.Width - 4;
 
-            checkListBox.Height =150;
+            checkListBox.Height = 150;
 
             //设置窗体
-            this.frmCheckList.Size = new Size(this.Width,this.checkListBox.Height);
-            this.pnlCheck.Size =frmCheckList.Size;
-           
+            this.frmCheckList.Size = new Size(this.Width, this.checkListBox.Height + this.textBox1.Height);
+            this.pnlCheck.Size = frmCheckList.Size;
+
 
             this.checkListBox.Width = this.frmCheckList.Width;
-            this.checkListBox.Height = this.frmCheckList.Height-lbSelectNo.Height;
+            this.checkListBox.Height = this.frmCheckList.Height - lbSelectNo.Height - textBox1.Height;
+            checkListBoxTmp.Size = checkListBox.Size;
 
             ReloationGrip();
 
@@ -172,13 +304,14 @@ namespace UserControlDLL
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        public void btnSelect_Click(object sender,EventArgs e)
+        public void btnSelect_Click(object sender, EventArgs e)
         {
             if (this.frmCheckList.Visible == false)
             {
                 Rectangle rec = this.RectangleToScreen(this.ClientRectangle);
                 this.frmCheckList.Location = new Point(rec.X, rec.Y + this.pnlBack.Height);
                 this.frmCheckList.Show();
+                this.textBox1.Text = "";
                 this.frmCheckList.BringToFront();
 
                 ReloationGrip();
@@ -194,10 +327,10 @@ namespace UserControlDLL
             {
                 checkListBox.SetItemChecked(i, true);
             }
-            tbSelectedValue.Text ="已选择"+checkListBox.Items.Count.ToString()+"项";
+            tbSelectedValue.Text = "已选择" + checkListBox.Items.Count.ToString() + "项";
         }
         //取消
-        private void lbSelectNo_Click(object sender,EventArgs e)
+        private void lbSelectNo_Click(object sender, EventArgs e)
         {
             for (int i = 0; i < checkListBox.Items.Count; i++)
             {
@@ -217,11 +350,11 @@ namespace UserControlDLL
 
         private void checkListBox_ItemCheck(object sender, ItemCheckEventArgs e)
         {
-            if (ItemClick!=null)
+            if (ItemClick != null)
             {
                 ItemClick(sender, e);
             }
-           //获取选中的数量
+            //获取选中的数量
             int nCount = this.checkListBox.CheckedItems.Count;
             if (this.checkListBox.CheckedItems.Contains(this.checkListBox.Items[e.Index]))
             {
@@ -239,10 +372,10 @@ namespace UserControlDLL
                 }
             }
             tbSelectedValue.Text = "已选择" + nCount.ToString() + "项";
-       
+
         }
         #region 获取与加载被选中项的索引
-        public List<int> GetItemCheckIndex() 
+        public List<int> GetItemCheckIndex()
         {
             List<int> checkIndexs = new List<int>();
             for (int i = 0; i < checkListBox.Items.Count; i++)
@@ -256,7 +389,7 @@ namespace UserControlDLL
         {
             foreach (int index in checkIndexs)
                 checkListBox.SetItemChecked(index, true);
-            if(checkIndexs.Count()>0)
+            if (checkIndexs.Count() > 0)
                 tbSelectedValue.Text = "已选择" + checkIndexs.Count().ToString() + "项";
         }
         #endregion
@@ -268,7 +401,7 @@ namespace UserControlDLL
         /// <param name="e"></param>
         private void lbGrip_MouseDown(object sender, MouseEventArgs e)
         {
-            if (e.Button==MouseButtons.Left)
+            if (e.Button == MouseButtons.Left)
             {
                 int offsetX = System.Math.Abs(Cursor.Position.X - frmCheckList.RectangleToScreen(this.frmCheckList.ClientRectangle).Right);
                 int offsetY = System.Math.Abs(Cursor.Position.Y - frmCheckList.RectangleToScreen(this.frmCheckList.ClientRectangle).Bottom);
