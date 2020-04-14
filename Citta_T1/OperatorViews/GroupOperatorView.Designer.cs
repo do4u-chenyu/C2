fe86f7eb96969fbfe9637e56b8c4b3967eaa7d17
phoenix1214 <273048269@qq.com>
2020-04-14 17:09:55 +0800

    新增分组算子窗口

---
+++ Citta_T1/OperatorViews/GroupOperatorView.Designer.cs
@@ -0,0 +1,371 @@
+﻿namespace Citta_T1.OperatorViews
+{
+    partial class GroupOperatorView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.topPanel = new System.Windows.Forms.Panel();
+            this.bottomPanel = new System.Windows.Forms.Panel();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.confirmButton = new System.Windows.Forms.Button();
+            this.keyPanel = new System.Windows.Forms.Panel();
+            this.valuePanel = new System.Windows.Forms.Panel();
+            this.DataInfoBox = new HZH_Controls.Controls.TextBoxEx();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBoxEx1 = new HZH_Controls.Controls.TextBoxEx();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.bottomPanel.SuspendLayout();
+            this.keyPanel.SuspendLayout();
+            this.valuePanel.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(48, 392);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 31);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "是否去重：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(48, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 31);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "分组字段：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(48, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "数据信息：";
+            // 
+            // topPanel
+            // 
+            this.topPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.topPanel.Location = new System.Drawing.Point(0, 0);
+            this.topPanel.Name = "topPanel";
+            this.topPanel.Size = new System.Drawing.Size(873, 56);
+            this.topPanel.TabIndex = 0;
+            // 
+            // bottomPanel
+            // 
+            this.bottomPanel.Controls.Add(this.cancelButton);
+            this.bottomPanel.Controls.Add(this.confirmButton);
+            this.bottomPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.bottomPanel.Location = new System.Drawing.Point(0, 523);
+            this.bottomPanel.Name = "bottomPanel";
+            this.bottomPanel.Size = new System.Drawing.Size(873, 96);
+            this.bottomPanel.TabIndex = 1;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.FlatAppearance.BorderSize = 0;
+            this.cancelButton.Font = new System.Drawing.Font("微软雅黑", 11F);
+            this.cancelButton.Location = new System.Drawing.Point(741, 28);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(94, 40);
+            this.cancelButton.TabIndex = 1;
+            this.cancelButton.Text = "取消";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // confirmButton
+            // 
+            this.confirmButton.BackColor = System.Drawing.SystemColors.Highlight;
+            this.confirmButton.FlatAppearance.BorderSize = 0;
+            this.confirmButton.Font = new System.Drawing.Font("微软雅黑", 11F);
+            this.confirmButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.confirmButton.Location = new System.Drawing.Point(591, 28);
+            this.confirmButton.Name = "confirmButton";
+            this.confirmButton.Size = new System.Drawing.Size(90, 40);
+            this.confirmButton.TabIndex = 0;
+            this.confirmButton.Text = "确认";
+            this.confirmButton.UseVisualStyleBackColor = false;
+            this.confirmButton.Click += new System.EventHandler(this.confirmButton_Click);
+            // 
+            // keyPanel
+            // 
+            this.keyPanel.Controls.Add(this.label3);
+            this.keyPanel.Controls.Add(this.label2);
+            this.keyPanel.Controls.Add(this.label1);
+            this.keyPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.keyPanel.Location = new System.Drawing.Point(0, 56);
+            this.keyPanel.Name = "keyPanel";
+            this.keyPanel.Size = new System.Drawing.Size(174, 467);
+            this.keyPanel.TabIndex = 2;
+            // 
+            // valuePanel
+            // 
+            this.valuePanel.Controls.Add(this.DataInfoBox);
+            this.valuePanel.Controls.Add(this.tableLayoutPanel2);
+            this.valuePanel.Controls.Add(this.groupBox1);
+            this.valuePanel.Controls.Add(this.panel1);
+            this.valuePanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.valuePanel.Location = new System.Drawing.Point(174, 56);
+            this.valuePanel.Name = "valuePanel";
+            this.valuePanel.Size = new System.Drawing.Size(699, 467);
+            this.valuePanel.TabIndex = 3;
+            // 
+            // DataInfoBox
+            // 
+            this.DataInfoBox.DecLength = 2;
+            this.DataInfoBox.InputType = HZH_Controls.TextInputType.NotControl;
+            this.DataInfoBox.Location = new System.Drawing.Point(8, 3);
+            this.DataInfoBox.MaxValue = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.DataInfoBox.MinValue = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            -2147483648});
+            this.DataInfoBox.MyRectangle = new System.Drawing.Rectangle(0, 0, 0, 0);
+            this.DataInfoBox.Name = "DataInfoBox";
+            this.DataInfoBox.OldText = null;
+            this.DataInfoBox.PromptColor = System.Drawing.Color.Gray;
+            this.DataInfoBox.PromptFont = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.DataInfoBox.PromptText = "";
+            this.DataInfoBox.ReadOnly = true;
+            this.DataInfoBox.RegexPattern = "";
+            this.DataInfoBox.Size = new System.Drawing.Size(136, 28);
+            this.DataInfoBox.TabIndex = 7;
+            this.DataInfoBox.Text = "test1.bcp";
+            this.DataInfoBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 120F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 57F));
+            this.tableLayoutPanel2.Controls.Add(this.button1, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.comboBox1, 0, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(142, 81);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(207, 48);
+            this.tableLayoutPanel2.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.SystemColors.Control;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.Control;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.Control;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = global::Citta_T1.Properties.Resources.add;
+            this.button1.Location = new System.Drawing.Point(153, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(51, 36);
+            this.button1.TabIndex = 5;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.add_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(3, 11);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(144, 26);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(8, 386);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(207, 37);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.groupBox1_Paint);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(113, 12);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(87, 22);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "不去重";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(7, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(69, 22);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "去重";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Location = new System.Drawing.Point(6, 130);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(694, 240);
+            this.panel1.TabIndex = 1;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 135F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 57F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 57F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(2, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(395, 127);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // textBoxEx1
+            // 
+            this.textBoxEx1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxEx1.DecLength = 2;
+            this.textBoxEx1.InputType = HZH_Controls.TextInputType.NotControl;
+            this.textBoxEx1.Location = new System.Drawing.Point(273, 10);
+            this.textBoxEx1.MaxValue = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.textBoxEx1.MinValue = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            -2147483648});
+            this.textBoxEx1.MyRectangle = new System.Drawing.Rectangle(0, 0, 0, 0);
+            this.textBoxEx1.Name = "textBoxEx1";
+            this.textBoxEx1.OldText = null;
+            this.textBoxEx1.PromptColor = System.Drawing.Color.Gray;
+            this.textBoxEx1.PromptFont = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.textBoxEx1.PromptText = "";
+            this.textBoxEx1.RegexPattern = "";
+            this.textBoxEx1.Size = new System.Drawing.Size(129, 28);
+            this.textBoxEx1.TabIndex = 4;
+            this.textBoxEx1.Text = "别名";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "大于 >",
+            "小于 <",
+            "等于 =",
+            "大于等于 ≥",
+            "小于等于 ≦",
+            "不等于 ≠"});
+            this.comboBox2.Location = new System.Drawing.Point(138, 11);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(129, 26);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // GroupOperatorView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(873, 619);
+            this.ControlBox = false;
+            this.Controls.Add(this.valuePanel);
+            this.Controls.Add(this.keyPanel);
+            this.Controls.Add(this.bottomPanel);
+            this.Controls.Add(this.topPanel);
+            this.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "GroupOperatorView";
+            this.ShowIcon = false;
+            this.Text = "取并集算子设置";
+            this.bottomPanel.ResumeLayout(false);
+            this.keyPanel.ResumeLayout(false);
+            this.keyPanel.PerformLayout();
+            this.valuePanel.ResumeLayout(false);
+            this.valuePanel.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel topPanel;
+        private System.Windows.Forms.Panel bottomPanel;
+        private System.Windows.Forms.Panel keyPanel;
+        private System.Windows.Forms.Panel valuePanel;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button confirmButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private HZH_Controls.Controls.TextBoxEx textBoxEx1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private HZH_Controls.Controls.TextBoxEx DataInfoBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
