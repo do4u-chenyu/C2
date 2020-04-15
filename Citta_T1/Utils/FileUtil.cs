c2f0287d04ebf2a467e7446bf7e68300f4219025
Small White <1309744250@qq.com>
2020-04-15 16:29:07 +0800

    增加文件创建时权限设置

---
+++ Citta_T1/Utils/FileUtil.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Citta_T1.Utils
+{
+    class FileUtil
+    {
+        public static void addpathPower(string pathname, string power)
+        {
+            string username = System.Environment.UserName;
+            DirectoryInfo dirinfo = new DirectoryInfo(pathname);
+
+            if ((dirinfo.Attributes & FileAttributes.ReadOnly) != 0)
+            {
+                dirinfo.Attributes = FileAttributes.Normal;
+            }
+
+            //取得访问控制列表   
+            DirectorySecurity dirsecurity = dirinfo.GetAccessControl();
+
+            switch (power)
+            {
+                case "FullControl":
+                    dirsecurity.AddAccessRule(new FileSystemAccessRule(username, FileSystemRights.FullControl, InheritanceFlags.ContainerInherit, PropagationFlags.InheritOnly, AccessControlType.Allow));
+                    break;
+                case "ReadOnly":
+                    dirsecurity.AddAccessRule(new FileSystemAccessRule(username, FileSystemRights.Read, AccessControlType.Allow));
+                    break;
+                case "Write":
+                    dirsecurity.AddAccessRule(new FileSystemAccessRule(username, FileSystemRights.Write, AccessControlType.Allow));
+                    break;
+                case "Modify":
+                    dirsecurity.AddAccessRule(new FileSystemAccessRule(username, FileSystemRights.Modify, AccessControlType.Allow));
+                    break;
+            }
+            dirinfo.SetAccessControl(dirsecurity);
+        }
+    }
+}
