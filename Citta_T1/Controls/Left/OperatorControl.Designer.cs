4010206e384e3f5ca593553ae9125a2ecb1404d6
刘晓凤 <lxfjiangnan@163.com>
2020-04-15 12:49:00 +0800

    增加新算子

--- Citta_T1/Controls/Left/OperatorControl.Designer.cs
+++ Citta_T1/Controls/Left/OperatorControl.Designer.cs
@@ -47,6 +47,12 @@
             this.leftpanelOpMax = new System.Windows.Forms.Button();
             this.leftpanelOpMin = new System.Windows.Forms.Button();
             this.leftpanelOpMean = new System.Windows.Forms.Button();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.pictureBox12 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -56,6 +62,9 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
             this.SuspendLayout();
             // 
             // leftpanelOpConect
@@ -84,7 +93,7 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(36, 84);
+            this.pictureBox2.Location = new System.Drawing.Point(36, 71);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(20, 21);
             this.pictureBox2.TabIndex = 3;
@@ -93,7 +102,7 @@
             // pictureBox3
             // 
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(36, 206);
+            this.pictureBox3.Location = new System.Drawing.Point(36, 167);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(20, 21);
             this.pictureBox3.TabIndex = 4;
@@ -102,7 +111,7 @@
             // pictureBox4
             // 
             this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
-            this.pictureBox4.Location = new System.Drawing.Point(36, 267);
+            this.pictureBox4.Location = new System.Drawing.Point(36, 215);
             this.pictureBox4.Name = "pictureBox4";
             this.pictureBox4.Size = new System.Drawing.Size(20, 21);
             this.pictureBox4.TabIndex = 6;
@@ -111,7 +120,7 @@
             // pictureBox5
             // 
             this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
-            this.pictureBox5.Location = new System.Drawing.Point(36, 389);
+            this.pictureBox5.Location = new System.Drawing.Point(36, 310);
             this.pictureBox5.Name = "pictureBox5";
             this.pictureBox5.Size = new System.Drawing.Size(20, 21);
             this.pictureBox5.TabIndex = 7;
@@ -120,7 +129,7 @@
             // pictureBox6
             // 
             this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
-            this.pictureBox6.Location = new System.Drawing.Point(36, 328);
+            this.pictureBox6.Location = new System.Drawing.Point(36, 264);
             this.pictureBox6.Name = "pictureBox6";
             this.pictureBox6.Size = new System.Drawing.Size(20, 21);
             this.pictureBox6.TabIndex = 8;
@@ -129,7 +138,7 @@
             // pictureBox7
             // 
             this.pictureBox7.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox7.Image")));
-            this.pictureBox7.Location = new System.Drawing.Point(36, 511);
+            this.pictureBox7.Location = new System.Drawing.Point(36, 406);
             this.pictureBox7.Name = "pictureBox7";
             this.pictureBox7.Size = new System.Drawing.Size(20, 21);
             this.pictureBox7.TabIndex = 9;
@@ -138,7 +147,7 @@
             // pictureBox8
             // 
             this.pictureBox8.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox8.Image")));
-            this.pictureBox8.Location = new System.Drawing.Point(36, 450);
+            this.pictureBox8.Location = new System.Drawing.Point(36, 359);
             this.pictureBox8.Name = "pictureBox8";
             this.pictureBox8.Size = new System.Drawing.Size(20, 21);
             this.pictureBox8.TabIndex = 10;
@@ -150,7 +159,7 @@
             this.leftPanelOpIntersect.FlatAppearance.BorderSize = 0;
             this.leftPanelOpIntersect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.leftPanelOpIntersect.Font = new System.Drawing.Font("微软雅黑", 10F);
-            this.leftPanelOpIntersect.Location = new System.Drawing.Point(70, 74);
+            this.leftPanelOpIntersect.Location = new System.Drawing.Point(70, 61);
             this.leftPanelOpIntersect.Name = "leftPanelOpIntersect";
             this.leftPanelOpIntersect.Size = new System.Drawing.Size(74, 40);
             this.leftPanelOpIntersect.TabIndex = 18;
@@ -161,7 +170,7 @@
             // pictureBox10
             // 
             this.pictureBox10.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox10.Image")));
-            this.pictureBox10.Location = new System.Drawing.Point(36, 145);
+            this.pictureBox10.Location = new System.Drawing.Point(36, 119);
             this.pictureBox10.Name = "pictureBox10";
             this.pictureBox10.Size = new System.Drawing.Size(20, 21);
             this.pictureBox10.TabIndex = 4;
@@ -173,7 +182,7 @@
             this.lefpanelOpUnion.FlatAppearance.BorderSize = 0;
             this.lefpanelOpUnion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.lefpanelOpUnion.Font = new System.Drawing.Font("微软雅黑", 10F);
-            this.lefpanelOpUnion.Location = new System.Drawing.Point(70, 135);
+            this.lefpanelOpUnion.Location = new System.Drawing.Point(70, 109);
             this.lefpanelOpUnion.Name = "lefpanelOpUnion";
             this.lefpanelOpUnion.Size = new System.Drawing.Size(74, 40);
             this.lefpanelOpUnion.TabIndex = 19;
@@ -188,7 +197,7 @@
             this.leftpanelOpDiffer.FlatAppearance.BorderSize = 0;
             this.leftpanelOpDiffer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.leftpanelOpDiffer.Font = new System.Drawing.Font("微软雅黑", 10F);
-            this.leftpanelOpDiffer.Location = new System.Drawing.Point(70, 196);
+            this.leftpanelOpDiffer.Location = new System.Drawing.Point(70, 157);
             this.leftpanelOpDiffer.Name = "leftpanelOpDiffer";
             this.leftpanelOpDiffer.Size = new System.Drawing.Size(74, 40);
             this.leftpanelOpDiffer.TabIndex = 20;
@@ -202,7 +211,7 @@
             this.leftpanelOpRandom.FlatAppearance.BorderSize = 0;
             this.leftpanelOpRandom.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.leftpanelOpRandom.Font = new System.Drawing.Font("微软雅黑", 10F);
-            this.leftpanelOpRandom.Location = new System.Drawing.Point(69, 257);
+            this.leftpanelOpRandom.Location = new System.Drawing.Point(69, 205);
             this.leftpanelOpRandom.Name = "leftpanelOpRandom";
             this.leftpanelOpRandom.Size = new System.Drawing.Size(74, 40);
             this.leftpanelOpRandom.TabIndex = 21;
@@ -216,7 +225,7 @@
             this.leftpanelOpFilter.FlatAppearance.BorderSize = 0;
             this.leftpanelOpFilter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.leftpanelOpFilter.Font = new System.Drawing.Font("微软雅黑", 10F);
-            this.leftpanelOpFilter.Location = new System.Drawing.Point(69, 318);
+            this.leftpanelOpFilter.Location = new System.Drawing.Point(69, 253);
             this.leftpanelOpFilter.Name = "leftpanelOpFilter";
             this.leftpanelOpFilter.Size = new System.Drawing.Size(74, 40);
             this.leftpanelOpFilter.TabIndex = 22;
@@ -230,7 +239,7 @@
             this.leftpanelOpMax.FlatAppearance.BorderSize = 0;
             this.leftpanelOpMax.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.leftpanelOpMax.Font = new System.Drawing.Font("微软雅黑", 10F);
-            this.leftpanelOpMax.Location = new System.Drawing.Point(69, 379);
+            this.leftpanelOpMax.Location = new System.Drawing.Point(69, 301);
             this.leftpanelOpMax.Name = "leftpanelOpMax";
             this.leftpanelOpMax.Size = new System.Drawing.Size(74, 40);
             this.leftpanelOpMax.TabIndex = 23;
@@ -244,7 +253,7 @@
             this.leftpanelOpMin.FlatAppearance.BorderSize = 0;
             this.leftpanelOpMin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.leftpanelOpMin.Font = new System.Drawing.Font("微软雅黑", 10F);
-            this.leftpanelOpMin.Location = new System.Drawing.Point(69, 440);
+            this.leftpanelOpMin.Location = new System.Drawing.Point(69, 349);
             this.leftpanelOpMin.Name = "leftpanelOpMin";
             this.leftpanelOpMin.Size = new System.Drawing.Size(74, 40);
             this.leftpanelOpMin.TabIndex = 24;
@@ -258,7 +267,7 @@
             this.leftpanelOpMean.FlatAppearance.BorderSize = 0;
             this.leftpanelOpMean.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.leftpanelOpMean.Font = new System.Drawing.Font("微软雅黑", 10F);
-            this.leftpanelOpMean.Location = new System.Drawing.Point(69, 501);
+            this.leftpanelOpMean.Location = new System.Drawing.Point(69, 397);
             this.leftpanelOpMean.Name = "leftpanelOpMean";
             this.leftpanelOpMean.Size = new System.Drawing.Size(74, 40);
             this.leftpanelOpMean.TabIndex = 25;
@@ -267,6 +276,78 @@
             this.leftpanelOpMean.UseVisualStyleBackColor = true;
             this.leftpanelOpMean.MouseDown += new System.Windows.Forms.MouseEventHandler(this.LeftPaneOp_MouseDown);
             // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox9.Image")));
+            this.pictureBox9.Location = new System.Drawing.Point(36, 455);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(20, 21);
+            this.pictureBox9.TabIndex = 26;
+            this.pictureBox9.TabStop = false;
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox11.Image")));
+            this.pictureBox11.Location = new System.Drawing.Point(36, 504);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(20, 21);
+            this.pictureBox11.TabIndex = 27;
+            this.pictureBox11.TabStop = false;
+            // 
+            // pictureBox12
+            // 
+            this.pictureBox12.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox12.Image")));
+            this.pictureBox12.Location = new System.Drawing.Point(36, 552);
+            this.pictureBox12.Name = "pictureBox12";
+            this.pictureBox12.Size = new System.Drawing.Size(20, 21);
+            this.pictureBox12.TabIndex = 28;
+            this.pictureBox12.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 10F);
+            this.button1.Location = new System.Drawing.Point(70, 445);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 40);
+            this.button1.TabIndex = 29;
+            this.button1.Text = "频率算子";
+            this.button1.UseMnemonic = false;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.LeftPaneOp_MouseDown);
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 10F);
+            this.button2.Location = new System.Drawing.Point(69, 493);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 40);
+            this.button2.TabIndex = 30;
+            this.button2.Text = "排序算子";
+            this.button2.UseMnemonic = false;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.LeftPaneOp_MouseDown);
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("微软雅黑", 10F);
+            this.button3.Location = new System.Drawing.Point(69, 541);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(74, 40);
+            this.button3.TabIndex = 31;
+            this.button3.Text = "分组算子";
+            this.button3.UseMnemonic = false;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.LeftPaneOp_MouseDown);
+            // 
             // OperatorControl
             // 
             this.AllowDrop = true;
@@ -274,6 +355,12 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox12);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.pictureBox9);
             this.Controls.Add(this.leftpanelOpConect);
             this.Controls.Add(this.leftpanelOpMean);
             this.Controls.Add(this.leftpanelOpMin);
@@ -303,6 +390,9 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -327,5 +417,11 @@
         private System.Windows.Forms.Button leftpanelOpMax;
         private System.Windows.Forms.Button leftpanelOpMin;
         private System.Windows.Forms.Button leftpanelOpMean;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.PictureBox pictureBox12;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
