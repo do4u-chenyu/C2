14c942cb461484b24890379a6a0c67064d9d1863
lu-qi123 <18705158378@163.com>
2020-04-21 17:02:17 +0800

    增加一些浮动提示信息

--- Citta_T1/MainForm.Designer.cs
+++ Citta_T1/MainForm.Designer.cs
@@ -386,7 +386,7 @@
             this.minMaxPictureBox.Size = new System.Drawing.Size(25, 24);
             this.minMaxPictureBox.TabIndex = 1;
             this.minMaxPictureBox.TabStop = false;
-            this.toolTip1.SetToolTip(this.minMaxPictureBox, "显示数据框");
+            this.toolTip1.SetToolTip(this.minMaxPictureBox, "隐藏(展开)底层面板");
             this.minMaxPictureBox.Click += new System.EventHandler(this.MinMaxPictureBox_Click);
             // 
             // panel8
@@ -677,6 +677,7 @@
             this.leftFoldButton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.leftFoldButton.TabIndex = 0;
             this.leftFoldButton.TabStop = false;
+            this.toolTip1.SetToolTip(this.leftFoldButton, "隐藏(展开)左侧面板");
             this.leftFoldButton.Click += new System.EventHandler(this.LeftFoldButton_Click);
             // 
             // blankButton
