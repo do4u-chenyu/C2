b2c6d23b6d3dd3b41a16f27b6f35e7d3b490a46f
phoenix1214 <273048269@qq.com>
2020-04-24 10:44:36 +0800

    1、修改算子factor判断逻辑；2、去重逻辑小修改；3、新增输入文件编码判断，统一输出utf-8编码

--- Citta_T1/Business/Schedule/Cmd/GroupOperatorCmd.cs
+++ Citta_T1/Business/Schedule/Cmd/GroupOperatorCmd.cs
@@ -20,18 +20,18 @@ namespace Citta_T1.Business.Schedule.Cmd
             string outfieldLine = TransOutputField(option.GetOption("outField").Split(','));//输出字段
 
             //是否去重(是对整个文件去重)、升降序
-            string repetition = option.GetOption("repetition") == "True" ? " -u " : "";
+            string repetition = option.GetOption("noRepetition") == "True" ? string.Format("sbin\\sort.exe {0} -u | ",this.sortConfig) : "";
             string order = option.GetOption("ascendingOrder") == "True" ? " -n " : "-nr ";
 
-            string factor1 = option.GetOption("factor1");
-            string awkSortCmd = "-k" + TransInputLine(factor1);
-            for (int i = 2; i <= option.OptionDict.Count() - 5; i++)
+            //拼接分组字段
+            string sortLineCmd = "-k" + TransInputLine(option.GetOption("factor1"));
+            for (int i = 2; i <= GetOptionFactorCount(); i++)
             {
                 string tmpfactor = option.GetOption("factor" + i.ToString());
-                awkSortCmd = awkSortCmd + " -k" + TransInputLine(tmpfactor);
+                sortLineCmd = sortLineCmd + " -k" + TransInputLine(tmpfactor);
             }
 
-            cmds.Add(string.Format("{0} | sbin\\sort.exe {1} {2} {3} {4} | sbin\\tr.exe -d '\\r' | sbin\\awk.exe -F'\\t' -v OFS='\\t' '{{ print {5}}}'>> {6}", TransInputfileToCmd(inputFilePath), this.sortConfig, repetition, order, awkSortCmd, outfieldLine, this.outputFilePath));
+            cmds.Add(string.Format("{0} | {1} sbin\\sort.exe {2} {3} {4} | sbin\\tr.exe -d '\\r' | sbin\\awk.exe -F'\\t' -v OFS='\\t' '{{ print {5}}}'>> {6}", TransInputfileToCmd(inputFilePath), repetition, this.sortConfig, order, sortLineCmd, outfieldLine, this.outputFilePath));
 
             return cmds;
         }
