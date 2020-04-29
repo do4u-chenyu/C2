d223e727f9e3232fd8b3c454f553d408c5a8395f
lu-qi123 <18705158378@163.com>
2020-04-29 21:44:43 +0800

    增加社交网络分析和自定义算子规划

--- Citta_T1/Controls/Left/FlowChartControl.Designer.cs
+++ Citta_T1/Controls/Left/FlowChartControl.Designer.cs
@@ -28,18 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FlowChartControl));
+            this.leftPanelOpRelate = new Citta_T1.Controls.Common.NoFocusButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.leftPanelOpCollide = new Citta_T1.Controls.Common.NoFocusButton();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // leftPanelOpRelate
+            // 
+            this.leftPanelOpRelate.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.leftPanelOpRelate.FlatAppearance.BorderSize = 0;
+            this.leftPanelOpRelate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.leftPanelOpRelate.Font = new System.Drawing.Font("微软雅黑", 10F);
+            this.leftPanelOpRelate.Location = new System.Drawing.Point(66, 18);
+            this.leftPanelOpRelate.Name = "leftPanelOpRelate";
+            this.leftPanelOpRelate.Size = new System.Drawing.Size(104, 40);
+            this.leftPanelOpRelate.TabIndex = 2;
+            this.leftPanelOpRelate.Text = "社交网络分析";
+            this.leftPanelOpRelate.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(32, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(20, 21);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // leftPanelOpCollide
+            // 
+            this.leftPanelOpCollide.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.leftPanelOpCollide.FlatAppearance.BorderSize = 0;
+            this.leftPanelOpCollide.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.leftPanelOpCollide.Font = new System.Drawing.Font("微软雅黑", 10F);
+            this.leftPanelOpCollide.Location = new System.Drawing.Point(66, 64);
+            this.leftPanelOpCollide.Name = "leftPanelOpCollide";
+            this.leftPanelOpCollide.Size = new System.Drawing.Size(104, 40);
+            this.leftPanelOpCollide.TabIndex = 20;
+            this.leftPanelOpCollide.Text = "自定义算子";
+            this.leftPanelOpCollide.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(32, 74);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(20, 21);
+            this.pictureBox2.TabIndex = 19;
+            this.pictureBox2.TabStop = false;
             // 
             // FlowChartControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.leftPanelOpCollide);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.leftPanelOpRelate);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "FlowChartControl";
             this.Size = new System.Drawing.Size(187, 637);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Common.NoFocusButton leftPanelOpRelate;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private Common.NoFocusButton leftPanelOpCollide;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
