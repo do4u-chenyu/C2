f952ce8de0dace5bed79e3a72e56ecba48badbc5
lu-qi123 <18705158378@163.com>
2020-05-07 16:33:15 +0800

    小重构,增加了一些安全检查

--- Citta_T1/IAOLab/CustomOP/CustomOperatorCmd.cs
+++ Citta_T1/IAOLab/CustomOP/CustomOperatorCmd.cs
@@ -8,6 +8,8 @@ namespace Citta_T1.Business.Schedule.Cmd
 {
     class CustomOperatorCmd : OperatorCmd
     {
+        private static int DefaultSleepSecond = 30;
+
         public CustomOperatorCmd(Triple triple) : base(triple)
         {
 
@@ -17,7 +19,22 @@ namespace Citta_T1.Business.Schedule.Cmd
         {
             List<string> cmds = new List<string>();
             // 休眠指定时间
+            DoSleepCommand();
             return cmds;
+        }
+
+        private void DoSleepCommand()
+        {
+            int sleepSecond = DefaultSleepSecond;
+            try
+            {
+                sleepSecond = Convert.ToInt32(option.GetOption("sleepSec", DefaultSleepSecond.ToString()));
+            }
+            catch
+            {
+                sleepSecond = DefaultSleepSecond;
+            }
+            System.Threading.Thread.Sleep(sleepSecond * 1000);
         }
     }
 }
