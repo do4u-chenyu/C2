37976f52208393da30211f4a85578e917d621e0a
lu-qi123 <18705158378@163.com>
2020-05-09 22:31:28 +0800

    继续修改python窗口配置

--- Citta_T1/IAOLab/PythonOP/PythonOperatorView.Designer.cs
+++ Citta_T1/IAOLab/PythonOP/PythonOperatorView.Designer.cs
@@ -128,12 +128,12 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label4.Location = new System.Drawing.Point(45, 126);
+            this.label4.Location = new System.Drawing.Point(13, 128);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(90, 22);
+            this.label4.Size = new System.Drawing.Size(122, 22);
             this.label4.TabIndex = 16;
-            this.label4.Text = "传入参数：";
+            this.label4.Text = "脚本传入参数：";
             // 
             // textBox2
             // 
