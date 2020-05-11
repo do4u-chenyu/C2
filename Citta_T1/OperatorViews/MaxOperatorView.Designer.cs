bd92309247f0f2e1061796bccb317505a13d5726
lu-qi123 <18705158378@163.com>
2020-05-11 16:31:30 +0800

    继续调整各个配置窗体的视觉效果

--- Citta_T1/OperatorViews/MaxOperatorView.Designer.cs
+++ Citta_T1/OperatorViews/MaxOperatorView.Designer.cs
@@ -38,9 +38,9 @@
             this.confirmButton = new System.Windows.Forms.Button();
             this.keyPanel = new System.Windows.Forms.Panel();
             this.valuePanel = new System.Windows.Forms.Panel();
-            this.DataInfoBox = new System.Windows.Forms.TextBox();
+            this.dataInfoBox = new System.Windows.Forms.TextBox();
             this.OutList = new UserControlDLL.ComCheckBoxList();
-            this.MaxValueBox = new System.Windows.Forms.ComboBox();
+            this.maxValueBox = new System.Windows.Forms.ComboBox();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.bottomPanel.SuspendLayout();
             this.keyPanel.SuspendLayout();
@@ -51,7 +51,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label3.Location = new System.Drawing.Point(32, 128);
+            this.label3.Location = new System.Drawing.Point(32, 105);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(90, 22);
@@ -62,7 +62,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label2.Location = new System.Drawing.Point(32, 64);
+            this.label2.Location = new System.Drawing.Point(32, 55);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(90, 22);
@@ -73,7 +73,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(32, 0);
+            this.label1.Location = new System.Drawing.Point(32, 4);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(90, 22);
@@ -84,9 +84,9 @@
             // 
             this.topPanel.Dock = System.Windows.Forms.DockStyle.Top;
             this.topPanel.Location = new System.Drawing.Point(0, 0);
-            this.topPanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.topPanel.Margin = new System.Windows.Forms.Padding(2);
             this.topPanel.Name = "topPanel";
-            this.topPanel.Size = new System.Drawing.Size(407, 37);
+            this.topPanel.Size = new System.Drawing.Size(340, 18);
             this.topPanel.TabIndex = 0;
             // 
             // bottomPanel
@@ -94,18 +94,18 @@
             this.bottomPanel.Controls.Add(this.cancelButton);
             this.bottomPanel.Controls.Add(this.confirmButton);
             this.bottomPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.bottomPanel.Location = new System.Drawing.Point(0, 203);
-            this.bottomPanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.bottomPanel.Location = new System.Drawing.Point(0, 164);
+            this.bottomPanel.Margin = new System.Windows.Forms.Padding(2);
             this.bottomPanel.Name = "bottomPanel";
-            this.bottomPanel.Size = new System.Drawing.Size(407, 64);
+            this.bottomPanel.Size = new System.Drawing.Size(340, 43);
             this.bottomPanel.TabIndex = 1;
             // 
             // cancelButton
             // 
             this.cancelButton.FlatAppearance.BorderSize = 0;
             this.cancelButton.Font = new System.Drawing.Font("微软雅黑", 11F);
-            this.cancelButton.Location = new System.Drawing.Point(326, 19);
-            this.cancelButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cancelButton.Location = new System.Drawing.Point(266, 9);
+            this.cancelButton.Margin = new System.Windows.Forms.Padding(2);
             this.cancelButton.Name = "cancelButton";
             this.cancelButton.Size = new System.Drawing.Size(63, 27);
             this.cancelButton.TabIndex = 1;
@@ -119,8 +119,8 @@
             this.confirmButton.FlatAppearance.BorderSize = 0;
             this.confirmButton.Font = new System.Drawing.Font("微软雅黑", 11F);
             this.confirmButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.confirmButton.Location = new System.Drawing.Point(236, 19);
-            this.confirmButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.confirmButton.Location = new System.Drawing.Point(176, 9);
+            this.confirmButton.Margin = new System.Windows.Forms.Padding(2);
             this.confirmButton.Name = "confirmButton";
             this.confirmButton.Size = new System.Drawing.Size(60, 27);
             this.confirmButton.TabIndex = 0;
@@ -134,63 +134,63 @@
             this.keyPanel.Controls.Add(this.label2);
             this.keyPanel.Controls.Add(this.label1);
             this.keyPanel.Dock = System.Windows.Forms.DockStyle.Left;
-            this.keyPanel.Location = new System.Drawing.Point(0, 37);
-            this.keyPanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.keyPanel.Location = new System.Drawing.Point(0, 18);
+            this.keyPanel.Margin = new System.Windows.Forms.Padding(2);
             this.keyPanel.Name = "keyPanel";
-            this.keyPanel.Size = new System.Drawing.Size(116, 166);
+            this.keyPanel.Size = new System.Drawing.Size(116, 146);
             this.keyPanel.TabIndex = 2;
             // 
             // valuePanel
             // 
-            this.valuePanel.Controls.Add(this.DataInfoBox);
+            this.valuePanel.Controls.Add(this.dataInfoBox);
             this.valuePanel.Controls.Add(this.OutList);
-            this.valuePanel.Controls.Add(this.MaxValueBox);
+            this.valuePanel.Controls.Add(this.maxValueBox);
             this.valuePanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.valuePanel.Location = new System.Drawing.Point(116, 37);
-            this.valuePanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.valuePanel.Location = new System.Drawing.Point(116, 18);
+            this.valuePanel.Margin = new System.Windows.Forms.Padding(2);
             this.valuePanel.Name = "valuePanel";
-            this.valuePanel.Size = new System.Drawing.Size(291, 166);
+            this.valuePanel.Size = new System.Drawing.Size(224, 146);
             this.valuePanel.TabIndex = 3;
             // 
-            // DataInfoBox
+            // dataInfoBox
             // 
-            this.DataInfoBox.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.DataInfoBox.Location = new System.Drawing.Point(0, 0);
-            this.DataInfoBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.DataInfoBox.Name = "DataInfoBox";
-            this.DataInfoBox.ReadOnly = true;
-            this.DataInfoBox.Size = new System.Drawing.Size(136, 23);
-            this.DataInfoBox.TabIndex = 3;
-            this.DataInfoBox.TabStop = false;
-            this.DataInfoBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.DataInfoBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.DataInfoBox_MouseClick);
-            this.DataInfoBox.LostFocus += new System.EventHandler(this.DataInfoBox_LostFocus);
+            this.dataInfoBox.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.dataInfoBox.Location = new System.Drawing.Point(0, 3);
+            this.dataInfoBox.Margin = new System.Windows.Forms.Padding(2);
+            this.dataInfoBox.Name = "dataInfoBox";
+            this.dataInfoBox.ReadOnly = true;
+            this.dataInfoBox.Size = new System.Drawing.Size(150, 23);
+            this.dataInfoBox.TabIndex = 3;
+            this.dataInfoBox.TabStop = false;
+            this.dataInfoBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.dataInfoBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.DataInfoBox_MouseClick);
+            this.dataInfoBox.LostFocus += new System.EventHandler(this.DataInfoBox_LostFocus);
             // 
             // OutList
             // 
             this.OutList.DataSource = null;
-            this.OutList.Font = new System.Drawing.Font("微软雅黑", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.OutList.Location = new System.Drawing.Point(0, 128);
+            this.OutList.Font = new System.Drawing.Font("微软雅黑", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.OutList.Location = new System.Drawing.Point(0, 106);
             this.OutList.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.OutList.Name = "OutList";
-            this.OutList.Size = new System.Drawing.Size(135, 21);
+            this.OutList.Size = new System.Drawing.Size(150, 24);
             this.OutList.TabIndex = 2;
             // 
-            // MaxValueBox
+            // maxValueBox
             // 
-            this.MaxValueBox.Font = new System.Drawing.Font("微软雅黑", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.MaxValueBox.FormattingEnabled = true;
-            this.MaxValueBox.Location = new System.Drawing.Point(0, 64);
-            this.MaxValueBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.MaxValueBox.Name = "MaxValueBox";
-            this.MaxValueBox.Size = new System.Drawing.Size(136, 24);
-            this.MaxValueBox.TabIndex = 1;
+            this.maxValueBox.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.maxValueBox.FormattingEnabled = true;
+            this.maxValueBox.Location = new System.Drawing.Point(0, 54);
+            this.maxValueBox.Margin = new System.Windows.Forms.Padding(2);
+            this.maxValueBox.Name = "maxValueBox";
+            this.maxValueBox.Size = new System.Drawing.Size(150, 25);
+            this.maxValueBox.TabIndex = 1;
             // 
             // MaxOperatorView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(407, 267);
+            this.ClientSize = new System.Drawing.Size(340, 207);
             this.ControlBox = false;
             this.Controls.Add(this.valuePanel);
             this.Controls.Add(this.keyPanel);
@@ -198,7 +198,7 @@
             this.Controls.Add(this.topPanel);
             this.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "MaxOperatorView";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -223,8 +223,8 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private UserControlDLL.ComCheckBoxList OutList;
-        private System.Windows.Forms.ComboBox MaxValueBox;
-        private System.Windows.Forms.TextBox DataInfoBox;
+        private System.Windows.Forms.ComboBox maxValueBox;
+        private System.Windows.Forms.TextBox dataInfoBox;
         private System.Windows.Forms.ToolTip toolTip1;
     }
 }
