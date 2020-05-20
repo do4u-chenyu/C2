e4d4f34798784282f4d62fe3d163b5bee4ef71ec
lu-qi123 <18705158378@163.com>
2020-05-20 11:44:22 +0800

    添加redo,undo框架,正在施工中

---
+++ Citta_T1/Core/UndoRedo/FixedCommandStack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Citta_T1.Core.UndoRedo
+{
+    // 固定大小的操作堆栈
+    // 要自行判断堆栈是否为空
+    class FixedCommandStack
+    {
+        private int capacity;
+        private LinkedList<ICommand> fixedStack;
+        public FixedCommandStack(int capacity = 100)
+        {
+            this.capacity = capacity;
+            fixedStack = new LinkedList<ICommand>();
+        }
+
+        public int Push(ICommand cmd)
+        {
+            if (fixedStack.Count == capacity)
+                fixedStack.RemoveLast();
+           
+            fixedStack.AddFirst(cmd);
+            return fixedStack.Count;
+        }
+
+        public ICommand Top()
+        {
+            return fixedStack.First();
+        }
+
+        public bool Empty()
+        {
+            return fixedStack.Count == 0;
+        }
+
+        public ICommand Pop()
+        {
+            ICommand cmd = Top();
+            fixedStack.RemoveFirst();
+            return cmd;
+        }
+    }
+}
