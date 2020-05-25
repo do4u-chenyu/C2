8b05ccdf63fd3e3b26102ab92bb2ccee0aca30d4
xiaobaibai <1309744250@qq.com>
2020-05-25 16:32:03 +0800

    增加视野框拖动框选状态自动消失设置

--- Citta_T1/Controls/Flow/NaviViewControl.cs
+++ Citta_T1/Controls/Flow/NaviViewControl.cs
@@ -91,7 +91,7 @@ namespace Citta_T1.Controls.Flow
 
             float factor = Global.GetCurrentDocument().ScreenFactor;
             Point mapOrigin = Global.GetCurrentDocument().MapOrigin;
-
+            
             int dx = Convert.ToInt32((startX - e.X ) * rate / factor);
             int dy = Convert.ToInt32((startY - e.Y ) * rate / factor);
             mapOrigin = new Point(mapOrigin.X + dx, mapOrigin.Y + dy);
@@ -105,7 +105,8 @@ namespace Citta_T1.Controls.Flow
             startX = e.X;
             startY = e.Y;
             Global.GetNaviViewControl().UpdateNaviView();
-            
+            Global.GetCanvasPanel().FrameWrapper.InitFrame();
+            Global.GetFlowControl().ResetStatus();
         }
 
         private void NaviViewControl_Paint(object sender, PaintEventArgs e)
