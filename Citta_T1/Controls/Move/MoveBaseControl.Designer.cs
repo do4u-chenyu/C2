39997adae0e6e6a1237761f11647cc6e2d5f0453
lu-qi123 <18705158378@163.com>
2020-05-29 22:23:02 +0800

    增加MoveBaseControl类，给三个Move算子提供基础服务

--- Citta_T1/Controls/Move/MoveBaseControl.Designer.cs
+++ Citta_T1/Controls/Move/MoveBaseControl.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace Citta_T1.Controls.Move
+﻿
+namespace Citta_T1.Controls.Move
 {
     partial class MoveBaseControl
     {
@@ -28,41 +29,101 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MoveBaseControl));
+            this.rightPictureBox = new System.Windows.Forms.PictureBox();
+            this.leftPictureBox = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.textBox = new System.Windows.Forms.TextBox();
-            this.txtButton = new System.Windows.Forms.Button();
+            this.helpToolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.txtButton = new Citta_T1.Controls.Common.NoFocusButton();
+            ((System.ComponentModel.ISupportInitialize)(this.rightPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.leftPictureBox)).BeginInit();
+            this.contextMenuStrip.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // rightPictureBox
+            // 
+            this.rightPictureBox.Location = new System.Drawing.Point(117, 5);
+            this.rightPictureBox.Name = "rightPictureBox";
+            this.rightPictureBox.Size = new System.Drawing.Size(14, 14);
+            this.rightPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.rightPictureBox.TabIndex = 1;
+            this.rightPictureBox.TabStop = false;
+            // 
+            // leftPicture
+            // 
+            this.leftPictureBox.Location = new System.Drawing.Point(11, 2);
+            this.leftPictureBox.Name = "leftPicture";
+            this.leftPictureBox.Size = new System.Drawing.Size(18, 22);
+            this.leftPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.leftPictureBox.TabIndex = 0;
+            this.leftPictureBox.TabStop = false;
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {});
+            this.contextMenuStrip.Name = "contextMenuStrip1";
+            this.contextMenuStrip.Size = new System.Drawing.Size(134, 148);
             // 
             // textBox
             // 
-            this.textBox.Location = new System.Drawing.Point(42, 2);
+            this.textBox.BackColor = System.Drawing.Color.White;
+            this.textBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox.Font = new System.Drawing.Font("微软雅黑", 9F);
+            this.textBox.Location = new System.Drawing.Point(31, 1);
             this.textBox.Name = "textBox";
-            this.textBox.Size = new System.Drawing.Size(21, 21);
-            this.textBox.TabIndex = 0;
+            this.textBox.ReadOnly = true;
+            this.textBox.Size = new System.Drawing.Size(60, 23);
+            this.textBox.TabIndex = 5;
+            this.textBox.Text = "连接算子";
+            this.textBox.Visible = false;
             // 
             // txtButton
             // 
-            this.txtButton.Location = new System.Drawing.Point(81, 2);
+            this.txtButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.txtButton.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtButton.FlatAppearance.BorderSize = 0;
+            this.txtButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.txtButton.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtButton.Location = new System.Drawing.Point(30, 2);
+            this.txtButton.Margin = new System.Windows.Forms.Padding(2);
             this.txtButton.Name = "txtButton";
-            this.txtButton.Size = new System.Drawing.Size(25, 21);
-            this.txtButton.TabIndex = 1;
-            this.txtButton.UseVisualStyleBackColor = true;
+            this.txtButton.Size = new System.Drawing.Size(60, 25);
+            this.txtButton.TabIndex = 8;
+            this.txtButton.UseVisualStyleBackColor = false;
             // 
-            // MoveBaseControl
+            // MoveOpControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AllowDrop = true;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ContextMenuStrip = this.contextMenuStrip;
             this.Controls.Add(this.txtButton);
             this.Controls.Add(this.textBox);
+            this.Controls.Add(this.rightPictureBox);
+            this.Controls.Add(this.leftPictureBox);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.Name = "MoveBaseControl";
-            this.Size = new System.Drawing.Size(150, 29);
+            this.Size = new System.Drawing.Size(169, 25);
+            ((System.ComponentModel.ISupportInitialize)(this.rightPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.leftPictureBox)).EndInit();
+            this.contextMenuStrip.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.PictureBox rightPictureBox;
+        private System.Windows.Forms.PictureBox leftPictureBox;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
         private System.Windows.Forms.TextBox textBox;
-        private System.Windows.Forms.Button txtButton;
+        private System.Windows.Forms.ToolTip helpToolTip;
+        private Citta_T1.Controls.Common.NoFocusButton txtButton;
     }
 }
