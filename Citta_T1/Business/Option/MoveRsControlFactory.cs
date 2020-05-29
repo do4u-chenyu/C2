aa5d246130cd6333389bf78bb7daaa5401fd6bef
刘晓凤 <lxfjiangnan@163.com>
2020-05-29 14:48:20 +0800

    结果算子重构bug修复

--- Citta_T1/Business/Option/MoveRsControlFactory.cs
+++ Citta_T1/Business/Option/MoveRsControlFactory.cs
@@ -23,52 +23,74 @@ namespace Citta_T1.Business.Option
             }
             return MoveRsControlFactoryInstance;
         }
-        private MoveRsControl NewMoveRsControl(MoveOpControl moc, List<string> columnName)
+        private MoveRsControl NewMoveRsControl(MoveOpControl moc, string resultFilePath, List<string> columns) 
         {
             //创建MoveRsControl控件
-            char seperator = '\t';
+            char separator = '\t';
             DSUtil.Encoding encoding = DSUtil.Encoding.UTF8;
             Point location = WorldBoundControl(moc);
             int id = Global.GetCurrentDocument().ElementCount;
             string createTime = DateTime.Now.ToString("yyyyMMdd_hhmmss");
-            string name = String.Format("L{0}_{1}",id, createTime);
             int sizeL = Global.GetCurrentDocument().WorldMap1.GetWmInfo().SizeLevel;
-            MoveRsControl mrc = Global.GetCanvasPanel().AddNewResult( name, sizeL,
-                                                                      location, seperator, encoding);
+            string path;
+            if (resultFilePath == string.Empty)
+                path = String.Format("L{0}_{1}.bcp", id, createTime);
+            else
+                path = resultFilePath;
+           string name = Path.GetFileNameWithoutExtension(path);
+            MoveRsControl mrc = Global.GetCanvasPanel().AddNewResult(
+                                name, sizeL,location, separator, encoding);
+
             //创建MoveRsControl的Xml文件
-            string path = BCPBuffer.GetInstance().CreateNewBCPFile(name + ".bcp", columnName);
+            if (resultFilePath == string.Empty)
+                path = BCPBuffer.GetInstance().CreateNewBCPFile(path, columns);
+
             mrc.FullFilePath = path;
             return mrc;
         }
         private Point WorldBoundControl(MoveOpControl moc)
         {
             /*
-             * 结果算子位置不超过地图大小
+             * 结果算子位置不超过地图右边界、下边界
              */
-            int x = moc.Location.X + moc.Width + 15;
-            int y = moc.Location.Y;
-            Point Pm = new Point(x,y);
+            
+            int rightBorder = 2000 - 2 * moc.Width;
+            int lowerBorder = 980 - moc.Height;
+            int interval = moc.Height + 5;
 
+            Point Pm = new Point(moc.Location.X + moc.Width + 25, moc.Location.Y);
             Point Pw = Global.GetCurrentDocument().WorldMap1.ScreenToWorld(Pm, true);
 
-            if (Pw.X < 20)
-            {
-                Pm.X = 20;
-            }
-            if (Pw.Y < 70)
-            {
-                Pm.Y = 70;
-            }
-            if (Pw.X > 2000 - moc.Width)
+            if (Pw.X > rightBorder)
             {
                 Pm.X = moc.Location.X;
-                Pm.Y = moc.Location.Y + moc.Height + 5;
+                Pm.Y = moc.Location.Y + interval;
             }
-            if (Pw.Y > 980 - moc.Height)
+            if (Pw.Y > lowerBorder)
             {
-                Pm.Y = moc.Location.Y - moc.Height - 5;
+                Pm.Y = moc.Location.Y - interval;
             }
             return Pm;
+        }
+        private void NewLineOpControlToRsControl(MoveOpControl moc,MoveRsControl mrc) 
+        {
+            int startX = moc.RectOut.Location.X + moc.Location.X;
+            int startY = moc.RectOut.Location.Y + moc.Location.Y;
+            int endX = mrc.Location.X + mrc.RectIn.Location.X;
+            int endY = mrc.Location.Y + mrc.RectIn.Location.Y;
+            PointF startPoint = new PointF(startX, startY);
+            PointF endPoint = new PointF(endX, endY);
+
+            Bezier line = new Bezier(startPoint, endPoint);
+            Global.GetCanvasPanel().RepaintObject(line);
+            ModelRelation newModelRelation = new ModelRelation(
+                                moc.ID, mrc.ID,
+                                startPoint,
+                                endPoint,
+                                0);
+            Global.GetCurrentDocument().AddModelRelation(newModelRelation);
+            moc.OutPinInit("lineExit");
+            mrc.rectInAdd(1);
         }
         public void CreateNewMoveRsControl(MoveOpControl moc, List<string> columnName)
         {
@@ -76,66 +98,21 @@ namespace Citta_T1.Business.Option
             /*
              * 创建新结果算子
              */
-            MoveRsControl mrc= NewMoveRsControl(moc, columnName);
+            MoveRsControl mrc= NewMoveRsControl(moc, string.Empty, columnName);
 
             /*
              * 1. 形成线。以OpCotrol的右针脚为起点，以RS的左针脚为起点，形成线段
              * 2. 控件绑定线。OpControl绑定线，RsControl绑定线
              */
 
-            PointF startPoint = new PointF(
-                   moc.RectOut.Location.X + moc.Location.X,
-                   moc.RectOut.Location.Y + moc.Location.Y
-                   );
-            PointF endPoint = new PointF(mrc.Location.X + mrc.RectIn.Location.X, mrc.Location.Y + mrc.RectIn.Location.Y);
-            Bezier line = new Bezier(startPoint, endPoint);
-            CanvasPanel canvas = Global.GetCanvasPanel();
+            NewLineOpControlToRsControl(moc, mrc);
 
-            canvas.RepaintObject(line);
-            ModelRelation newModelRelation = new ModelRelation(
-                                moc.ID, mrc.ID,
-                                new Point(moc.RectOut.Location.X + moc.Location.X, moc.RectOut.Location.Y + moc.Location.Y),
-                                new Point(mrc.RectIn.Location.X + mrc.Location.X, mrc.RectIn.Location.Y + mrc.Location.Y),
-                                0);
-            Global.GetCurrentDocument().AddModelRelation(newModelRelation);
-
-            moc.OutPinInit("lineExit");
-            mrc.rectInAdd(1);
-           
         }
-        public void CreateNewMoveRsControl(MoveOpControl moveOpControl, string path, char separator = '\t', DSUtil.Encoding encoding = DSUtil.Encoding.UTF8)
+        public void CreateNewMoveRsControl(MoveOpControl moc, string path)
         {
 
-            int x = moveOpControl.Location.X + moveOpControl.Width + 15;
-            int y = moveOpControl.Location.Y;
-            //string tmpBcpFileName = String.Format("L{0}_{1}.bcp", Global.GetCurrentDocument().ElementCount, DateTime.Now.ToString("yyyyMMdd_hhmmss"));
-            MoveRsControl mrc = Global.GetCanvasPanel().AddNewResult(
-                System.IO.Path.GetFileNameWithoutExtension(Path.GetFileNameWithoutExtension(path)), Global.GetCurrentDocument().WorldMap1.GetWmInfo().SizeLevel,
-                new Point(x, y), separator, encoding);
-            /*
-             * 1. 形成线。以OpCotrol的右针脚为起点，以RS的左针脚为起点，形成线段
-             * 2. 控件绑定线。OpControl绑定线，RsControl绑定线
-             */
-
-            PointF startPoint = new PointF(
-                   moveOpControl.RectOut.Location.X + moveOpControl.Location.X,
-                   moveOpControl.RectOut.Location.Y + moveOpControl.Location.Y
-                   );
-            PointF endPoint = new PointF(mrc.Location.X + mrc.RectIn.Location.X, mrc.Location.Y + mrc.RectIn.Location.Y);
-            Bezier line = new Bezier(startPoint, endPoint);
-            CanvasPanel canvas = Global.GetCanvasPanel();
-
-            canvas.RepaintObject(line);
-            ModelRelation newModelRelation = new ModelRelation(
-                                moveOpControl.ID, mrc.ID,
-                                new Point(moveOpControl.RectOut.Location.X + moveOpControl.Location.X, moveOpControl.RectOut.Location.Y + moveOpControl.Location.Y),
-                                new Point(mrc.RectIn.Location.X + mrc.Location.X, mrc.RectIn.Location.Y + mrc.Location.Y),
-                                0);
-            Global.GetCurrentDocument().AddModelRelation(newModelRelation);
-
-            moveOpControl.OutPinInit("lineExit");
-            mrc.rectInAdd(1);
-            mrc.FullFilePath = path;
+            MoveRsControl mrc = NewMoveRsControl(moc, path, null);
+            NewLineOpControlToRsControl(moc, mrc);
         }
     }
 }
