287344ade5e3d1218cb7839a716161608fbcae0a
lu-qi123 <18705158378@163.com>
2020-06-06 19:07:37 +0800

    简化random窗口

--- Citta_T1/OperatorViews/RandomOperatorView.Designer.cs
+++ Citta_T1/OperatorViews/RandomOperatorView.Designer.cs
@@ -3,11 +3,6 @@
     partial class RandomOperatorView
     {
         /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
@@ -28,20 +23,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.topPanel = new System.Windows.Forms.Panel();
             this.bottomPanel = new System.Windows.Forms.Panel();
-            this.cancelButton = new System.Windows.Forms.Button();
-            this.confirmButton = new System.Windows.Forms.Button();
             this.keyPanel = new System.Windows.Forms.Panel();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.valuePanel = new System.Windows.Forms.Panel();
             this.randomNumBox = new System.Windows.Forms.TextBox();
-            this.dataInfoBox = new System.Windows.Forms.TextBox();
-            this.outList = new Citta_T1.Controls.Common.ComCheckBoxList();
-            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.bottomPanel.SuspendLayout();
             this.keyPanel.SuspendLayout();
             this.valuePanel.SuspendLayout();
@@ -78,7 +67,6 @@
             this.cancelButton.TabIndex = 1;
             this.cancelButton.Text = "取消";
             this.cancelButton.UseVisualStyleBackColor = true;
-            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
             // 
             // confirmButton
             // 
@@ -93,7 +81,6 @@
             this.confirmButton.TabIndex = 0;
             this.confirmButton.Text = "确认";
             this.confirmButton.UseVisualStyleBackColor = false;
-            this.confirmButton.Click += new System.EventHandler(this.ConfirmButton_Click);
             // 
             // keyPanel
             // 
@@ -143,8 +130,8 @@
             // valuePanel
             // 
             this.valuePanel.Controls.Add(this.randomNumBox);
-            this.valuePanel.Controls.Add(this.dataInfoBox);
-            this.valuePanel.Controls.Add(this.outList);
+            this.valuePanel.Controls.Add(this.dataSourceTB0);
+            this.valuePanel.Controls.Add(this.outListCCBL0);
             this.valuePanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.valuePanel.Location = new System.Drawing.Point(116, 21);
             this.valuePanel.Margin = new System.Windows.Forms.Padding(2);
@@ -164,26 +151,24 @@
             // 
             // dataInfoBox
             // 
-            this.dataInfoBox.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.dataInfoBox.Location = new System.Drawing.Point(0, 1);
-            this.dataInfoBox.Margin = new System.Windows.Forms.Padding(2);
-            this.dataInfoBox.Name = "dataInfoBox";
-            this.dataInfoBox.ReadOnly = true;
-            this.dataInfoBox.Size = new System.Drawing.Size(150, 23);
-            this.dataInfoBox.TabIndex = 3;
-            this.dataInfoBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.dataInfoBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.DataInfoBox_MouseClick);
-            this.dataInfoBox.LostFocus += new System.EventHandler(this.DataInfoBox_LostFocus);
+            this.dataSourceTB0.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.dataSourceTB0.Location = new System.Drawing.Point(0, 1);
+            this.dataSourceTB0.Margin = new System.Windows.Forms.Padding(2);
+            this.dataSourceTB0.Name = "dataInfoBox";
+            this.dataSourceTB0.ReadOnly = true;
+            this.dataSourceTB0.Size = new System.Drawing.Size(150, 23);
+            this.dataSourceTB0.TabIndex = 3;
+            this.dataSourceTB0.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // outList
             // 
-            this.outList.DataSource = null;
-            this.outList.Font = new System.Drawing.Font("微软雅黑", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.outList.Location = new System.Drawing.Point(0, 106);
-            this.outList.Margin = new System.Windows.Forms.Padding(0);
-            this.outList.Name = "outList";
-            this.outList.Size = new System.Drawing.Size(150, 24);
-            this.outList.TabIndex = 2;
+            this.outListCCBL0.DataSource = null;
+            this.outListCCBL0.Font = new System.Drawing.Font("微软雅黑", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.outListCCBL0.Location = new System.Drawing.Point(0, 106);
+            this.outListCCBL0.Margin = new System.Windows.Forms.Padding(0);
+            this.outListCCBL0.Name = "outList";
+            this.outListCCBL0.Size = new System.Drawing.Size(150, 24);
+            this.outListCCBL0.TabIndex = 2;
             // 
             // RandomOperatorView
             // 
@@ -211,19 +196,13 @@
         }
 
         #endregion
-
         private System.Windows.Forms.Panel topPanel;
         private System.Windows.Forms.Panel bottomPanel;
         private System.Windows.Forms.Panel keyPanel;
         private System.Windows.Forms.Panel valuePanel;
-        private System.Windows.Forms.Button cancelButton;
-        private System.Windows.Forms.Button confirmButton;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
-        private Citta_T1.Controls.Common.ComCheckBoxList outList;
         private System.Windows.Forms.TextBox randomNumBox;
-        private System.Windows.Forms.TextBox dataInfoBox;
-        private System.Windows.Forms.ToolTip toolTip1;
     }
 }
--- Citta_T1/OperatorViews/RandomOperatorView.cs
+++ Citta_T1/OperatorViews/RandomOperatorView.cs
@@ -1,45 +1,31 @@
 ﻿using Citta_T1.Business.Model;
 using Citta_T1.Business.Option;
-using Citta_T1.Controls.Move.Dt;
 using Citta_T1.Controls.Move.Op;
 using Citta_T1.Core;
+using Citta_T1.OperatorViews.Base;
 using Citta_T1.Utils;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace Citta_T1.OperatorViews
 {
 
-    public partial class RandomOperatorView : Form
+    public partial class RandomOperatorView : BaseOperatorView
     {
-        private MoveOpControl opControl;
-        private string dataPath;
         private string oldRandomNum;
-        private List<int> oldOutList;
-        private string oldOptionDict;
-        private string[] columnName;
-        private List<string> selectColumn;
-        private List<string> oldColumnName;
-        private static LogUtil log = LogUtil.GetInstance("RandomOperatorView");
-        private OptionInfoCheck optionInfoCheck;
-        public RandomOperatorView(MoveOpControl opControl)
+        private List<string> oldColumnsName = new List<string>();
+
+        public RandomOperatorView(MoveOpControl opControl) : base(opControl)
         {
             InitializeComponent();
-            this.optionInfoCheck = new OptionInfoCheck();
-            this.dataPath = String.Empty;
-            this.oldOutList = new List<int>();
-            oldColumnName = new List<string>();
-            this.opControl = opControl;
             InitOptionInfo();
             LoadOption();
             this.oldRandomNum = this.randomNumBox.Text;
-            this.oldOptionDict = string.Join(",", this.opControl.Option.OptionDict.ToList());
 
-            SetTextBoxName(this.dataInfoBox);
+            SetTextBoxName(this.dataSourceTB0);
         }
         #region 初始化配置
         private void InitOptionInfo()
@@ -61,70 +47,45 @@ namespace Citta_T1.OperatorViews
             {
                 if (me.ID == startID)
                 {
-                    this.dataPath = me.FullFilePath;
+                    this.dataSourceFFP0 = me.FullFilePath;
                     separator = me.Separator;
                     //设置数据信息选项
-                    this.dataInfoBox.Text = Path.GetFileNameWithoutExtension(this.dataPath);
-                    this.toolTip1.SetToolTip(this.dataInfoBox, this.dataInfoBox.Text);
+                    this.dataSourceTB0.Text = Path.GetFileNameWithoutExtension(this.dataSourceFFP0);
+                    this.toolTip1.SetToolTip(this.dataSourceTB0, this.dataSourceFFP0);
                     encoding = me.Encoding.ToString();
                     break;
                 }
             }
-            if (this.dataPath != String.Empty)
-                SetOption(this.dataPath, this.dataInfoBox.Text, encoding, separator);
+            if (this.dataSourceFFP0 != String.Empty)
+                SetOption(this.dataSourceFFP0, this.dataSourceTB0.Text, encoding, separator);
 
         }
         private void SetOption(string path, string dataName, string encoding, char separator)
         {
 
             BcpInfo bcpInfo = new BcpInfo(path, dataName, ElementType.Empty, OpUtil.EncodingEnum(encoding), separator);
-            this.columnName = bcpInfo.ColumnArray;
-            foreach (string name in columnName)
+            this.nowColumnsName0 = bcpInfo.ColumnArray;
+            foreach (string name in nowColumnsName0)
             {
-                this.outList.AddItems(name);
+                this.outListCCBL0.AddItems(name);
             }
 
-            this.opControl.FirstDataSourceColumns = this.columnName;
-            this.opControl.Option.SetOption("columnname0", String.Join("\t", this.columnName));
-        }
-
-
-        private void SetTextBoxName(TextBox textBox)
-        {
-            string dataName = textBox.Text;
-            int maxLength = 18;
-            MatchCollection chs = Regex.Matches(dataName, "[\u4E00-\u9FA5]");
-            int sumcount = chs.Count * 2;
-            int sumcountDigit = Regex.Matches(dataName, "[a-zA-Z0-9]").Count;
-
-            //防止截取字符串时中文乱码
-            foreach (Match mc in chs)
-            {
-                if (dataName.IndexOf(mc.ToString()) == maxLength)
-                {
-                    maxLength -= 1;
-                    break;
-                }
-            }
-
-            if (sumcount + sumcountDigit > maxLength)
-            {
-                textBox.Text = ConvertUtil.GB2312.GetString(ConvertUtil.GB2312.GetBytes(dataName), 0, maxLength) + "...";
-            }
+            this.opControl.FirstDataSourceColumns = this.nowColumnsName0;
+            this.opControl.Option.SetOption("columnname0", String.Join("\t", this.nowColumnsName0));
         }
         #endregion
         #region 配置信息的保存与加载
         private void SaveOption()
         {
             this.opControl.Option.SetOption("randomnum", this.randomNumBox.Text);
-            List<int> checkIndexs = this.outList.GetItemCheckIndex();
-            List<int> outIndexs = new List<int>(this.oldOutList);
+            List<int> checkIndexs = this.outListCCBL0.GetItemCheckIndex();
+            List<int> outIndexs = new List<int>(this.oldOutList0);
             Global.GetOptionDao().UpdateOutputCheckIndexs(checkIndexs, outIndexs);
             string outField = string.Join("\t", outIndexs);
             this.opControl.Option.SetOption("outfield", outField);
 
             ElementStatus oldStatus = this.opControl.Status;
-            if (this.oldOptionDict != string.Join(",", this.opControl.Option.OptionDict.ToList()))
+            if (this.oldOptionDictStr != string.Join(",", this.opControl.Option.OptionDict.ToList()))
                 this.opControl.Status = ElementStatus.Ready;
 
             if (oldStatus == ElementStatus.Done && this.opControl.Status == ElementStatus.Ready)
@@ -136,20 +97,20 @@ namespace Citta_T1.OperatorViews
         {
             if (!string.IsNullOrEmpty( this.opControl.Option.GetOption("randomnum")))
                 this.randomNumBox.Text = this.opControl.Option.GetOption("randomnum");
-            if (!Global.GetOptionDao().IsCleanOption(this.opControl, this.columnName, "outfield"))
+            if (!Global.GetOptionDao().IsCleanOption(this.opControl, this.nowColumnsName0, "outfield"))
             {
                 string[] checkIndexs = this.opControl.Option.GetOptionSplit("outfield");
                 int[] indexs = Array.ConvertAll<string, int>(checkIndexs, int.Parse);
-                this.oldOutList = indexs.ToList();
-                this.outList.LoadItemCheckIndex(indexs);
+                this.oldOutList0 = indexs.ToList();
+                this.outListCCBL0.LoadItemCheckIndex(indexs);
                 foreach (int index in indexs)
-                    this.oldColumnName.Add(this.outList.Items[index].ToString());
+                    this.oldColumnsName.Add(this.outListCCBL0.Items[index].ToString());
             }
 
         }
         #endregion
         #region 添加取消
-        private void ConfirmButton_Click(object sender, EventArgs e)
+        protected override void ConfirmButton_Click(object sender, EventArgs e)
         {
             //未设置字段警告
             if (this.randomNumBox.Text == String.Empty)
@@ -157,25 +118,25 @@ namespace Citta_T1.OperatorViews
                 MessageBox.Show("随机条数字段不能为空,请输入一个整数");
                 return;
             }
-            if (this.outList.GetItemCheckIndex().Count == 0)
+            if (this.outListCCBL0.GetItemCheckIndex().Count == 0)
             {
                 MessageBox.Show("请选择算子要输出的字段");
                 return;
             }
             this.DialogResult = DialogResult.OK;
-            if (this.dataInfoBox.Text == String.Empty) return;
+            if (this.dataSourceTB0.Text == String.Empty) return;
             SaveOption();
             //内容修改，引起文档dirty
             if (this.oldRandomNum != this.randomNumBox.Text)
                 Global.GetMainForm().SetDocumentDirty();
-            else if (String.Join(",", this.oldOutList) != this.opControl.Option.GetOption("outfield"))
+            else if (String.Join(",", this.oldOutList0) != this.opControl.Option.GetOption("outfield"))
                 Global.GetMainForm().SetDocumentDirty();
             //生成结果控件,创建relation,bcp结果文件
-            this.selectColumn = this.outList.GetItemCheckText();
+            this.selectedColumns = this.outListCCBL0.GetItemCheckText();
             ModelElement resultElement = Global.GetCurrentDocument().SearchResultElementByOpID(this.opControl.ID);
             if (resultElement == ModelElement.Empty)
             {
-                MoveRsControlFactory.GetInstance().CreateNewMoveRsControl(this.opControl, this.selectColumn);
+                MoveRsControlFactory.GetInstance().CreateNewMoveRsControl(this.opControl, this.selectedColumns);
                 return;
             }
             // 对应的结果文件置脏
@@ -183,32 +144,11 @@ namespace Citta_T1.OperatorViews
             //输出变化，重写BCP文件
             List<string> outName = new List<string>();
             foreach (string index in this.opControl.Option.GetOptionSplit("outfield"))
-            { outName.Add(this.columnName[Convert.ToInt32(index)]); }
-            if (String.Join(",", this.oldOutList) != this.opControl.Option.GetOption("outfield"))
-                Global.GetOptionDao().DoOutputCompare(this.oldColumnName, outName, this.opControl.ID);
-        }
-
-        private void cancelButton_Click(object sender, EventArgs e)
-        {
-            this.DialogResult = DialogResult.Cancel;
-            Close();
+            { outName.Add(this.nowColumnsName0[Convert.ToInt32(index)]); }
+            if (String.Join(",", this.oldOutList0) != this.opControl.Option.GetOption("outfield"))
+                Global.GetOptionDao().DoOutputCompare(this.oldColumnsName, outName, this.opControl.ID);
         }
         #endregion
-
-        private void DataInfoBox_MouseClick(object sender, MouseEventArgs e)
-        {
-            this.dataInfoBox.Text = Path.GetFileNameWithoutExtension(this.dataPath);
-        }
-
-        private void DataInfoBox_LostFocus(object sender, EventArgs e)
-        {
-            SetTextBoxName(this.dataInfoBox);
-        }
-
-        private void RandomNumBox_KeyPress(object sender, KeyPressEventArgs e)
-        {
-
-        }
 
         private void RandomNumBox_Leave(object sender, EventArgs e)
         {
