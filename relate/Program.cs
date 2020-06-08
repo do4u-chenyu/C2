b990ce92e41b719515843971cc633151baa3d04d
phoenix1214 <273048269@qq.com>
2020-06-08 16:24:06 +0800

    新增cat_xls\relate项目

---
+++ relate/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+
+namespace relate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args != null && args.Length == 9)
+                {
+                    DealRelate dr = new DealRelate(args);
+                    dr.RelateTwoTables();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
