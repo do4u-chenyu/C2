1c4302d4cf3df89586e0c8ad317eecf3aa4f1a77
lu-qi123 <18705158378@163.com>
2020-06-09 17:30:54 +0800

    简化base配置窗口的校验函数逻辑

--- Citta_T1/OperatorViews/Base/BaseOperatorView.cs
+++ Citta_T1/OperatorViews/Base/BaseOperatorView.cs
@@ -151,41 +151,26 @@ namespace Citta_T1.OperatorViews.Base
 
         protected void Control_Leave(object sender, EventArgs e)
         {
-            List<string> columnName = new List<string>();
-            foreach (var item in (sender as ComboBox).Items)
+            ComboBox comboBox = sender as ComboBox;
+            if (comboBox.Items.Count == 0 || String.IsNullOrEmpty(comboBox.Text)) return;
+            if (!comboBox.Items.Contains(comboBox.Text))
             {
-                columnName.Add(item.ToString());
+                comboBox.Text = String.Empty;
+                MessageBox.Show("未输入正确列名，请从下拉列表中选择正确列名");
             }
-            IsIllegalInputName((sender as ComboBox), columnName.ToArray(), (sender as ComboBox).Text);
         }
         protected void Control_KeyUp(object sender, KeyEventArgs e)
         {
-            List<string> columnName = new List<string>();
-            foreach (var item in (sender as ComboBox).Items)
-            {
-                columnName.Add(item.ToString());
-            }
             if (e.KeyCode == Keys.Enter)
-                IsIllegalInputName((sender as ComboBox), columnName.ToArray(), (sender as ComboBox).Text);
-        }
-
-        protected void IsIllegalInputName(Control control, String[] columnName, String name)
-        {
-            if (columnName.Count() == 0 || name == "") return;
-            if (!columnName.Contains(name))
-            {
-                control.Text = "";
-                MessageBox.Show("未输入正确列名，请从下拉列表中选择正确列名");
-            }
+                Control_Leave(sender, e);
         }
 
         protected void IsIllegalCharacter(object sender, EventArgs e)
         {
-
             if ((sender as TextBox).Text.Contains("\\t"))
             {
-                (sender as TextBox).Text = "";
-                MessageBox.Show("输入非法字'\\t'，请重新输入过滤条件");
+                (sender as TextBox).Text = String.Empty;
+                MessageBox.Show("输入非法字TAB键，请重新输入过滤条件");
             }
         }
 
