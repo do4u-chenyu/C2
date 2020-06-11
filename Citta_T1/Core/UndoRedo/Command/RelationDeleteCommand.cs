f54802b3f3196d96579f12a933a78c6e1702e35b
Darkxiete <470058433@qq.com>
2020-06-11 09:26:56 +0800

    新增关系删除的UndoRedo

--- Citta_T1/Core/UndoRedo/Command/RelationDeleteCommand.cs
+++ Citta_T1/Core/UndoRedo/Command/RelationDeleteCommand.cs
@@ -2,16 +2,27 @@
 
 namespace Citta_T1.Core.UndoRedo.Command
 {
+
     class RelationDeleteCommand : ICommand
     {
+        private readonly int startID;
+        private readonly int endID;
+        public RelationDeleteCommand(int startID, int endID)
+        {
+            this.startID = startID;
+            this.endID = endID;
+        }
         public bool Redo()
         {
-            throw new NotImplementedException();
+            Global.GetCanvasPanel().DeleteRelationByCtrID(this.startID, this.endID);
+            Global.GetCanvasPanel().Invalidate(false);
+            return true;
         }
 
         public bool Undo()
         {
-            throw new NotImplementedException();
+            Global.GetCanvasPanel().AddNewRelationByCtrID(this.startID, this.endID);
+            return true;
         }
     }
 }
