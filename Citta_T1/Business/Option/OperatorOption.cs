8d24b344aeb306768fca071d29b1efbf45226c3a
刘晓凤 <lxfjiangnan@163.com>
2020-07-16 09:48:44 +0800

    异常情况check函数功能完善

--- Citta_T1/Business/Option/OperatorOption.cs
+++ Citta_T1/Business/Option/OperatorOption.cs
@@ -79,7 +79,8 @@ namespace Citta_T1.Business.Option
 
                 [ElementSubType.FreqOperator] = new RegisterInfo[]       { new RegisterInfo("outfield0", TypeCode.Int32, -1) },
 
-                [ElementSubType.GroupOperator] = new RegisterInfo[]      { new RegisterInfo("factor0", TypeCode.Int32, new int[] { -1 }),
+                [ElementSubType.GroupOperator] = new RegisterInfo[]      { new RegisterInfo("outfield0", TypeCode.Int32, -1),
+                                                                           new RegisterInfo("factor0", TypeCode.Int32, new int[] { -1 }),
                                                                            new RegisterInfo("factorH", TypeCode.Int32, new int[] { -1 })},
 
                 [ElementSubType.KeywordOperator] = new RegisterInfo[]    { new RegisterInfo("outfield0", TypeCode.Int32, -1),
@@ -308,7 +309,7 @@ namespace Citta_T1.Business.Option
                 if (ConvertUtil.IsInt(num))
                     nums.Add(int.Parse(num));
             }
-            return nums.Max();
+            return nums.Max() + 1;
         }
         private bool CheckFactorNonInt(String[] itemList, int[] maxIndexs)
         {
