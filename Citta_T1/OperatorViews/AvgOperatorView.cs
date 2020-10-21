fc8d3a8284910368a9114eedd0d2f4e92452d0da
晨 清 <1261921617@qq.com>
2020-10-21 16:48:42 +0800

    {平均值和关联算子修改}

--- Citta_T1/OperatorViews/AvgOperatorView.cs
+++ Citta_T1/OperatorViews/AvgOperatorView.cs
@@ -71,5 +71,10 @@ namespace C2.OperatorViews
  
         }
         #endregion
+
+        private void confirmButton_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
--- Citta_T1/OperatorViews/CollideOperatorView.Designer.cs
+++ Citta_T1/OperatorViews/CollideOperatorView.Designer.cs
@@ -30,112 +30,120 @@
             this.button1 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.bottomPanel.SuspendLayout();
-            this.keyPanel.SuspendLayout();
             this.valuePanel.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataSourceTB1
             // 
-            this.dataSourceTB1.Location = new System.Drawing.Point(187, 2);
-            this.dataSourceTB1.Size = new System.Drawing.Size(144, 29);
+            this.dataSourceTB1.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.dataSourceTB1.Location = new System.Drawing.Point(329, 6);
+            this.dataSourceTB1.Size = new System.Drawing.Size(144, 28);
             this.dataSourceTB1.TabIndex = 10;
             this.dataSourceTB1.Visible = true;
             // 
             // dataSourceTB0
             // 
-            this.dataSourceTB0.Location = new System.Drawing.Point(4, 2);
+            this.dataSourceTB0.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.dataSourceTB0.Location = new System.Drawing.Point(146, 6);
+            this.dataSourceTB0.Size = new System.Drawing.Size(144, 28);
             this.dataSourceTB0.TabIndex = 9;
             // 
             // cancelButton
             // 
             this.cancelButton.FlatAppearance.BorderSize = 0;
-            this.cancelButton.Location = new System.Drawing.Point(473, 19);
+            this.cancelButton.Location = new System.Drawing.Point(429, 13);
             this.cancelButton.Size = new System.Drawing.Size(63, 27);
             // 
             // confirmButton
             // 
             this.confirmButton.FlatAppearance.BorderSize = 0;
-            this.confirmButton.Location = new System.Drawing.Point(370, 19);
+            this.confirmButton.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.confirmButton.Location = new System.Drawing.Point(329, 13);
             this.confirmButton.Size = new System.Drawing.Size(60, 27);
+            this.confirmButton.UseVisualStyleBackColor = true;
             // 
             // outListCCBL0
             // 
-            this.outListCCBL0.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.outListCCBL0.Location = new System.Drawing.Point(4, 263);
-            this.outListCCBL0.Size = new System.Drawing.Size(144, 28);
+            this.outListCCBL0.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.outListCCBL0.Location = new System.Drawing.Point(146, 53);
+            this.outListCCBL0.Size = new System.Drawing.Size(144, 26);
             this.outListCCBL0.TabIndex = 8;
             // 
             // comboBox0
             // 
+            this.comboBox0.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.comboBox0.Location = new System.Drawing.Point(2, 2);
+            this.comboBox0.Size = new System.Drawing.Size(86, 26);
             // 
             // comboBox1
             // 
+            this.comboBox1.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.comboBox1.Location = new System.Drawing.Point(92, 2);
+            this.comboBox1.Size = new System.Drawing.Size(86, 26);
             this.comboBox1.Visible = true;
             // 
             // topPanel
             // 
+            this.topPanel.BackColor = System.Drawing.SystemColors.Window;
             this.topPanel.Size = new System.Drawing.Size(539, 37);
             // 
             // bottomPanel
             // 
-            this.bottomPanel.Location = new System.Drawing.Point(0, 315);
-            this.bottomPanel.Size = new System.Drawing.Size(539, 64);
-            // 
-            // keyPanel
-            // 
-            this.keyPanel.Controls.Add(this.label3);
-            this.keyPanel.Controls.Add(this.label2);
-            this.keyPanel.Controls.Add(this.label1);
-            this.keyPanel.Location = new System.Drawing.Point(0, 37);
-            this.keyPanel.Size = new System.Drawing.Size(116, 278);
-            this.keyPanel.Controls.SetChildIndex(this.label1, 0);
-            this.keyPanel.Controls.SetChildIndex(this.label2, 0);
-            this.keyPanel.Controls.SetChildIndex(this.label3, 0);
+            this.bottomPanel.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.bottomPanel.Location = new System.Drawing.Point(0, 336);
+            this.bottomPanel.Size = new System.Drawing.Size(539, 43);
             // 
             // valuePanel
             // 
+            this.valuePanel.BackColor = System.Drawing.SystemColors.Window;
+            this.valuePanel.Controls.Add(this.label3);
+            this.valuePanel.Controls.Add(this.label2);
+            this.valuePanel.Controls.Add(this.label1);
             this.valuePanel.Controls.Add(this.dataSourceTB1);
             this.valuePanel.Controls.Add(this.dataSourceTB0);
             this.valuePanel.Controls.Add(this.outListCCBL0);
             this.valuePanel.Controls.Add(this.tableLayoutPanel2);
             this.valuePanel.Controls.Add(this.panel1);
-            this.valuePanel.Location = new System.Drawing.Point(116, 37);
-            this.valuePanel.Size = new System.Drawing.Size(423, 278);
+            this.valuePanel.Location = new System.Drawing.Point(0, 37);
+            this.valuePanel.Size = new System.Drawing.Size(539, 299);
             this.valuePanel.Controls.SetChildIndex(this.panel1, 0);
             this.valuePanel.Controls.SetChildIndex(this.tableLayoutPanel2, 0);
             this.valuePanel.Controls.SetChildIndex(this.outListCCBL0, 0);
             this.valuePanel.Controls.SetChildIndex(this.dataSourceTB0, 0);
             this.valuePanel.Controls.SetChildIndex(this.dataSourceTB1, 0);
+            this.valuePanel.Controls.SetChildIndex(this.label1, 0);
+            this.valuePanel.Controls.SetChildIndex(this.label2, 0);
+            this.valuePanel.Controls.SetChildIndex(this.label3, 0);
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(32, 2);
-            this.label1.Size = new System.Drawing.Size(123, 30);
+            this.label1.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(57, 11);
+            this.label1.Size = new System.Drawing.Size(85, 19);
+            this.label1.Text = "数据信息";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label3.Location = new System.Drawing.Point(32, 261);
+            this.label3.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(57, 55);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(123, 30);
+            this.label3.Size = new System.Drawing.Size(85, 19);
             this.label3.TabIndex = 0;
-            this.label3.Text = "输出字段：";
+            this.label3.Text = "输出字段";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label2.Location = new System.Drawing.Point(32, 59);
+            this.label2.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(57, 100);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(123, 30);
+            this.label2.Size = new System.Drawing.Size(85, 19);
             this.label2.TabIndex = 0;
-            this.label2.Text = "碰撞条件：";
+            this.label2.Text = "碰撞条件";
             // 
             // tableLayoutPanel2
             // 
@@ -146,7 +154,7 @@
             this.tableLayoutPanel2.Controls.Add(this.button1, 2, 0);
             this.tableLayoutPanel2.Controls.Add(this.comboBox0, 0, 0);
             this.tableLayoutPanel2.Controls.Add(this.comboBox1, 1, 0);
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(95, 54);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(146, 94);
             this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(2);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 1;
@@ -160,7 +168,7 @@
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.BackColor = System.Drawing.SystemColors.Window;
             this.button1.FlatAppearance.BorderColor = System.Drawing.SystemColors.Control;
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.Control;
@@ -172,12 +180,12 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(34, 24);
             this.button1.TabIndex = 5;
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // panel1
             // 
             this.panel1.AutoScroll = true;
-            this.panel1.Location = new System.Drawing.Point(4, 87);
+            this.panel1.Location = new System.Drawing.Point(54, 126);
             this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(419, 160);
@@ -189,7 +197,6 @@
             this.ClientSize = new System.Drawing.Size(539, 379);
             this.ControlBox = true;
             this.Controls.Add(this.valuePanel);
-            this.Controls.Add(this.keyPanel);
             this.Controls.Add(this.bottomPanel);
             this.Controls.Add(this.topPanel);
             this.Cursor = System.Windows.Forms.Cursors.Default;
@@ -203,8 +210,6 @@
             this.ShowIcon = true;
             this.Text = "碰撞算子设置";
             this.bottomPanel.ResumeLayout(false);
-            this.keyPanel.ResumeLayout(false);
-            this.keyPanel.PerformLayout();
             this.valuePanel.ResumeLayout(false);
             this.valuePanel.PerformLayout();
             this.tableLayoutPanel2.ResumeLayout(false);
