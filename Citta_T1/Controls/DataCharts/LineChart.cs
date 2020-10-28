97f935001276b079682376ac3c7d13380fcffdb2
smallWhite <1309744250@qq.com>
2020-10-28 16:41:26 +0800

    折线图制作中

---
+++ Citta_T1/Controls/DataCharts/LineChart.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C2.Controls.DataCharts
+{
+    public partial class LineChart : UserControl
+    {
+        public LineChart()
+        {
+            InitializeComponent();
+        }
+        void InitChart()
+        {
+
+        }
+
+
+    }
+}
