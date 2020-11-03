a7b123f06be59fcf465a4d071640884d648cc966
晨 清 <1261921617@qq.com>
2020-11-03 16:59:46 +0800

    {所有算子界面修改提交}

--- Citta_T1/Dialogs/C1OperatorViews/GroupOperatorView.Designer.cs
+++ Citta_T1/Dialogs/C1OperatorViews/GroupOperatorView.Designer.cs
@@ -44,6 +44,9 @@
             this.panel4 = new System.Windows.Forms.Panel();
             this.lable_line1 = new System.Windows.Forms.Label();
             this.lable_line2 = new System.Windows.Forms.Label();
+            this.label_line3 = new System.Windows.Forms.Label();
+            this.label_line4 = new System.Windows.Forms.Label();
+            this.label_line5 = new System.Windows.Forms.Label();
             this.bottomPanel.SuspendLayout();
             this.valuePanel.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
@@ -55,39 +58,42 @@
             // 
             // dataSourceTB0
             // 
-            this.dataSourceTB0.Location = new System.Drawing.Point(93, 36);
+            this.dataSourceTB0.Location = new System.Drawing.Point(76, 34);
             this.dataSourceTB0.Size = new System.Drawing.Size(150, 28);
             this.dataSourceTB0.TabIndex = 9;
             // 
             // cancelButton
             // 
             this.cancelButton.FlatAppearance.BorderSize = 0;
-            this.cancelButton.Location = new System.Drawing.Point(442, 11);
+            this.cancelButton.Location = new System.Drawing.Point(313, 11);
             this.cancelButton.Size = new System.Drawing.Size(63, 27);
             // 
             // confirmButton
             // 
             this.confirmButton.FlatAppearance.BorderSize = 0;
             this.confirmButton.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.confirmButton.Location = new System.Drawing.Point(341, 11);
+            this.confirmButton.Location = new System.Drawing.Point(212, 11);
             this.confirmButton.Size = new System.Drawing.Size(60, 27);
             // 
             // comboBox0
             // 
             this.comboBox0.Location = new System.Drawing.Point(2, 2);
-            this.comboBox0.Size = new System.Drawing.Size(134, 26);
+            this.comboBox0.Size = new System.Drawing.Size(146, 26);
             // 
             // topPanel
             // 
-            this.topPanel.Size = new System.Drawing.Size(525, 10);
+            this.topPanel.Size = new System.Drawing.Size(402, 10);
             // 
             // bottomPanel
             // 
-            this.bottomPanel.Location = new System.Drawing.Point(0, 402);
-            this.bottomPanel.Size = new System.Drawing.Size(525, 49);
+            this.bottomPanel.Location = new System.Drawing.Point(0, 407);
+            this.bottomPanel.Size = new System.Drawing.Size(402, 49);
             // 
             // valuePanel
             // 
+            this.valuePanel.Controls.Add(this.label_line5);
+            this.valuePanel.Controls.Add(this.label_line4);
+            this.valuePanel.Controls.Add(this.label_line3);
             this.valuePanel.Controls.Add(this.lable_line2);
             this.valuePanel.Controls.Add(this.lable_line1);
             this.valuePanel.Controls.Add(this.label5);
@@ -101,7 +107,7 @@
             this.valuePanel.Controls.Add(this.dataSourceTB0);
             this.valuePanel.Controls.Add(this.panel1);
             this.valuePanel.Location = new System.Drawing.Point(0, 10);
-            this.valuePanel.Size = new System.Drawing.Size(525, 392);
+            this.valuePanel.Size = new System.Drawing.Size(402, 397);
             this.valuePanel.Controls.SetChildIndex(this.panel1, 0);
             this.valuePanel.Controls.SetChildIndex(this.dataSourceTB0, 0);
             this.valuePanel.Controls.SetChildIndex(this.panel2, 0);
@@ -114,6 +120,9 @@
             this.valuePanel.Controls.SetChildIndex(this.label5, 0);
             this.valuePanel.Controls.SetChildIndex(this.lable_line1, 0);
             this.valuePanel.Controls.SetChildIndex(this.lable_line2, 0);
+            this.valuePanel.Controls.SetChildIndex(this.label_line3, 0);
+            this.valuePanel.Controls.SetChildIndex(this.label_line4, 0);
+            this.valuePanel.Controls.SetChildIndex(this.label_line5, 0);
             // 
             // label1
             // 
@@ -125,7 +134,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label3.Location = new System.Drawing.Point(11, 207);
+            this.label3.Location = new System.Drawing.Point(11, 182);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(85, 19);
@@ -136,7 +145,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label2.Location = new System.Drawing.Point(11, 237);
+            this.label2.Location = new System.Drawing.Point(11, 240);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(85, 19);
@@ -147,7 +156,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label5.Location = new System.Drawing.Point(11, 73);
+            this.label5.Location = new System.Drawing.Point(11, 65);
             this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(85, 19);
@@ -158,7 +167,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label4.Location = new System.Drawing.Point(11, 169);
+            this.label4.Location = new System.Drawing.Point(11, 125);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(85, 19);
@@ -169,7 +178,7 @@
             // 
             this.sortByString.AutoSize = true;
             this.sortByString.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.sortByString.Location = new System.Drawing.Point(140, 6);
+            this.sortByString.Location = new System.Drawing.Point(177, 4);
             this.sortByString.Margin = new System.Windows.Forms.Padding(2);
             this.sortByString.Name = "sortByString";
             this.sortByString.Size = new System.Drawing.Size(144, 23);
@@ -182,7 +191,7 @@
             this.sortByNum.AutoSize = true;
             this.sortByNum.Checked = true;
             this.sortByNum.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.sortByNum.Location = new System.Drawing.Point(0, 6);
+            this.sortByNum.Location = new System.Drawing.Point(5, 2);
             this.sortByNum.Margin = new System.Windows.Forms.Padding(2);
             this.sortByNum.Name = "sortByNum";
             this.sortByNum.Size = new System.Drawing.Size(125, 23);
@@ -196,7 +205,7 @@
             this.ascendingOrder.AutoSize = true;
             this.ascendingOrder.Checked = true;
             this.ascendingOrder.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.ascendingOrder.Location = new System.Drawing.Point(3, -2);
+            this.ascendingOrder.Location = new System.Drawing.Point(4, -2);
             this.ascendingOrder.Margin = new System.Windows.Forms.Padding(2);
             this.ascendingOrder.Name = "ascendingOrder";
             this.ascendingOrder.Size = new System.Drawing.Size(106, 23);
@@ -210,7 +219,7 @@
             // 
             this.descendingOrder.AutoSize = true;
             this.descendingOrder.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.descendingOrder.Location = new System.Drawing.Point(143, -2);
+            this.descendingOrder.Location = new System.Drawing.Point(176, -2);
             this.descendingOrder.Margin = new System.Windows.Forms.Padding(2);
             this.descendingOrder.Name = "descendingOrder";
             this.descendingOrder.Size = new System.Drawing.Size(106, 23);
@@ -221,16 +230,16 @@
             // tableLayoutPanel2
             // 
             this.tableLayoutPanel2.ColumnCount = 2;
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 120F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 38F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 150F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 25F));
             this.tableLayoutPanel2.Controls.Add(this.button1, 1, 0);
             this.tableLayoutPanel2.Controls.Add(this.comboBox0, 0, 0);
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(5, 7);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(1, 38);
             this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(2);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 1;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(176, 32);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(175, 32);
             this.tableLayoutPanel2.TabIndex = 6;
             this.tableLayoutPanel2.Controls.SetChildIndex(this.comboBox0, 0);
             this.tableLayoutPanel2.Controls.SetChildIndex(this.button1, 0);
@@ -245,10 +254,10 @@
             this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.Control;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Image = global::C2.Properties.Resources.add;
-            this.button1.Location = new System.Drawing.Point(140, 4);
+            this.button1.Location = new System.Drawing.Point(152, 4);
             this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(34, 24);
+            this.button1.Size = new System.Drawing.Size(21, 24);
             this.button1.TabIndex = 5;
             this.button1.UseVisualStyleBackColor = false;
             // 
@@ -257,7 +266,7 @@
             this.repetition.AutoSize = true;
             this.repetition.Checked = true;
             this.repetition.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.repetition.Location = new System.Drawing.Point(140, 5);
+            this.repetition.Location = new System.Drawing.Point(178, 5);
             this.repetition.Margin = new System.Windows.Forms.Padding(2);
             this.repetition.Name = "repetition";
             this.repetition.Size = new System.Drawing.Size(87, 23);
@@ -270,7 +279,7 @@
             // 
             this.noRepetition.AutoSize = true;
             this.noRepetition.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.noRepetition.Location = new System.Drawing.Point(0, 5);
+            this.noRepetition.Location = new System.Drawing.Point(6, 5);
             this.noRepetition.Margin = new System.Windows.Forms.Padding(2);
             this.noRepetition.Name = "noRepetition";
             this.noRepetition.Size = new System.Drawing.Size(68, 23);
@@ -282,17 +291,17 @@
             // 
             this.panel1.AutoScroll = true;
             this.panel1.Controls.Add(this.tableLayoutPanel2);
-            this.panel1.Location = new System.Drawing.Point(4, 266);
+            this.panel1.Location = new System.Drawing.Point(73, 271);
             this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(405, 123);
+            this.panel1.Size = new System.Drawing.Size(229, 123);
             this.panel1.TabIndex = 1;
             // 
             // panel2
             // 
             this.panel2.Controls.Add(this.sortByNum);
             this.panel2.Controls.Add(this.sortByString);
-            this.panel2.Location = new System.Drawing.Point(133, 125);
+            this.panel2.Location = new System.Drawing.Point(71, 92);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(328, 30);
             this.panel2.TabIndex = 11;
@@ -301,7 +310,7 @@
             // 
             this.panel3.Controls.Add(this.ascendingOrder);
             this.panel3.Controls.Add(this.descendingOrder);
-            this.panel3.Location = new System.Drawing.Point(130, 169);
+            this.panel3.Location = new System.Drawing.Point(72, 157);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(326, 19);
             this.panel3.TabIndex = 12;
@@ -310,7 +319,7 @@
             // 
             this.panel4.Controls.Add(this.noRepetition);
             this.panel4.Controls.Add(this.repetition);
-            this.panel4.Location = new System.Drawing.Point(133, 199);
+            this.panel4.Location = new System.Drawing.Point(71, 209);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(309, 30);
             this.panel4.TabIndex = 13;
@@ -319,7 +328,7 @@
             // 
             this.lable_line1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.lable_line1.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.lable_line1.Location = new System.Drawing.Point(-13, 29);
+            this.lable_line1.Location = new System.Drawing.Point(-13, 27);
             this.lable_line1.Name = "lable_line1";
             this.lable_line1.Size = new System.Drawing.Size(550, 2);
             this.lable_line1.TabIndex = 42;
@@ -328,15 +337,42 @@
             // 
             this.lable_line2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.lable_line2.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.lable_line2.Location = new System.Drawing.Point(-13, 97);
+            this.lable_line2.Location = new System.Drawing.Point(-13, 87);
             this.lable_line2.Name = "lable_line2";
             this.lable_line2.Size = new System.Drawing.Size(550, 2);
             this.lable_line2.TabIndex = 43;
             // 
+            // label_line3
+            // 
+            this.label_line3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label_line3.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label_line3.Location = new System.Drawing.Point(-13, 148);
+            this.label_line3.Name = "label_line3";
+            this.label_line3.Size = new System.Drawing.Size(550, 2);
+            this.label_line3.TabIndex = 44;
+            // 
+            // label_line4
+            // 
+            this.label_line4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label_line4.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label_line4.Location = new System.Drawing.Point(-13, 204);
+            this.label_line4.Name = "label_line4";
+            this.label_line4.Size = new System.Drawing.Size(550, 2);
+            this.label_line4.TabIndex = 45;
+            // 
+            // label_line5
+            // 
+            this.label_line5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label_line5.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label_line5.Location = new System.Drawing.Point(-13, 263);
+            this.label_line5.Name = "label_line5";
+            this.label_line5.Size = new System.Drawing.Size(550, 2);
+            this.label_line5.TabIndex = 46;
+            // 
             // GroupOperatorView
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.ClientSize = new System.Drawing.Size(525, 451);
+            this.ClientSize = new System.Drawing.Size(402, 456);
             this.ControlBox = true;
             this.Controls.Add(this.valuePanel);
             this.Controls.Add(this.bottomPanel);
@@ -344,9 +380,9 @@
             this.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(543, 500);
+            this.MaximumSize = new System.Drawing.Size(420, 505);
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(543, 500);
+            this.MinimumSize = new System.Drawing.Size(420, 505);
             this.Name = "GroupOperatorView";
             this.ShowIcon = true;
             this.Text = "分组算子设置";
@@ -384,5 +420,8 @@
         private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.Label lable_line2;
         private System.Windows.Forms.Label lable_line1;
+        private System.Windows.Forms.Label label_line5;
+        private System.Windows.Forms.Label label_line4;
+        private System.Windows.Forms.Label label_line3;
     }
 }
