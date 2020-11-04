eff9e42e86281e1e9845891b4652f015d45386dd
刘晓凤 <lxfjiangnan@163.com>
2020-11-04 17:34:46 +0800

    图表挂件初始化

---
+++ Citta_T1/Model/Widgets/ChartWidget.cs
@@ -0,0 +1,30 @@
+﻿using C2.Controls.MapViews;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C2.Model.Widgets
+{
+    class ChartWidget: Widget
+    {
+        public const string TypeID = "CHART";
+        public override bool ResponseMouse
+        {
+            get
+            {
+                return true;
+            }
+        }
+        public override Size CalculateSize(MindMapLayoutArgs e)
+        {
+            return new Size(20, 20);
+        }
+        public override string GetTypeID()
+        {
+            return TypeID;
+        }
+    }
+}
