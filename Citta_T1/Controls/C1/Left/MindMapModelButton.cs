4ab091b2738487355cfff2d98359732d3a2ca69f
刘晓凤 <lxfjiangnan@163.com>
2020-11-06 17:29:49 +0800

    左侧面板 业务视图菜单栏功能完善

--- Citta_T1/Controls/C1/Left/MindMapModelButton.cs
+++ Citta_T1/Controls/C1/Left/MindMapModelButton.cs
@@ -33,9 +33,7 @@ namespace C2.Controls.Left
             //Global.GetMainForm().LoadDocument(this.textButton.Text);
             //TODO
             //打开blu模型
-            this.OpenToolStripMenuItem.Enabled = false;
-            this.RenameToolStripMenuItem.Enabled = false;
-            this.DeleteToolStripMenuItem.Enabled = false;
+
         }
 
         private void MindMapModelButton_Load(object sender, EventArgs e)
@@ -165,12 +163,19 @@ namespace C2.Controls.Left
 
         private void contextMenuStrip1_Opening(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            ModelDocument model = Global.GetModelDocumentDao().FindModelDocument(this.ModelTitle);
-            //模型没打开能够导出，否则文档非dirty才能导出
-            if (model == null)
-                this.ExportModel.Enabled = true;
+            if (Global.GetMainForm().OpendDocuments().Contains(ModelTitle))
+            {
+                this.OpenToolStripMenuItem.Enabled = false;
+                this.RenameToolStripMenuItem.Enabled = false;
+                this.DeleteToolStripMenuItem.Enabled = false;
+            }
             else
-                this.ExportModel.Enabled = !model.Modified;
+            {
+                this.OpenToolStripMenuItem.Enabled = true;
+                this.RenameToolStripMenuItem.Enabled = true;
+                this.DeleteToolStripMenuItem.Enabled = true;
+            }
+
         }
 
     }
