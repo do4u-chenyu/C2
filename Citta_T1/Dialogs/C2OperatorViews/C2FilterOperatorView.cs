843266eb97be4bfeaec7ed648c251bf80ef6ddb0
phoenix1214 <273048269@qq.com>
2020-11-06 14:34:01 +0800

    添加过滤算子

---
+++ Citta_T1/Dialogs/C2OperatorViews/C2FilterOperatorView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using C2.Dialogs.Base;
+using C2.Model.Widgets;
+
+namespace C2.Dialogs.C2OperatorViews
+{
+    public partial class C2FilterOperatorView : C2BaseOperatorView
+    {
+        public C2FilterOperatorView(OperatorWidget operatorWidget) : base(operatorWidget)
+        {
+            InitializeComponent();
+        }
+    }
+}
