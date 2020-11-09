cce4294bb1b704a8529322c79fc9d0b30cf1b267
Darkxiete <470058433@qq.com>
2020-11-09 16:39:35 +0800

    1. 修改Charts部分。去掉菜单，仅留下布局 2. 组织结构图默认成向下的组织架构布局

--- Citta_T1/Controls/ChartPageViews/MultiChartsView.cs
+++ Citta_T1/Controls/ChartPageViews/MultiChartsView.cs
@@ -13,7 +13,6 @@ namespace C2.ChartPageView
     public class MultiChartsView : MyTabControl
     {
         Document _Document;
-        SpecialTabItem BtnNewChart;
         List<ToolStripItem> LastCustomTabMenus { get; set; }
 
         public event EventHandler NewChartPage;
