bfe9ade36bbd5c8c834e820785141974f3932bd0
phoenix1214 <273048269@qq.com>
2020-11-16 09:53:56 +0800

    1、抽出mindmapview.schedule类；2、新增业务视图日志

---
+++ Citta_T1/ChartControls/MindMap/MindMapView/MindMapView.Schedule.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using C2.Business.Schedule.Cmd;
+using C2.Core;
+using C2.Dialogs.Base;
+using C2.Dialogs.C2OperatorViews;
+using C2.Model;
+using C2.Model.MindMaps;
+using C2.Model.Widgets;
+using C2.Utils;
+
+namespace C2.Controls.MapViews
+{
+    public partial class MindMapView
+    {
+        private static LogUtil log = LogUtil.GetInstance("MindMapView");
+        private void GenRunCmds()
+        {
+            if (opw.Status == OpStatus.Ready || opw.Status == OpStatus.Done)
+            {
+                //Global.GetDocumentForm().ShowRunLab();
+                List<string> cmds = GenerateCmd();
+                if (cmds == null)
+                    return;
+                this.Cursor = Cursors.WaitCursor;
+                MessageBox.Show(RunLinuxCommand(cmds));
+                this.Cursor = Cursors.Default;
+                //Global.GetDocumentForm().HideRunLab();
+                DataItem resultItem = opw.ResultItem;
+                ResultWidget rsw = (opw.Container as Topic).FindWidget<ResultWidget>();
+                if (rsw == null)
+                {
+                    AddResult(new Topic[] { opw.Container as Topic }, resultItem);
+                    Global.GetCurrentDocument().Modified = false; //新建了一个挂件，此时文档dirty，需要置false
+                    TopicUpdate(opw.Container as Topic, null);
+                }
+
+                else
+                {
+                    //TODO
+                    //是否对undo redo有影响
+                    rsw.DataItems.RemoveAll(di => di.ResultDataType == DataItem.ResultType.SingleOp);
+                    rsw.DataItems.Add(resultItem);
+                }
+                opw.Status = OpStatus.Done;
+            }
+        }
+
+
+        private C2BaseOperatorView GenerateOperatorView()
+        {
+            switch (opw.OpType)
+            {
+                case OpType.MaxOperator: return new C2MaxOperatorView(opw);
+                case OpType.CustomOperator: return new C2CustomOperatorView(opw);
+                case OpType.MinOperator: return new C2MinOperatorView(opw);
+                case OpType.AvgOperator: return new C2AvgOperatorView(opw);
+                case OpType.DataFormatOperator: return new C2DataFormatOperatorView(opw);
+                case OpType.RandomOperator: return new C2RandomOperatorView(opw);
+                case OpType.FreqOperator: return new C2FreqOperatorView(opw);
+                case OpType.SortOperator: return new C2SortOperatorView(opw);
+                case OpType.FilterOperator: return new C2FilterOperatorView(opw);
+                case OpType.GroupOperator: return new C2GroupOperatorView(opw);
+                case OpType.PythonOperator: return new C2PythonOperatorView(opw);
+                default: return null;
+            }
+        }
+
+        private List<string> GenerateCmd()
+        {
+            switch (opw.OpType)
+            {
+                case OpType.MaxOperator: return (new MaxOperatorCmd(opw)).GenCmd();
+                case OpType.CustomOperator: return (new CustomOperatorCmd(opw)).GenCmd();
+                case OpType.MinOperator: return (new MinOperatorCmd(opw)).GenCmd();
+                case OpType.RandomOperator: return (new RandomOperatorCmd(opw)).GenCmd();
+                case OpType.FilterOperator: return (new FilterOperatorCmd(opw)).GenCmd();
+                case OpType.AvgOperator: return (new AvgOperatorCmd(opw)).GenCmd();
+                case OpType.SortOperator: return (new SortOperatorCmd(opw)).GenCmd();
+                case OpType.FreqOperator: return (new FreqOperatorCmd(opw)).GenCmd();
+                case OpType.GroupOperator: return (new GroupOperatorCmd(opw)).GenCmd();
+                case OpType.DataFormatOperator: return (new DataFormatOperatorCmd(opw)).GenCmd();
+                case OpType.PythonOperator: return (new PythonOperatorCmd(opw)).GenCmd();
+                default: return null;
+            }
+        }
+
+
+        public string RunLinuxCommand(List<string> cmds)
+        {
+            // 补充条件检查, cmds 不能为空
+            if (cmds == null || !cmds.Any())
+                return "";
+            string message = String.Empty;
+
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe";
+            //p.StartInfo.Arguments = "/c " + string.Join(";",cmds);
+            p.StartInfo.UseShellExecute = false; // 不显示用户界面
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.RedirectStandardInput = true;//可以重定向输入  
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+
+
+            try
+            {
+                if (p.Start())//开始进程  
+                {
+                    log.Info("=====运算开始====");
+                    foreach (string cmd in cmds)
+                    {
+                        log.Info(cmd);
+                        p.StandardInput.WriteLine(cmd);
+                    }
+                    log.Info("=====运算结束====");
+                    p.BeginErrorReadLine();
+                    p.BeginOutputReadLine();
+
+                    p.StandardInput.WriteLine("exit");
+                    p.WaitForExit(); //等待进程结束，等待时间为指定的毫秒
+
+                    message = "成功执行完毕";
+
+                    if (p.ExitCode != 0)
+                    {
+                        message = "执行程序非正常退出，请检查程序后再运行。";
+                    }
+
+                }
+            }
+            catch (System.InvalidOperationException)
+            {
+                //没有关联进程的异常，是由于用户点击终止按钮，导致进程被关闭
+                //UpdateLogDelegate("InvalidOperationException: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                //异常停止的处理方法
+                message = ex.Message;
+            }
+            finally
+            {
+                if (p != null)
+                    p.Dispose();//释放资源
+                p.Close();
+            }
+            return message;
+        }
+
+    }
+}
