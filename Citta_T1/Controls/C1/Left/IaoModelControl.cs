c5932d04a0d8f9e75c0bc1185d31f5d209d1fd39
wll <2462685790@qq.com>
2020-11-27 09:58:07 +0800

    'Iao实验室改造'

---
+++ Citta_T1/Controls/C1/Left/IaoModelControl.cs
@@ -0,0 +1,27 @@
+﻿using C2.Business.Model;
+using C2.Core;
+using C2.Utils;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace C2.Controls.Left
+{
+    public partial class IaoModelControl : UserControl
+    {
+        public IaoModelControl()
+        {
+            InitializeComponent();
+        }
+
+        private void IaoModelControl_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen p = new Pen(Color.FromArgb(195, 195, 195), 1);  //画笔 1宽度.
+            p.DashStyle = System.Drawing.Drawing2D.DashStyle.Custom;
+            p.DashPattern = new float[] { 4, 4 };
+            g.DrawLine(p, 0, 30, 200, 30);//x1,y1,x2,y2
+        }     
+    }
+}
