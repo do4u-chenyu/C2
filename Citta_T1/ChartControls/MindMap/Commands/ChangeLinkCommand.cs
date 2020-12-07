dd2815a322a1d37c74a3286dca3e2ede564b1bca
273048269@qq.com <273048269@qq.com>
2020-12-07 09:58:33 +0800

    新增link改变的撤销恢复命令

---
+++ Citta_T1/ChartControls/MindMap/Commands/ChangeLinkCommand.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using C2.Model.MindMaps;
+
+namespace C2.Controls.MapViews
+{
+    class ChangeLinkCommand : Command
+    {
+        Topic NewFromTopic;
+        Topic NewToTopic;
+        Topic OldFromTopic;
+        Topic OldToTopic;
+        Link ChangeLink;
+
+        public override string Name
+        {
+            get { return "Change Link"; }
+        }
+
+        public ChangeLinkCommand(Link link, Topic from, Topic to)
+        {
+            ChangeLink = link;
+            OldFromTopic = link.From;
+            OldToTopic = link.Target;
+            NewFromTopic = from;
+            NewToTopic = to;
+        }
+
+        public override bool Execute()
+        {
+            if (ChangeLink == null || (NewFromTopic == null && NewToTopic == null) || (OldFromTopic == null && OldToTopic == null))
+                return false;
+            if (NewFromTopic != null)
+                ChangeLink.From = NewFromTopic;
+            else if (NewToTopic != null)
+                ChangeLink.Target = NewToTopic;
+
+            return true;
+        }
+
+        public override bool Rollback()
+        {
+            if (ChangeLink == null || (NewFromTopic == null && NewToTopic == null) || (OldFromTopic == null && OldToTopic == null))
+                return false;
+            if (NewFromTopic != null)
+                ChangeLink.From = OldFromTopic;
+            else if (NewToTopic != null)
+                ChangeLink.Target = OldToTopic;
+
+            return true;
+        }
+
+        public override bool Redo()
+        {
+            return Execute();
+        }
+    }
+}
