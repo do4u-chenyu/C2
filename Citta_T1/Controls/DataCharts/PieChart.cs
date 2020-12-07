0745a1d61f6422064807a5447116d0bd5612a7c4
smallWhite <1309744250@qq.com>
2020-12-07 17:23:59 +0800

    重构饼图代码继承基类

--- Citta_T1/Controls/DataCharts/PieChart.cs
+++ Citta_T1/Controls/DataCharts/PieChart.cs
@@ -1,80 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Drawing;
-using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
 
 namespace C2.Controls.DataCharts
 {
-    public partial class PieChart : UserControl
+    class PieChart:BaseChart
     {
-        public System.Windows.Forms.DataVisualization.Charting.Chart GetChart { get => this.chart1; }
-        public bool EmptyInput { get; set; }
-        public PieChart(List<List<string>> dataList, List<string> title)
+        public System.Windows.Forms.DataVisualization.Charting.Chart GetChart { get => Chart1; }
+
+        public PieChart(List<List<string>> dataList, List<string> title) : base(dataList, title)
         {
-            EmptyInput = false;
-            InitializeComponent();
-            InitChart(title[0],title[1],title[2]);
-            DataBind(dataList[0], dataList[1]);
+            SetStyle();
         }
-        public void InitChart(string title,string xTitle,string yTitle)
+        void SetStyle()
         {
-            chart1.Titles.Add(title);
-            chart1.Titles[0].ForeColor = Color.White;
-            chart1.Titles[0].Font = new Font("微软雅黑", 12f, FontStyle.Regular);
-            chart1.Titles[0].Alignment = ContentAlignment.TopCenter;
-
-            //控件背景
-            chart1.BackColor = Color.Transparent;
-            //图表区背景
-            chart1.ChartAreas[0].BackColor = Color.Transparent;
-            chart1.ChartAreas[0].BorderColor = Color.Transparent;
-            //X轴标签间距
-            chart1.ChartAreas[0].AxisX.Interval = 1;
-            chart1.ChartAreas[0].AxisX.LabelStyle.IsStaggered = true;
-            chart1.ChartAreas[0].AxisX.LabelStyle.Angle = -45;
-            chart1.ChartAreas[0].AxisX.TitleFont = new Font("微软雅黑", 14f, FontStyle.Regular);
-            chart1.ChartAreas[0].AxisX.TitleForeColor = Color.White;
-
-            //X坐标轴颜色
-            chart1.ChartAreas[0].AxisX.LineColor = ColorTranslator.FromHtml("#38587a"); ;
-            chart1.ChartAreas[0].AxisX.LabelStyle.ForeColor = Color.White;
-            chart1.ChartAreas[0].AxisX.LabelStyle.Font = new Font("微软雅黑", 10f, FontStyle.Regular);
-            //X坐标轴标题
-            chart1.ChartAreas[0].AxisX.Title = xTitle;
-            chart1.ChartAreas[0].AxisX.TitleFont = new Font("微软雅黑", 10f, FontStyle.Regular);
-            chart1.ChartAreas[0].AxisX.TitleForeColor = Color.White;
-            chart1.ChartAreas[0].AxisX.TextOrientation = TextOrientation.Horizontal;
-            chart1.ChartAreas[0].AxisX.ToolTip = xTitle;
-            //X轴网络线条
-            chart1.ChartAreas[0].AxisX.MajorGrid.Enabled = true;
-            chart1.ChartAreas[0].AxisX.MajorGrid.LineColor = ColorTranslator.FromHtml("#2c4c6d");
-
-            //Y坐标轴颜色
-            chart1.ChartAreas[0].AxisY.LineColor = ColorTranslator.FromHtml("#38587a");
-            chart1.ChartAreas[0].AxisY.LabelStyle.ForeColor = Color.White;
-            chart1.ChartAreas[0].AxisY.LabelStyle.Font = new Font("微软雅黑", 10f, FontStyle.Regular);
-            //Y坐标轴标题
-            chart1.ChartAreas[0].AxisY.Title = yTitle;
-            chart1.ChartAreas[0].AxisY.TitleFont = new Font("微软雅黑", 10f, FontStyle.Regular);
-            chart1.ChartAreas[0].AxisY.TitleForeColor = Color.White;
-            chart1.ChartAreas[0].AxisY.TextOrientation = TextOrientation.Rotated270;
-            chart1.ChartAreas[0].AxisY.ToolTip = yTitle;
-            //Y轴网格线条
-            chart1.ChartAreas[0].AxisY.MajorGrid.Enabled = true;
-            chart1.ChartAreas[0].AxisY.MajorGrid.LineColor = ColorTranslator.FromHtml("#2c4c6d");
-
-            chart1.ChartAreas[0].AxisY2.LineColor = Color.Transparent;
-
-            //背景渐变
-            chart1.ChartAreas[0].BackGradientStyle = GradientStyle.None;
-
-            //图例样式
+            Chart1.ChartAreas[0].BackGradientStyle = GradientStyle.None;
+            Chart1.Series[0].ChartType = SeriesChartType.Pie;
+            Chart1.Series[0].Points[0].Color = Color.Gray;
+            Chart1.Series[0].Palette = ChartColorPalette.BrightPastel;
+            Chart1.ChartAreas[0].Area3DStyle.Enable3D = true;
+            Chart1.Series[0].CustomProperties = "PieLabelStyle = Outside";
             Legend legend2 = new Legend("#VALX");
             legend2.Title = "图例";
             legend2.TitleBackColor = Color.Transparent;
@@ -83,68 +32,13 @@ namespace C2.Controls.DataCharts
             legend2.TitleFont = new Font("微软雅黑", 10f, FontStyle.Regular);
             legend2.Font = new Font("微软雅黑", 8f, FontStyle.Regular);
             legend2.ForeColor = Color.White;
-
-            chart1.Series[0].XValueType = ChartValueType.String;  //设置X轴上的值类型
-            chart1.Series[0].Label = "#VAL";                //设置显示X Y的值    
-            chart1.Series[0].LabelForeColor = Color.White;
-            chart1.Series[0].ToolTip = "#VALX:#VAL(宗)";     //鼠标移动到对应点显示数值
-            chart1.Series[0].ChartType = SeriesChartType.Pie;    //图类型(折线)
-
-            chart1.Series[0].Color = Color.Lime;
-            chart1.Series[0].LegendText = legend2.Name;
-            chart1.Series[0].IsValueShownAsLabel = true;
-            chart1.Series[0].LabelForeColor = Color.White;
-            chart1.Series[0].CustomProperties = "DrawingStyle = Cylinder";
-            chart1.Series[0].CustomProperties = "PieLabelStyle = Outside";
-            chart1.Legends.Add(legend2);
-            chart1.Legends[0].Position.Auto = true;
-            chart1.Series[0].IsValueShownAsLabel = true;
+            Chart1.Legends[0] = legend2;
+            Chart1.Legends[0].Position.Auto = true;
+            Chart1.Series[0].IsValueShownAsLabel = true;
             //是否显示图例
-            chart1.Series[0].IsVisibleInLegend = true;
-            chart1.Series[0].ShadowOffset = 0;
-
-            //饼图折线
-            //chart1.Series[0]["PieLineColor"] = "White";
-            ////绑定数据
-            //chart1.Series[0].Points.DataBindXY(x, y);
-            //chart1.Series[0].Points[0].Color = Color.White;
-            ////绑定颜色
-            //chart1.Series[0].Palette = ChartColorPalette.BrightPastel;
-
-        }
-        void DataBind(List<string> x, List<string> y)
-        {
-            int count = 0;
-            try
-            {
-                chart1.Series[0]["PieLineColor"] = "Gray";
-                List<double> y_double = new List<double>();
-                foreach (string tmp in y)
-                {
-                    try
-                    {
-                        y_double.Add(Convert.ToDouble(tmp));
-                    }
-                    catch
-                    {
-                        y_double.Add(0);
-                        count++;
-                    }
-
-                }
-                if (y.Count == count)
-                {
-                    EmptyInput = true;
-                }
-                chart1.Series[0].Points.DataBindXY(x, y_double);
-                chart1.Series[0].Points[0].Color = Color.Gray;
-                chart1.Series[0].Palette = ChartColorPalette.BrightPastel;
-                chart1.ChartAreas[0].Area3DStyle.Enable3D = true;
-            }
-            catch
-            {
-                return;
-            }
+            Chart1.Series[0].IsVisibleInLegend = true;
+            Chart1.Series[0].ShadowOffset = 0;
         }
     }
 }
+
