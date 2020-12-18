c37dc0ec985c79a8a145c61d2ede7a97820d546d
lu-qi123 <18705158378@163.com>
2020-12-18 16:48:53 +0800

    初始化自定义SQL配置窗口

---
+++ Citta_T1/Dialogs/C2OperatorViews/C2SqlOperatorView.cs
@@ -0,0 +1,12 @@
+﻿using C2.Dialogs.Base;
+
+namespace C2.Dialogs.C2OperatorViews
+{
+    public partial class C2SqlOperatorView : C2BaseOperatorView
+    {
+        public C2SqlOperatorView()
+        {
+            InitializeComponent();
+        }
+    }
+}
