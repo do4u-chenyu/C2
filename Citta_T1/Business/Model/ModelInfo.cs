b8c94d25886a2a3eb944022d83d0655d1b5a5823
lu-qi123 <18705158378@163.com>
2021-01-11 14:05:57 +0800

    变量名小修改

---
+++ Citta_T1/Business/Model/ModelInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace C2.Business.Model
+{
+    class ModelInfo
+    {
+        public static string[] LoadAllModelTitle(string modelPath)
+        {
+            string[] modelTitles = new string[0];
+            try
+            {
+                DirectoryInfo userDir = new DirectoryInfo(modelPath);
+                DirectoryInfo[] dir = userDir.GetDirectories();
+                modelTitles = Array.ConvertAll(dir, value => Convert.ToString(value));
+            }
+            catch { }
+            return modelTitles;
+        }
+    }
+}
