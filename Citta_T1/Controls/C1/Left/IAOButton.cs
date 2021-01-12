7a43dfb7c05541e3cad7e45479c214b8ce4c6e1e
bigeyes100 <2462685790@qq.com>
2021-01-12 14:21:21 +0800

    '中英文转换'

--- Citta_T1/Controls/C1/Left/IAOButton.cs
+++ Citta_T1/Controls/C1/Left/IAOButton.cs
@@ -1,6 +1,7 @@
 ﻿
 using C2.Business.Model;
 using C2.Core;
+using C2.Globalization;
 using C2.Model;
 using C2.Utils;
 using System;
@@ -16,24 +17,31 @@ namespace C2.Controls.Left
             InitializeComponent();
             txtButton.Name = ffp;
             txtButton.Text = ffp;
+            this.leftPictureBox.Image = global::C2.Properties.Resources.Apk;
             switch (ffp)
             {
                 case "APK":
+                    this.txtButton.Text = Lang._("APK");
                     this.leftPictureBox.Image = global::C2.Properties.Resources.Apk;
                     break;
                 case "BaseStation":
+                    this.txtButton.Text = Lang._("BaseStation");
                     this.leftPictureBox.Image = global::C2.Properties.Resources.BaseStation;
                     break;
                 case "Wifi":
+                    this.txtButton.Text = Lang._("Wifi");
                     this.leftPictureBox.Image = global::C2.Properties.Resources.Wifi;
                     break;
                 case "Card":
+                    this.txtButton.Text = Lang._("Card");
                     this.leftPictureBox.Image = global::C2.Properties.Resources.Card;
                     break;
                 case "Tude":
+                    this.txtButton.Text = Lang._("Tude");
                     this.leftPictureBox.Image = global::C2.Properties.Resources.Tude;
                     break;
                 case "Ip":
+                    this.txtButton.Text = Lang._("Ip");
                     this.leftPictureBox.Image = global::C2.Properties.Resources.Ip;
                     break;
             }
