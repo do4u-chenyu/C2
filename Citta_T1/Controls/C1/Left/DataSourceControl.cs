dfdbb11a34d185cab3dfb2f445c432822a9764e9
bigeyes100 <2462685790@qq.com>
2021-01-14 09:56:04 +0800

    '完成字段搜索'

--- Citta_T1/Controls/C1/Left/DataSourceControl.cs
+++ Citta_T1/Controls/C1/Left/DataSourceControl.cs
@@ -394,6 +394,11 @@ namespace C2.Controls.Left
         private void TableFilterTextBox_TextChanged(object sender, EventArgs e)
         {
             this.tableFilterTextBox.ForeColor = SystemColors.WindowText;
+            if (this.optComboBox.Text.ToString() == String.Empty)
+            {
+                ReLayoutTableFrame(RelateTableButtons);
+                return;
+            }
             switch (this.optComboBox.Text.ToString())
             {
                 case "表名": ReLayoutTableFrame(RelateTableButtons.FindAll(t => t.LinkSourceName.Contains(tableFilterTextBox.Text.ToUpper())));
@@ -401,6 +406,7 @@ namespace C2.Controls.Left
                 case "字段名": ReLayoutTableFrame(RelateTableButtons.FindAll(t => t.ColumnName.Contains(tableFilterTextBox.Text.ToUpper())));
                     break;
             }
+
         }
         #endregion
 
