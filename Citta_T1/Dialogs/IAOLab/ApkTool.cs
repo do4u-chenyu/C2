4207bed0b71a626290c8623efc5e53695002eb01
735946247@qq.com <735946247@qq.com>
2021-01-15 14:40:57 +0800

    iao实验室ApkTool apk大小计算，apk反编译功能完成

--- Citta_T1/Dialogs/IAOLab/ApkTool.cs
+++ Citta_T1/Dialogs/IAOLab/ApkTool.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,35 +15,137 @@ namespace C2.Dialogs.IAOLab
 {
     public partial class ApkTool : BaseDialog
     {
+        private readonly object _objLock = new object();
+        
+        public delegate void UpdateLog(string log);//声明一个更新主线程日志的委托
+        public UpdateLog UpdateLogDelegate;
         public ApkTool()
         {
             InitializeComponent();
 
         }
-        public void ExactApk()
+        public string ExactApk(string apkpath, string jdkpath)
         {
             // 运行JAR包
-
+            string tmpPath = Path.GetTempPath()+"ApkTool";
+            if (!Directory.Exists(tmpPath))
+            {
+              Directory.CreateDirectory(tmpPath);
+            }
+            RunLinuxCommandApkTool(GetCmdCommand(apkpath, jdkpath));
             GetApkInfo();
-            GetApkSize();
+
 
             //先不用生成result,读取需要的数据加载到内存，并控件预览框展示
 
 
             // 删除临时结果文件
+            return null;
         }
-        private void GetApkInfo()
+        private string GetApkInfo(string apkName)
         {
-
+            Path.GetTempPath();
+            return null;
         }
-        private void GetApkSize()
+        private long GetApkSize(string filepath)
         {
-
+            
+            using (FileStream fileStream = new FileStream(filepath, FileMode.Open, FileAccess.Read))
+            {
+                long size = fileStream.Length;
+                size /= (1024 * 1024);
+                return size;
+            }
         }
         public void ExporResult()
         {
             // 右键导出，生成结果文件到excel
             // python的save方法调用
+        }
+        public List<string> GetCmdCommand(string apkpath,string jdkpath)
+        {
+            string setJdkPath = "set path = " + jdkpath + "bin;% path %";
+            List<string> cmdList = null;
+            cmdList.Add(setJdkPath);
+            DirectoryInfo dir = new DirectoryInfo(apkpath);
+            //检索表示当前目录的文件和子目录
+            FileSystemInfo[] fsinfos = dir.GetFileSystemInfos();
+            //遍历检索的文件和子目录
+            foreach (FileSystemInfo fsinfo in fsinfos)
+            {
+                
+                string cmdApk = "java - jar apktool_2.4.1.jar d - f" + fsinfo.FullName+ "-o"+ Path.GetTempPath(); 
+                //Console.WriteLine(fsinfo.FullName);
+                //将得到的文件全路径放入到集合中
+                cmdList.Add(cmdApk);
+                
+            }
+            return cmdList;
+        }
+        public string RunLinuxCommandApkTool(List<string> cmds)
+        {
+            // 补充条件检查, cmds 不能为空
+            if (cmds == null || !cmds.Any())
+                return "";
+            string errorMessage = String.Empty;
+
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe";
+            //p.StartInfo.Arguments = "/c " + string.Join(";",cmds);
+            p.StartInfo.UseShellExecute = false; // 不显示用户界面
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.RedirectStandardInput = true;//可以重定向输入  
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+
+
+            try
+            {
+                if (p.Start())//开始进程  
+                {
+                    foreach (string cmd in cmds)
+                    {
+                        UpdateLogDelegate("执行命令: " + cmd);
+                        p.StandardInput.WriteLine(cmd);
+                    }
+
+                    //多线程下异步读取
+                    //p.ErrorDataReceived += new DataReceivedEventHandler(p_ErrorDataReceived);
+
+                    p.BeginErrorReadLine();
+                    p.BeginOutputReadLine();
+
+                    p.StandardInput.WriteLine("exit");
+                    p.WaitForExit(); //等待进程结束，等待时间为指定的毫秒
+
+                    UpdateLogDelegate("退出码" + p.ExitCode.ToString());
+                    if (p.ExitCode != 0)
+                    {
+                        errorMessage = "执行程序非正常退出，请检查程序后再运行。";
+                        UpdateLogDelegate("执行程序非正常退出，请检查程序后再运行。");
+                    }
+
+                }
+            }
+            catch (System.InvalidOperationException)
+            {
+                //没有关联进程的异常，是由于用户点击终止按钮，导致进程被关闭
+                //UpdateLogDelegate("InvalidOperationException: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                //异常停止的处理方法
+                errorMessage = ex.Message;
+                UpdateLogDelegate("RunLinuxCommand进程异常: " + ex.Message);
+            }
+            finally
+            {
+                if (p != null)
+                    p.Dispose();//释放资源
+                p.Close();
+               
+            }
+            return errorMessage;
         }
     }
 }
