5d2f5a5aed95dab5f79b8f12d2e9df2af73563f9
735946247@qq.com <735946247@qq.com>
2021-01-29 11:09:58 +0800

    apktoolbug修复

--- Citta_T1/Dialogs/IAOLab/ApkTool.Designer.cs
+++ Citta_T1/Dialogs/IAOLab/ApkTool.Designer.cs
@@ -39,6 +39,12 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ICON = new System.Windows.Forms.DataGridViewImageColumn();
+            this.fileName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ApkName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.packageName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mainFunction = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.size = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.tipLable = new System.Windows.Forms.Label();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
@@ -47,12 +53,6 @@
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.ICON = new System.Windows.Forms.DataGridViewImageColumn();
-            this.fileName = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.ApkName = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.packageName = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.mainFunction = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.size = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -68,7 +68,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(162, 23);
             this.label1.TabIndex = 0;
-            this.label1.Text = "请输入APK存放目录";
+            this.label1.Text = "请选择APK存放目录";
             // 
             // inputPathTextBox
             // 
@@ -129,7 +129,7 @@
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(160, 23);
             this.label2.TabIndex = 8;
-            this.label2.Text = "请输入JDK所在路径";
+            this.label2.Text = "请选择JDK所在路径";
             // 
             // pictureBox1
             // 
@@ -179,53 +179,6 @@
             this.dataGridView1.Size = new System.Drawing.Size(895, 272);
             this.dataGridView1.TabIndex = 11;
             this.dataGridView1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.DataGridView1_MouseDown);
-            // 
-            // tipLable
-            // 
-            this.tipLable.AutoSize = true;
-            this.tipLable.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.tipLable.Location = new System.Drawing.Point(28, 94);
-            this.tipLable.Name = "tipLable";
-            this.tipLable.Size = new System.Drawing.Size(466, 15);
-            this.tipLable.TabIndex = 12;
-            this.tipLable.Text = "jdk路径格式：C:\\Program Files\\Java\\jdk-13.0.1\\bin\\java.exe";
-            // 
-            // contextMenuStrip1
-            // 
-            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.导出到ExcelToolStripMenuItem});
-            this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(161, 28);
-            // 
-            // 导出到ExcelToolStripMenuItem
-            // 
-            this.导出到ExcelToolStripMenuItem.Name = "导出到ExcelToolStripMenuItem";
-            this.导出到ExcelToolStripMenuItem.Size = new System.Drawing.Size(160, 24);
-            this.导出到ExcelToolStripMenuItem.Text = "导出到Excel";
-            this.导出到ExcelToolStripMenuItem.Click += new System.EventHandler(this.ExportToExcelToolStripMenuItem_Click);
-            // 
-            // openFileDialog1
-            // 
-            this.openFileDialog1.FileName = "openFileDialog1";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(673, 104);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(210, 25);
-            this.textBox1.TabIndex = 14;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.label3.Location = new System.Drawing.Point(28, 114);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(363, 15);
-            this.label3.TabIndex = 15;
-            this.label3.Text = "APK分析需要占用较大C盘空间，多个APK建议分批分析";
             // 
             // ICON
             // 
@@ -277,6 +230,53 @@
             this.size.Name = "size";
             this.size.ReadOnly = true;
             this.size.Width = 66;
+            // 
+            // tipLable
+            // 
+            this.tipLable.AutoSize = true;
+            this.tipLable.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.tipLable.Location = new System.Drawing.Point(28, 94);
+            this.tipLable.Name = "tipLable";
+            this.tipLable.Size = new System.Drawing.Size(466, 15);
+            this.tipLable.TabIndex = 12;
+            this.tipLable.Text = "jdk路径格式：C:\\Program Files\\Java\\jdk-13.0.1\\bin\\java.exe";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.导出到ExcelToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(161, 28);
+            // 
+            // 导出到ExcelToolStripMenuItem
+            // 
+            this.导出到ExcelToolStripMenuItem.Name = "导出到ExcelToolStripMenuItem";
+            this.导出到ExcelToolStripMenuItem.Size = new System.Drawing.Size(160, 24);
+            this.导出到ExcelToolStripMenuItem.Text = "导出到Excel";
+            this.导出到ExcelToolStripMenuItem.Click += new System.EventHandler(this.ExportToExcelToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(673, 104);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(210, 25);
+            this.textBox1.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label3.Location = new System.Drawing.Point(28, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(363, 15);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "APK分析需要占用较大C盘空间，多个APK建议分批分析";
             // 
             // ApkTool
             // 
