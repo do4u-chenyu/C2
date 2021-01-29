8ad068e8883f4d37d1d9087e42daa834d9cb6d69
刘晓凤 <lxfjiangnan@163.com>
2021-01-29 15:58:35 +0800

    修改hive名称

--- Citta_T1/Database/HiveDAOImpl.cs
+++ Citta_T1/Database/HiveDAOImpl.cs
@@ -149,8 +149,9 @@ namespace C2.Database
             }
             catch (Exception ex)
             {
-                HelpUtil.ShowMessageBox(ex.ToString(),"提示信息",System.Windows.Forms.MessageBoxIcon.Warning);
                 log.Error(HelpUtil.DbCannotBeConnectedInfo + ", 详情：" + ex.ToString());   // 辅助工具类，showmessage不能放在外面
+              //  QueryFailureException();
+                
             }
             return sb.ToString().Trim(OpUtil.DefaultLineSeparator);
         }
