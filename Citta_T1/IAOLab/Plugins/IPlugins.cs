dde4b3180827f83598fbb98a64fef18b82e26b3d
lu-qi123 <18705158378@163.com>
2021-01-29 16:19:40 +0800

    添加插件接口类

---
+++ Citta_T1/IAOLab/Plugins/IPlugins.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace C2.IAOLab.Plugins
+{
+    interface IPlugins
+    {
+        String GetPluginName();
+        String GetPluginDescription();
+        String GetPluginVersion();
+        DialogResult ShowDialog();
+    }
+}
