594b2791d9352f875feb7e00690d1d61d9e805a6
273048269@qq.com <273048269@qq.com>
2021-02-04 15:42:29 +0800

    内嵌浏览器窗口新增菜单栏

--- Citta_T1/IAOLab/WebEngine/Dialogs/WebBrowserDialog.cs
+++ Citta_T1/IAOLab/WebEngine/Dialogs/WebBrowserDialog.cs
@@ -1,32 +1,21 @@
-﻿using System;
+﻿using C2.Controls;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using C2.Controls;
 
-namespace C2.IAOLab.WebEngine
+namespace C2.IAOLab.WebEngine.Dialogs
 {
-    [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
-    [System.Runtime.InteropServices.ComVisibleAttribute(true)]
-    partial class WebBrowserDialog :  StandardDialog
+    partial class WebBrowserDialog : StandardDialog
     {
         public WebBrowserDialog()
         {
             InitializeComponent();
-        }
-
-        private void WebBrowserDialog_Load(object sender, EventArgs e)
-        {
-            string str_url = Application.StartupPath + "\\StartMap.html";
-            //Uri url = new Uri(str_url);
-            //webBrowser1.Url = url;
-            //webBrowser1.ObjectForScripting = this;
         }
     }
 }
