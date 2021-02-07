7f83e9371e37df7345180369d8b0c693d65e89b4
lu-qi123 <18705158378@163.com>
2021-02-07 21:35:40 +0800

    创建 QQ爬虫 插件项目工程

---
+++ C2Plugins/QQSpiderPlugin/Form1.cs
@@ -0,0 +1,46 @@
+﻿using C2.IAOLab.Plugins;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QQSpiderPlugin
+{
+    public partial class Form1 : Form, IPlugin
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string GetPluginDescription()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Image GetPluginImage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetPluginName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetPluginVersion()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DialogResult ShowFormDialog()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
