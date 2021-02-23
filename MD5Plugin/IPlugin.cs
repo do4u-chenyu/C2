7a00276ac2f9f58980b81128176d94a35bac66e9
lu-qi123 <18705158378@163.com>
2021-02-23 20:49:58 +0800

    新增MD5插件

---
+++ MD5Plugin/IPlugin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace C2.IAOLab.Plugins
+{
+    interface IPlugin
+    {
+        String GetPluginName();
+        String GetPluginDescription();
+        String GetPluginVersion();
+        Image GetPluginImage(); 
+        DialogResult ShowFormDialog();
+    }
+}
