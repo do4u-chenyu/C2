9f73d9c4b120721618d04c5f3ae14e626d398854
bigeyes100 <2462685790@qq.com>
2021-02-24 20:55:03 +0800

    '编辑器80%'

--- Citta_T1/IAOLab/WebEngine/Dialogs/WebBrowserDialog.Designer.cs
+++ Citta_T1/IAOLab/WebEngine/Dialogs/WebBrowserDialog.Designer.cs
@@ -31,6 +31,8 @@
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.htmlEditorControlEx1 = new ICSharpCode.TextEditor.TextEditorControlEx();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // toolStrip1
@@ -46,12 +48,14 @@
             // 
             // webBrowser1
             // 
-            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.webBrowser1.Location = new System.Drawing.Point(0, 25);
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(12, 23);
             this.webBrowser1.Margin = new System.Windows.Forms.Padding(0);
             this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(932, 467);
+            this.webBrowser1.Size = new System.Drawing.Size(920, 411);
             this.webBrowser1.TabIndex = 10004;
             // 
             // htmlEditorControlEx1
@@ -59,21 +63,33 @@
             this.htmlEditorControlEx1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.htmlEditorControlEx1.Enabled = false;
             this.htmlEditorControlEx1.FoldingStrategy = "XML";
             this.htmlEditorControlEx1.Font = new System.Drawing.Font("Courier New", 10F);
-            this.htmlEditorControlEx1.Location = new System.Drawing.Point(12, 28);
+            this.htmlEditorControlEx1.Location = new System.Drawing.Point(24, 18);
             this.htmlEditorControlEx1.Name = "htmlEditorControlEx1";
-            this.htmlEditorControlEx1.Size = new System.Drawing.Size(408, 400);
+            this.htmlEditorControlEx1.Size = new System.Drawing.Size(104, 397);
             this.htmlEditorControlEx1.SyntaxHighlighting = "HTML";
             this.htmlEditorControlEx1.TabIndex = 10005;
-            this.htmlEditorControlEx1.Visible = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.htmlEditorControlEx1);
+            this.panel1.Enabled = false;
+            this.panel1.Location = new System.Drawing.Point(12, 28);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(147, 415);
+            this.panel1.TabIndex = 10006;
+            this.panel1.Visible = false;
             // 
             // WebBrowserDialog
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(932, 492);
-            this.Controls.Add(this.htmlEditorControlEx1);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.webBrowser1);
             this.Controls.Add(this.toolStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -86,7 +102,8 @@
             this.Load += new System.EventHandler(this.WebBrowserDialog_Load);
             this.Controls.SetChildIndex(this.toolStrip1, 0);
             this.Controls.SetChildIndex(this.webBrowser1, 0);
-            this.Controls.SetChildIndex(this.htmlEditorControlEx1, 0);
+            this.Controls.SetChildIndex(this.panel1, 0);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -97,5 +114,6 @@
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.WebBrowser webBrowser1;
         private ICSharpCode.TextEditor.TextEditorControlEx htmlEditorControlEx1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
