c5f81800238da14788e51f75ad7741d32cc86a5c
刘晓凤 <lxfjiangnan@163.com>
2021-02-26 14:35:49 +0800

    施工  软件更新

--- C2Shell/Program.cs
+++ C2Shell/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace C2Shell
 {
@@ -11,16 +13,18 @@ namespace C2Shell
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("==============ok========");
             IsNeedUpdate();
-   
-        
+
+            StartCoreProcess();
 
         }
         private static void IsNeedUpdate()
         {
             // update路径是否为空
-            if (Directory.Exists(""))
+            //  string packagePath = Path.Combine(Application.StartupPath, "update");
+            string packagePath = string.Empty;
+            if (Directory.Exists(packagePath))
                 return;
             if (!ExecuteUpdate())
             {
@@ -31,13 +35,22 @@ namespace C2Shell
         private static bool ExecuteUpdate()
         {
             // 解压update目录
-
+            // 更新进度窗体
             // 执行 setup.bat脚本
             return true;
         }
         private static void  Rollback()
         {
             // 执行 rollback.bat脚本
+        }
+        private static void StartCoreProcess()
+        {
+            //string strPathExe = Environment.CurrentDirectory + "\\FaceRecognition" + "\\IDFaceDemo.exe";
+            //Process process = new System.Diagnostics.Process();
+            //process.StartInfo.FileName = strPathExe;
+
+            //process.Start();
+
         }
     }
 }
