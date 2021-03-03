a06b1aa520cc4b621f95e3d8905f480575c81226
xxlisa <1261921617@qq.com>
2021-03-03 15:56:06 +0800

    MD5插件更新

--- C2Plugins/MD5Plugin/Form1.Designer.cs
+++ C2Plugins/MD5Plugin/Form1.Designer.cs
@@ -143,6 +143,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.ClientSize = new System.Drawing.Size(1369, 684);
             this.Controls.Add(this.radioButton4);
             this.Controls.Add(this.radioButton3);
