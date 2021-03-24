796d2b6928ebe2b14178009a9926a715029aba01
lu-qi123 <18705158378@163.com>
2021-03-24 18:33:52 +0800

    全文工具施工中，添加右键菜单

--- Citta_T1/Controls/C1/Left/SearchToolkitButton.Designer.cs
+++ Citta_T1/Controls/C1/Left/SearchToolkitButton.Designer.cs
@@ -28,10 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.新建任务ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.leftPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rightPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // noFocusButton
+            // 
+            this.noFocusButton.FlatAppearance.BorderSize = 0;
+            // 
+            // 新建任务ToolStripMenuItem
+            // 
+            this.新建任务ToolStripMenuItem.Name = "新建任务ToolStripMenuItem";
+            this.新建任务ToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            this.新建任务ToolStripMenuItem.Text = "新建任务";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            // 
+            // SearchToolkitButton
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "SearchToolkitButton";
+            this.Controls.SetChildIndex(this.noFocusButton, 0);
+            this.Controls.SetChildIndex(this.leftPictureBox, 0);
+            this.Controls.SetChildIndex(this.rightPictureBox, 0);
+            this.Controls.SetChildIndex(this.textBox, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.leftPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rightPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStripMenuItem 新建任务ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
     }
 }
--- Citta_T1/Controls/C1/Left/SearchToolkitButton.cs
+++ Citta_T1/Controls/C1/Left/SearchToolkitButton.cs
@@ -27,33 +27,38 @@ namespace C2.Controls.C1.Left
 
         private void InitButtonMenu()
         {
-            //ToolStripMenuItem RemoveToolStripMenuItem = new ToolStripMenuItem();
-            //RemoveToolStripMenuItem.Name = "ReviewToolStripMenuItem";
-            //RemoveToolStripMenuItem.Size = new System.Drawing.Size(196, 22);
-            //RemoveToolStripMenuItem.Text = "删除任务";
-            //RemoveToolStripMenuItem.ToolTipText = "从面板中移除任务,同时删除本地结果文件";
-            //RemoveToolStripMenuItem.Click += new System.EventHandler(RemoveToolStripMenuItem_Click);
+            ToolStripMenuItem RemoveToolStripMenuItem = new ToolStripMenuItem
+            {
+                Name = "ReviewToolStripMenuItem",
+                Size = new Size(196, 22),
+                Text = "删除任务",
+                ToolTipText = "从面板中移除任务,同时删除本地结果文件"
+            };
+            RemoveToolStripMenuItem.Click += new EventHandler(RemoveToolStripMenuItem_Click);
 
-            //ToolStripMenuItem OpenDatasourceToolStripMenuItem = new ToolStripMenuItem();
-            //OpenDatasourceToolStripMenuItem.Name = "OpenDatasourceToolStripMenuItem";
-            //OpenDatasourceToolStripMenuItem.Size = new System.Drawing.Size(196, 22);
-            //OpenDatasourceToolStripMenuItem.Text = "打开源文件";
-            //OpenDatasourceToolStripMenuItem.ToolTipText = "从本地文本编辑器中打开文件";
-            //OpenDatasourceToolStripMenuItem.Click += new System.EventHandler(OpenDatasourceToolStripMenuItem_Click);
 
-            //ToolStripMenuItem ResultToolStripMenuItem = new ToolStripMenuItem();
-            //ResultToolStripMenuItem.Name = "ResultToolStripMenuItem";
-            //ResultToolStripMenuItem.Size = new System.Drawing.Size(196, 22);
-            //ResultToolStripMenuItem.Text = "查看结果";
-            //ResultToolStripMenuItem.ToolTipText = "查看任务返回结果";
-            //ResultToolStripMenuItem.Click += new System.EventHandler(ResultToolStripMenuItem_Click);
+            ToolStripMenuItem ResultToolStripMenuItem = new ToolStripMenuItem
+            {
+                Name = "ResultToolStripMenuItem",
+                Size = new Size(196, 22),
+                Text = "查看结果",
+                ToolTipText = "查看任务返回结果"
+            };
+            ResultToolStripMenuItem.Click += new EventHandler(ResultToolStripMenuItem_Click);
 
-            //this.contextMenuStrip.Items.AddRange(new ToolStripItem[] {
-            //        OpenDatasourceToolStripMenuItem,
-            //        ResultToolStripMenuItem,
-            //        RemoveToolStripMenuItem
-            //     });
+            this.contextMenuStrip.Items.AddRange(new ToolStripItem[] {
+                    ResultToolStripMenuItem,
+                    RemoveToolStripMenuItem
+                 });
 
+        }
+
+        private void ResultToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void RemoveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
         }
     }
 }
