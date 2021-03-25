a8e4014fbc3b01f21ae2d342f4135dc7e3ea80c3
273048269@qq.com <273048269@qq.com>
2021-03-25 09:16:12 +0800

    1、修改TOTP代码；2、简化用户认证窗口名称

--- Citta_T1/Business/WebsiteFeatureDetection/TOTP.cs
+++ Citta_T1/Business/WebsiteFeatureDetection/TOTP.cs
@@ -1,28 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace C2.Business.WebsiteFeatureDetection
 {
-    public static class TOTP
+    class TOTP
     {
-        private static readonly byte[] mapping = { 26, 27, 28, 29, 30, 255, 255, 255, 255, 255,
-                                        255, 255, 255, 255, 255, 0, 1, 2, 3, 4,
-                                        5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
-                                        15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
-                                        25 };
-        private const int InByteSize = 8;
-        private const int OutByteSize = 5;
-        private const string Base32Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
-
-        public static string GetHotp(string base32EncodedSecret, long counter)
+        private static TOTP TOTPInstance;
+        public static TOTP GetInstance()
         {
-            byte[] message = BitConverter.GetBytes(counter)
-                .Reverse().ToArray(); // Assuming Intel machine (little endian)
-            byte[] secret = base32EncodedSecret.ToByteArray();
+            if (TOTPInstance == null)
+            {
+                TOTPInstance = new TOTP();
+            }
+            return TOTPInstance;
+        }
+        public string GetTotp(string user)
+        {
+            string base32EncodedSecret = ToBase32String(Encoding.UTF8.GetBytes(user.ToUpper() + "qazwer!@$#")).Replace("=", "");
+            DateTime epochStart = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            long counter = (long)Math.Floor((DateTime.UtcNow - epochStart).TotalSeconds / 60);
+            return GetHotp(base32EncodedSecret, counter);
+        }
+
+        private string GetHotp(string base32EncodedSecret, long counter)
+        {
+            byte[] message = BitConverter.GetBytes(counter).Reverse().ToArray(); // Assuming Intel machine (little endian)
+            byte[] secret = ToByteArray(base32EncodedSecret);
 
             byte[] hash;
             using (HMACSHA1 hmac = new HMACSHA1(secret, true))
@@ -38,16 +43,14 @@ namespace C2.Business.WebsiteFeatureDetection
             return hotp.ToString("D6");
         }
 
-        public static string GetTotp(string user)
+        private byte[] ToByteArray(string secret)
         {
-            string base32EncodedSecret = ToBase32String(Encoding.UTF8.GetBytes(user.ToUpper() + "qazwer!@$#")).Replace("=", "");
-            DateTime epochStart = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-            long counter = (long)Math.Floor((DateTime.UtcNow - epochStart).TotalSeconds / 60);
-            return GetHotp(base32EncodedSecret, counter);
-        }
+            byte[] mapping = { 26, 27, 28, 29, 30, 255, 255, 255, 255, 255,
+                                        255, 255, 255, 255, 255, 0, 1, 2, 3, 4,
+                                        5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+                                        15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+                                        25 };
 
-        public static byte[] ToByteArray(this string secret)
-        {
             secret = secret.ToUpperInvariant();
             byte[] byteArray = new byte[(secret.Length + 7) / 8 * 5];
 
@@ -69,45 +72,46 @@ namespace C2.Business.WebsiteFeatureDetection
                 }
                 srcCounter = (srcCounter + 1) % 8;
             }
-
             return byteArray;
         }
 
-        static string ToBase32String(byte[] bytes)
+        private string ToBase32String(byte[] bytes)
         {
-            StringBuilder builder = new StringBuilder(bytes.Length * InByteSize / OutByteSize);
-
+            int inByteSize = 8;
+            int outByteSize = 5;
+            string base32Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
             int bytesPosition = 0;
             int bytesSubPosition = 0;
             byte outputBase32Byte = 0;
             int outputBase32BytePosition = 0;
 
+            StringBuilder builder = new StringBuilder(bytes.Length * inByteSize / outByteSize);
             while (bytesPosition < bytes.Length)
             {
-                int bitsAvailableInByte = Math.Min(InByteSize - bytesSubPosition, OutByteSize - outputBase32BytePosition);
+                int bitsAvailableInByte = Math.Min(inByteSize - bytesSubPosition, outByteSize - outputBase32BytePosition);
                 outputBase32Byte <<= bitsAvailableInByte;
-                outputBase32Byte |= (byte)(bytes[bytesPosition] >> (InByteSize - (bytesSubPosition + bitsAvailableInByte)));
+                outputBase32Byte |= (byte)(bytes[bytesPosition] >> (inByteSize - (bytesSubPosition + bitsAvailableInByte)));
                 bytesSubPosition += bitsAvailableInByte;
-                if (bytesSubPosition >= InByteSize)
+                if (bytesSubPosition >= inByteSize)
                 {
                     bytesPosition++;
                     bytesSubPosition = 0;
                 }
 
                 outputBase32BytePosition += bitsAvailableInByte;
-                if (outputBase32BytePosition >= OutByteSize)
+                if (outputBase32BytePosition >= outByteSize)
                 {
                     outputBase32Byte &= 0x1F;  // 0x1F = 00011111 in binary
-                    builder.Append(Base32Alphabet[outputBase32Byte]);
+                    builder.Append(base32Alphabet[outputBase32Byte]);
                     outputBase32BytePosition = 0;
                 }
             }
 
             if (outputBase32BytePosition > 0)
             {
-                outputBase32Byte <<= (OutByteSize - outputBase32BytePosition);
+                outputBase32Byte <<= (outByteSize - outputBase32BytePosition);
                 outputBase32Byte &= 0x1F;  // 0x1F = 00011111 in binary
-                builder.Append(Base32Alphabet[outputBase32Byte]);
+                builder.Append(base32Alphabet[outputBase32Byte]);
             }
 
             return builder.ToString();
