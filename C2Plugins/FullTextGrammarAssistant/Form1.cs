382adb1f0bdff47ee522582591cfc894eb4326ad
丽颖 文 <1395198665@qq.com>
2021-03-30 11:06:18 +0800

    IAO实验室添加全文语法助手图标

---
+++ C2Plugins/FullTextGrammarAssistant/Form1.cs
@@ -0,0 +1,51 @@
+﻿using C2.IAOLab.Plugins;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FullTextGrammarAssistant
+{
+    public partial class 全文语法查询助手 : Form, IPlugin
+    {
+        public 全文语法查询助手()
+        {
+            InitializeComponent();
+        }
+
+        public string GetPluginDescription()
+        {
+            return "全文语法助手";
+        }
+
+        public Image GetPluginImage()
+        {
+            return this.Icon.ToBitmap();
+        }
+
+        public string GetPluginName()
+        {
+            return "全文语法助手";
+        }
+
+        public string GetPluginVersion()
+        {
+            return "0.0.1";
+        }
+
+        public DialogResult ShowFormDialog()
+        {
+            return this.ShowDialog();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
