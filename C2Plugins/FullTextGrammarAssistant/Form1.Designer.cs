d215df718ce1f1ef75a27fd25b2eb5cc4f5e3076
丽颖 文 <1395198665@qq.com>
2021-04-09 08:52:48 +0800

    全文命令行生成

--- C2Plugins/FullTextGrammarAssistant/Form1.Designer.cs
+++ C2Plugins/FullTextGrammarAssistant/Form1.Designer.cs
@@ -74,6 +74,7 @@ namespace FullTextGrammarAssistant
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.checkBox14 = new System.Windows.Forms.CheckBox();
             this.panel3 = new System.Windows.Forms.Panel();
             this.panel4 = new System.Windows.Forms.Panel();
             this.panel5 = new System.Windows.Forms.Panel();
@@ -94,17 +95,23 @@ namespace FullTextGrammarAssistant
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.previewCmdGroup = new System.Windows.Forms.GroupBox();
+            this.previewCmdText = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
             this.panel4.SuspendLayout();
             this.panel5.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
+            this.previewCmdGroup.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(65, 40);
+            this.label1.Location = new System.Drawing.Point(97, 40);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(62, 18);
             this.label1.TabIndex = 1;
@@ -113,7 +120,7 @@ namespace FullTextGrammarAssistant
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(61, 95);
+            this.label2.Location = new System.Drawing.Point(79, 95);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(80, 18);
             this.label2.TabIndex = 4;
@@ -125,7 +132,7 @@ namespace FullTextGrammarAssistant
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(792, 28);
             this.textBox1.TabIndex = 5;
-            this.textBox1.Text = "hello AND world OR 你好";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label3
             // 
@@ -358,7 +365,7 @@ namespace FullTextGrammarAssistant
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(43, 331);
+            this.label4.Location = new System.Drawing.Point(43, 334);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(116, 18);
             this.label4.TabIndex = 16;
@@ -441,7 +448,7 @@ namespace FullTextGrammarAssistant
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(61, 814);
+            this.label5.Location = new System.Drawing.Point(65, 814);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(98, 18);
             this.label5.TabIndex = 20;
@@ -556,7 +563,7 @@ namespace FullTextGrammarAssistant
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(29, 913);
+            this.label8.Location = new System.Drawing.Point(29, 909);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(134, 18);
             this.label8.TabIndex = 36;
@@ -564,7 +571,7 @@ namespace FullTextGrammarAssistant
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(732, 1002);
+            this.button1.Location = new System.Drawing.Point(737, 1188);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 40);
             this.button1.TabIndex = 37;
@@ -573,7 +580,7 @@ namespace FullTextGrammarAssistant
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(889, 1002);
+            this.button2.Location = new System.Drawing.Point(889, 1188);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 40);
             this.button2.TabIndex = 38;
@@ -582,6 +589,7 @@ namespace FullTextGrammarAssistant
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.checkBox14);
             this.panel2.Controls.Add(this.checkBox16);
             this.panel2.Controls.Add(this.checkBox19);
             this.panel2.Controls.Add(this.checkBox2);
@@ -603,6 +611,18 @@ namespace FullTextGrammarAssistant
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(807, 119);
             this.panel2.TabIndex = 41;
+            // 
+            // checkBox14
+            // 
+            this.checkBox14.AutoSize = true;
+            this.checkBox14.Checked = true;
+            this.checkBox14.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox14.Location = new System.Drawing.Point(701, 94);
+            this.checkBox14.Name = "checkBox14";
+            this.checkBox14.Size = new System.Drawing.Size(70, 22);
+            this.checkBox14.TabIndex = 23;
+            this.checkBox14.Text = "语音";
+            this.checkBox14.UseVisualStyleBackColor = true;
             // 
             // panel3
             // 
@@ -637,7 +657,7 @@ namespace FullTextGrammarAssistant
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(473, 3);
+            this.textBox6.Location = new System.Drawing.Point(470, 3);
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(253, 28);
             this.textBox6.TabIndex = 46;
@@ -653,8 +673,6 @@ namespace FullTextGrammarAssistant
             this.comboBox5.Name = "comboBox5";
             this.comboBox5.Size = new System.Drawing.Size(94, 26);
             this.comboBox5.TabIndex = 42;
-            this.comboBox5.SelectedIndex = comboBox5.Items.IndexOf("AND");
-
             // 
             // comboBox3
             // 
@@ -677,7 +695,6 @@ namespace FullTextGrammarAssistant
             this.comboBox3.Name = "comboBox3";
             this.comboBox3.Size = new System.Drawing.Size(219, 26);
             this.comboBox3.TabIndex = 18;
-            this.comboBox3.SelectedIndex = comboBox3.Items.IndexOf("上网账号");
             // 
             // comboBox4
             // 
@@ -694,7 +711,6 @@ namespace FullTextGrammarAssistant
             this.comboBox4.Name = "comboBox4";
             this.comboBox4.Size = new System.Drawing.Size(135, 26);
             this.comboBox4.TabIndex = 19;
-            this.comboBox4.SelectedIndex = comboBox4.Items.IndexOf("精确匹配");
             // 
             // textBox5
             // 
@@ -714,7 +730,6 @@ namespace FullTextGrammarAssistant
             this.comboBox6.Name = "comboBox6";
             this.comboBox6.Size = new System.Drawing.Size(94, 26);
             this.comboBox6.TabIndex = 43;
-            this.comboBox6.SelectedIndex = comboBox6.Items.IndexOf("NOT");
             // 
             // comboBox8
             // 
@@ -727,11 +742,10 @@ namespace FullTextGrammarAssistant
             "等于",
             "小于",
             "小于等于"});
-            this.comboBox8.Location = new System.Drawing.Point(333, 3);
+            this.comboBox8.Location = new System.Drawing.Point(330, 3);
             this.comboBox8.Name = "comboBox8";
             this.comboBox8.Size = new System.Drawing.Size(134, 26);
             this.comboBox8.TabIndex = 45;
-            this.comboBox8.SelectedIndex = comboBox8.Items.IndexOf("模糊匹配");
             // 
             // comboBox7
             // 
@@ -752,9 +766,8 @@ namespace FullTextGrammarAssistant
             "上级网址"});
             this.comboBox7.Location = new System.Drawing.Point(105, 3);
             this.comboBox7.Name = "comboBox7";
-            this.comboBox7.Size = new System.Drawing.Size(220, 26);
+            this.comboBox7.Size = new System.Drawing.Size(219, 26);
             this.comboBox7.TabIndex = 44;
-            this.comboBox7.SelectedIndex = comboBox7.Items.IndexOf("源IP");
             // 
             // textBox3
             // 
@@ -762,7 +775,6 @@ namespace FullTextGrammarAssistant
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(360, 28);
             this.textBox3.TabIndex = 50;
-            this.textBox3.Text = "2021/4/7 15:50:27";
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // textBox4
@@ -771,7 +783,7 @@ namespace FullTextGrammarAssistant
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(360, 28);
             this.textBox4.TabIndex = 51;
-            this.textBox4.Text = "2020/4/7 15:50:27";
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
             // 
             // comboBox2
             // 
@@ -788,7 +800,6 @@ namespace FullTextGrammarAssistant
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(135, 26);
             this.comboBox2.TabIndex = 18;
-            this.comboBox2.SelectedIndex = comboBox2.Items.IndexOf("大于");
             // 
             // textBox2
             // 
@@ -818,7 +829,6 @@ namespace FullTextGrammarAssistant
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(326, 26);
             this.comboBox1.TabIndex = 17;
-            this.comboBox1.SelectedIndex = comboBox1.Items.IndexOf("文件大小（单位：字节）");
             // 
             // tableLayoutPanel1
             // 
@@ -828,7 +838,7 @@ namespace FullTextGrammarAssistant
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 140F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 263F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 53F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 52F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 55F));
             this.tableLayoutPanel1.Controls.Add(this.comboBox7, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.textBox6, 3, 0);
             this.tableLayoutPanel1.Controls.Add(this.comboBox8, 2, 0);
@@ -845,9 +855,9 @@ namespace FullTextGrammarAssistant
             // button3
             // 
             this.button3.Image = global::FullTextGrammarAssistant.Properties.Resources.加;
-            this.button3.Location = new System.Drawing.Point(736, 3);
+            this.button3.Location = new System.Drawing.Point(733, 3);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(28, 28);
+            this.button3.Size = new System.Drawing.Size(30, 30);
             this.button3.TabIndex = 47;
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
@@ -855,9 +865,9 @@ namespace FullTextGrammarAssistant
             // button4
             // 
             this.button4.Image = global::FullTextGrammarAssistant.Properties.Resources.减;
-            this.button4.Location = new System.Drawing.Point(789, 3);
+            this.button4.Location = new System.Drawing.Point(786, 3);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(28, 28);
+            this.button4.Size = new System.Drawing.Size(30, 30);
             this.button4.TabIndex = 48;
             this.button4.UseVisualStyleBackColor = true;
             // 
@@ -869,12 +879,67 @@ namespace FullTextGrammarAssistant
             this.tableLayoutPanel2.Size = new System.Drawing.Size(839, 52);
             this.tableLayoutPanel2.TabIndex = 54;
             // 
+            // previewCmdGroup
+            // 
+            this.previewCmdGroup.Controls.Add(this.previewCmdText);
+            this.previewCmdGroup.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.previewCmdGroup.Location = new System.Drawing.Point(157, 903);
+            this.previewCmdGroup.Margin = new System.Windows.Forms.Padding(2);
+            this.previewCmdGroup.Name = "previewCmdGroup";
+            this.previewCmdGroup.Padding = new System.Windows.Forms.Padding(2);
+            this.previewCmdGroup.Size = new System.Drawing.Size(779, 120);
+            this.previewCmdGroup.TabIndex = 55;
+            this.previewCmdGroup.TabStop = false;
+            this.previewCmdGroup.Text = "query";
+            // 
+            // previewCmdText
+            // 
+            this.previewCmdText.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.previewCmdText.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.previewCmdText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.previewCmdText.Location = new System.Drawing.Point(2, 26);
+            this.previewCmdText.Margin = new System.Windows.Forms.Padding(2);
+            this.previewCmdText.Multiline = true;
+            this.previewCmdText.Name = "previewCmdText";
+            this.previewCmdText.ReadOnly = true;
+            this.previewCmdText.Size = new System.Drawing.Size(775, 92);
+            this.previewCmdText.TabIndex = 2;
+            this.previewCmdText.TextChanged += new System.EventHandler(this.previewCmdText_TextChanged);
+            // 
+            // textBox7
+            // 
+            this.textBox7.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.textBox7.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox7.Location = new System.Drawing.Point(2, 26);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox7.Multiline = true;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(775, 92);
+            this.textBox7.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox7);
+            this.groupBox1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox1.Location = new System.Drawing.Point(157, 1031);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(779, 120);
+            this.groupBox1.TabIndex = 56;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "jar";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(1023, 1050);
+            this.ClientSize = new System.Drawing.Size(1116, 1050);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.previewCmdGroup);
             this.Controls.Add(this.tableLayoutPanel2);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.textBox4);
@@ -923,6 +988,10 @@ namespace FullTextGrammarAssistant
             this.panel5.PerformLayout();
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
+            this.previewCmdGroup.ResumeLayout(false);
+            this.previewCmdGroup.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -992,6 +1061,11 @@ namespace FullTextGrammarAssistant
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox previewCmdGroup;
+        private System.Windows.Forms.TextBox previewCmdText;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox14;
     }
 }
 
