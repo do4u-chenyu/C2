5bab7f3d0952613cb248cd02fd0e2071c60c1531
735946247@qq.com <735946247@qq.com>
2021-04-09 17:01:33 +0800

    wifimacbug修改

---
+++ Citta_T1/Business/IAOLab/WifiMac/Place.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C2.Business.IAOLab.WifiMac
+{
+    class Place
+    {
+        public string address { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string latitude { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string accuracy { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string state { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string tgdid { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string longitude { get; set; }
+        ///
+    }
+}
