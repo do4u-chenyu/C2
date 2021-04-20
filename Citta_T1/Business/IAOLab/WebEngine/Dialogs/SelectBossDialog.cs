9423580fe68bf548e462738067a51bb003599fd2
phoenix1214 <273048269@qq.com>
2021-04-20 18:07:05 +0800

    1、修复数据大屏同名列生成datatable出错的问题；2、优化数据大屏IE版本过低时提示（待测试）

--- Citta_T1/Business/IAOLab/WebEngine/Dialogs/SelectBossDialog.cs
+++ Citta_T1/Business/IAOLab/WebEngine/Dialogs/SelectBossDialog.cs
@@ -41,13 +41,9 @@ namespace C2.IAOLab.WebEngine.Dialogs
             LoadOption();
             webBrowser = browser;
         }
-        public void Clear()
-        {
-            foreach (var bitmap in this.bossTypeDict)
-                bitmap.Dispose();
-            this.bossTypeDict.Clear();
-        }
-        //图表关联，保存配置
+
+
+        #region 保存加载配置
         private void SavaOption()
         {
             SavaDataOption("Datasource", datasource.SelectedIndex);
@@ -62,33 +58,6 @@ namespace C2.IAOLab.WebEngine.Dialogs
             SaveChartOption("BasicMap", basicMapX.SelectedIndex, new List<int>() { basicMapY.SelectedIndex });
         }
 
-        private void LoadOption()
-        {
-            LoadData();
-            LoadChartOptions();
-        }
-
-        private void LoadChartOptions()
-        {
-            //加载数据源、类型加载图表配置
-            LoadChartOption("SimpleBar", simpleBarX, simpleBarY);
-            LoadChartOption("BasicLineChart", basicLineChartX, basicLineChartY);
-            LoadChartOption("BasicScatter", basicScatterX, basicScatterY);
-            LoadChartOption("GradientLineChart", gradientLineChartX, gradientLineChartY);
-            LoadChartOption("StackBar", stackBarX, stackBarY);
-            LoadChartOption("BasicPie", basicPieX, basicPieY);
-            LoadChartOption("PictorialBar", pictorialBarX, pictorialBarY);
-            LoadChartOption("BasicMap", basicMapX, basicMapY);
-        }
-        protected override bool OnOKButtonClick()
-        {
-            //前100行所有列的数据生成datatable、图表配置项生成chartOptions、生成js
-            DataTable dataTable = GenDataTable();
-            SavaOption();
-            GenBossHtml.GetInstance().TransDataToHtml(dataTable, ChartOptions);
-
-            return base.OnOKButtonClick();
-        }
         private void SavaDataOption(string dataType, int idx)
         {
             ChartOptions[dataType] = new int[] { idx };
@@ -107,6 +76,13 @@ namespace C2.IAOLab.WebEngine.Dialogs
             else
                 ChartOptions.Add(chartType, tmpList.ToArray());
         }
+
+        private void LoadOption()
+        {
+            LoadData();
+            LoadChartOptions();
+        }
+
         private void LoadData()
         {
             //初始化大屏类型
@@ -122,7 +98,7 @@ namespace C2.IAOLab.WebEngine.Dialogs
             }
             if (DataItems.Count == 0)//如果节点没数据，直接返回
                 return;
-            else if(!ChartOptions.ContainsKey("Datasource") || ChartOptions["Datasource"].Length == 0)//ChartOptions未配置，默认展示第一个文件的第1列
+            else if (!ChartOptions.ContainsKey("Datasource") || ChartOptions["Datasource"].Length == 0)//ChartOptions未配置，默认展示第一个文件的第1列
                 selectData = DataItems[0];
             else
                 selectData = DataItems[ChartOptions["Datasource"][0]];
@@ -131,15 +107,30 @@ namespace C2.IAOLab.WebEngine.Dialogs
             this.bcpInfo = new BcpInfo(selectData.FilePath, selectData.FileEncoding, new char[] { selectData.FileSep });
         }
 
+        private void LoadChartOptions()
+        {
+            //加载数据源、类型加载图表配置
+            LoadChartOption("SimpleBar", simpleBarX, simpleBarY);
+            LoadChartOption("BasicLineChart", basicLineChartX, basicLineChartY);
+            LoadChartOption("BasicScatter", basicScatterX, basicScatterY);
+            LoadChartOption("GradientLineChart", gradientLineChartX, gradientLineChartY);
+            LoadChartOption("StackBar", stackBarX, stackBarY);
+            LoadChartOption("BasicPie", basicPieX, basicPieY);
+            LoadChartOption("PictorialBar", pictorialBarX, pictorialBarY);
+            LoadChartOption("BasicMap", basicMapX, basicMapY);
+        }
+
         private void LoadChartOption(string chartType, Control controlX, Control controlY)
         {
-            int defaultIdx = 0;
-            int[] tmpIdx;
+
             if (bcpInfo == null || bcpInfo.ColumnArray.Length == 0)
                 return;
 
+            int defaultXIdx = 0;
+            int defaultYIdx = bcpInfo.ColumnArray.Length == 1 ? 0 : 1;
+            int[] tmpIdx;
             if (!ChartOptions.ContainsKey(chartType) || ChartOptions["Datasource"].Length == 0)
-                tmpIdx = new int[] { defaultIdx, defaultIdx};
+                tmpIdx = new int[] { defaultXIdx, defaultYIdx };
             else
                 tmpIdx = ChartOptions[chartType];
 
@@ -156,42 +147,71 @@ namespace C2.IAOLab.WebEngine.Dialogs
                 (controlY as ComCheckBoxList).LoadItemCheckIndex(tmpIdx.Skip(1).ToArray());
             }
         }
+        #endregion
+
+        #region 事件
+        protected override bool OnOKButtonClick()
+        {
+            //前100行所有列的数据生成datatable、图表配置项生成chartOptions、生成js
+            TranDataToHtml();
+            return base.OnOKButtonClick();
+        }
 
         private void Datasource_SelectedIndexChanged(object sender, EventArgs e)
         {
             selectData = DataItems[this.datasource.SelectedIndex];
             this.bcpInfo = new BcpInfo(selectData.FilePath, selectData.FileEncoding, new char[] { selectData.FileSep });
             ChangeControlContent();
-            if(oldDataIdx != -1)
+            if (oldDataIdx != -1)
                 ChartOptions = new Dictionary<string, int[]>();
             oldDataIdx = datasource.SelectedIndex;
             LoadChartOptions();
         }
 
-        private void ChangeControlContent()
+        private void BossType_SelectedIndexChanged(object sender, EventArgs e)
         {
-            foreach (Control panel in this.panel1.Controls)
-            {
-                if (!(panel is Panel))
-                    continue;
-                foreach(Control ct in panel.Controls)
-                {
-                    if (ct is ComboBox && (ct.Name.EndsWith("X") || ct.Name.EndsWith("Y")))
-                    {
-                        (ct as ComboBox).Text = string.Empty;
-                        (ct as ComboBox).Items.Clear();
-                        (ct as ComboBox).Items.AddRange(bcpInfo.ColumnArray);
-                    }
-                    else if (ct is ComCheckBoxList)
-                    {
-                        (ct as ComCheckBoxList).ClearText();
-                        (ct as ComCheckBoxList).Items.Clear();
-                        (ct as ComCheckBoxList).Items.AddRange(bcpInfo.ColumnArray);
-                    }
-                }
-            }
+            //根据选中的模板更新界面
+            BossTemplate selectBossTemplate = BossTemplateCollection.GetInstance().GetTemplateByIdx(bossType.SelectedIndex);
 
+            this.pictureBox1.Image = bossTypeDict[bossType.SelectedIndex];
+            this.label19.Text = bossType.Text.Substring(bossType.Text.IndexOf("（") + 1, bossType.Text.IndexOf("）") - 1 - bossType.Text.IndexOf("（"));
+            this.WebUrl = Path.Combine(Application.StartupPath, "Business\\IAOLab\\WebEngine\\Html", string.Format("BossIndex{0}.html", (bossType.SelectedIndex + 1).ToString()));
+            ChangeConfigContent(selectBossTemplate);
         }
+
+        private async void PreviewBtn_ClickAsync(object sender, EventArgs e)
+        {
+            TranDataToHtml();
+            webBrowser.Navigate(WebUrl);
+
+            this.Cursor = Cursors.WaitCursor;
+            await PageLoad(10);
+            this.Cursor = Cursors.Default;
+
+            Bitmap bitmap = new Bitmap(webBrowser.Width, webBrowser.Height);
+            Rectangle rectangle = new Rectangle(0, 0, webBrowser.Width, webBrowser.Height);  // 绘图区域
+            webBrowser.DrawToBitmap(bitmap, rectangle);
+            this.pictureBox1.Image = bitmap;
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            Enlarge();
+        }
+
+        private void ZoomInBtn_Click(object sender, EventArgs e)
+        {
+            Enlarge();
+        }
+        #endregion
+        
+        private void TranDataToHtml()
+        {
+            DataTable dataTable = GenDataTable();
+            SavaOption();
+            GenBossHtml.GetInstance().TransDataToHtml(dataTable, ChartOptions);
+        }
+
         private DataTable GenDataTable()
         {
             string fileContent;
@@ -199,8 +219,23 @@ namespace C2.IAOLab.WebEngine.Dialogs
 
             if (bcpInfo == null || bcpInfo.ColumnArray.IsEmpty())
                 return dataTable;
+
+
+            // 可能有同名列，这里需要重命名一下
+            Dictionary<string, int> induplicatedName = new Dictionary<string, int>() { };
             foreach (string col in bcpInfo.ColumnArray)
-                dataTable.Columns.Add(col);
+            {
+                if (!induplicatedName.ContainsKey(col))
+                {
+                    induplicatedName.Add(col, 0);
+                    dataTable.Columns.Add(col);
+                } 
+                else
+                {
+                    induplicatedName[col] += 1;
+                    dataTable.Columns.Add(col + "_" + induplicatedName[col]);
+                } 
+            }    
 
             if (selectData.FileType == OpUtil.ExtType.Excel)
                 fileContent = BCPBuffer.GetInstance().GetCachePreviewExcelContent(selectData.FilePath);
@@ -222,34 +257,28 @@ namespace C2.IAOLab.WebEngine.Dialogs
             }
             return dataTable;
         }
-
-        void Enlarge()
+        private void ChangeControlContent()
         {
-            var image = this.pictureBox1.Image;
-            if (image == null)
-                return;
-
-            var dialog = new C2.Dialogs.PictureViewDialog(image);
-            dialog.ImageName = this.bossType.Text;
-            dialog.SetZoomType(ZoomType.FitPage);
-            dialog.ShowDialog();
-
-        }
-
-        private void PictureBox1_Click(object sender, EventArgs e)
-        {
-            Enlarge();
-        }
-
-        private void BossType_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            //根据选中的模板更新界面
-            BossTemplate selectBossTemplate = BossTemplateCollection.GetInstance().GetTemplateByIdx(bossType.SelectedIndex);
-
-            this.pictureBox1.Image = bossTypeDict[bossType.SelectedIndex];
-            this.label19.Text = bossType.Text.Substring(bossType.Text.IndexOf("（") + 1, bossType.Text.IndexOf("）") - 1 - bossType.Text.IndexOf("（"));
-            this.WebUrl = Path.Combine(Application.StartupPath, "Business\\IAOLab\\WebEngine\\Html", string.Format("BossIndex{0}.html", (bossType.SelectedIndex + 1).ToString()));
-            ChangeConfigContent(selectBossTemplate);
+            foreach (Control panel in this.panel1.Controls)
+            {
+                if (!(panel is Panel))
+                    continue;
+                foreach (Control ct in panel.Controls)
+                {
+                    if (ct is ComboBox && (ct.Name.EndsWith("X") || ct.Name.EndsWith("Y")))
+                    {
+                        (ct as ComboBox).Text = string.Empty;
+                        (ct as ComboBox).Items.Clear();
+                        (ct as ComboBox).Items.AddRange(bcpInfo.ColumnArray);
+                    }
+                    else if (ct is ComCheckBoxList)
+                    {
+                        (ct as ComCheckBoxList).ClearText();
+                        (ct as ComCheckBoxList).Items.Clear();
+                        (ct as ComCheckBoxList).Items.AddRange(bcpInfo.ColumnArray);
+                    }
+                }
+            }
         }
 
         private void ChangeConfigContent(BossTemplate bossTemplate)
@@ -279,23 +308,6 @@ namespace C2.IAOLab.WebEngine.Dialogs
             }
         }
 
-        private async void PreviewBtn_ClickAsync(object sender, EventArgs e)
-        {
-            DataTable dataTable = GenDataTable();
-            SavaOption();
-            GenBossHtml.GetInstance().TransDataToHtml(dataTable, ChartOptions);
-            webBrowser.Navigate(WebUrl);
-
-            this.Cursor = Cursors.WaitCursor;
-            await PageLoad(10);
-            this.Cursor = Cursors.Default;
-
-            Bitmap bitmap = new Bitmap(webBrowser.Width, webBrowser.Height);
-            Rectangle rectangle = new Rectangle(0, 0, webBrowser.Width, webBrowser.Height);  // 绘图区域
-            webBrowser.DrawToBitmap(bitmap, rectangle);
-            this.pictureBox1.Image = bitmap;
-        }
-
         private async Task PageLoad(int TimeOut)
         {
             TaskCompletionSource<bool> PageLoaded = null;
@@ -319,9 +331,24 @@ namespace C2.IAOLab.WebEngine.Dialogs
             }
         }
 
-        private void ZoomInBtn_Click(object sender, EventArgs e)
+        void Enlarge()
         {
-            Enlarge();
+            var image = this.pictureBox1.Image;
+            if (image == null)
+                return;
+
+            var dialog = new C2.Dialogs.PictureViewDialog(image);
+            dialog.ImageName = this.bossType.Text;
+            dialog.SetZoomType(ZoomType.FitPage);
+            dialog.ShowDialog();
+
         }
+        public void Clear()
+        {
+            foreach (var bitmap in this.bossTypeDict)
+                bitmap.Dispose();
+            this.bossTypeDict.Clear();
+        }
+
     }
 }
