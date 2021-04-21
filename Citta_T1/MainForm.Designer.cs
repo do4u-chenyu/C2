55eee699ea9afe24be74203b26e8fefb0d20a725
lu-qi123 <18705158378@163.com>
2021-04-21 15:17:00 +0800

    小修改

--- Citta_T1/MainForm.Designer.cs
+++ Citta_T1/MainForm.Designer.cs
@@ -187,9 +187,9 @@ namespace C2
             this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(8, 4);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(228, 36);
+            this.label1.Size = new System.Drawing.Size(255, 36);
             this.label1.TabIndex = 0;
-            this.label1.Text = "IAO单兵作战平台";
+            this.label1.Text = "IAO单兵作战工具箱";
             // 
             // leftMainMenuPanel
             // 
