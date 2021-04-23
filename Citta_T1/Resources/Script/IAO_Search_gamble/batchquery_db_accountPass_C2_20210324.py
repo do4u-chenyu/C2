8f75a92667125822c9542075e6553fa77f1618e3
735946247@qq.com <735946247@qq.com>
2021-04-23 09:31:58 +0800

    全文脚本阿克苏问题修复

--- Citta_T1/Resources/Script/IAO_Search_gamble/batchquery_db_accountPass_C2_20210324.py
+++ Citta_T1/Resources/Script/IAO_Search_gamble/batchquery_db_accountPass_C2_20210324.py
@@ -90,7 +90,7 @@ class Query(Thread):
             '--start', startTime,
             '--end', endTime,
             '--contextlen', '1000',
-            '--datatype', 'normal,garbage',
+            '--datatype', 'all',
             '--maxcount', '2147483647'
         ]
         if '=' not in key_word:
