7dd986c88a8d347f96186536b16f12197878f183
735946247@qq.com <735946247@qq.com>
2021-06-04 15:22:43 +0800

    asp导出word完成模板修改

--- Citta_T1/Model/MindMaps/tmpDocxFile.cs
+++ Citta_T1/Model/MindMaps/tmpDocxFile.cs
@@ -16,7 +16,7 @@ namespace C2.Model.MindMaps
     {
         private int imgNo;
         private List<string> unsucceedAttachment = new List<string>();
-        private void WriteNoteToDocx(Widget topicNote, Document docx)
+        private void WriteNoteToDocx(Widget topicNote, Document docx, StyleCollection styles)
         {
             if (topicNote != null)
             {
@@ -27,24 +27,27 @@ namespace C2.Model.MindMaps
                 foreach (string paragraph in paragraphs)
                 {
                     if (paragraph != string.Empty)
-                        WriteParagraph(paragraph, docx);
+                        WriteParagraph(paragraph, docx, styles);
                 }
                 topicNote.Remark = relRemark;
             }
         }
-        private void WriteParagraph(string text, Document docx)
+        private void WriteParagraph(string text, Document docx, StyleCollection styles)
         {
             DocumentBuilder builder = new DocumentBuilder(docx);
             builder.MoveToDocumentEnd();
-            builder.Font.Size = 10.5;
-            //builder.Font.Bold = true;
-            builder.Font.Name = "宋体";
-            builder.Font.Name = "TimesNewRoma";
-            builder.ParagraphFormat.Alignment = ParagraphAlignment.Left;
-            builder.ParagraphFormat.LineSpacing = 18;
+            //builder.Font.Size = 10.5;
+            ////builder.Font.Bold = true;
+            //builder.Font.Name = "宋体";
+            //builder.Font.Name = "Times New Roman";
+            //builder.ParagraphFormat.Alignment = ParagraphAlignment.Justify;
+            //builder.ParagraphFormat.LineSpacing = 18;
+            //builder.Font.Bold = false;
+            
+            builder.ParagraphFormat.Style = builder.ParagraphFormat.Style.Styles["IAO正文"];
             builder.ParagraphFormat.FirstLineIndent = 21;
-            builder.Font.Bold = false;
             builder.Writeln(text);
+            builder.ParagraphFormat.FirstLineIndent = 0;
         }
         private Size RotateImageSize(int width, int height)
         {
@@ -56,7 +59,7 @@ namespace C2.Model.MindMaps
             string picturePath = pictureWidget.ImageUrl;
             if (!File.Exists(picturePath))
                 return;
-            string fileName = Path.GetFileName(picturePath);
+            string fileName = Path.GetFileNameWithoutExtension(picturePath);
             try
             {
                 DocumentBuilder builder = new DocumentBuilder(docx);
@@ -69,12 +72,14 @@ namespace C2.Model.MindMaps
                 builder.InsertImage(picturePath,size.Width,size.Height);
                 builder.Writeln("");//把光标移动到下一行
 
-                builder.Font.Bold = false;
-                builder.Font.Name = "宋体";
-                builder.Font.Name = "TimesNewRoma";
-                builder.Font.Size = 9;
-                builder.ParagraphFormat.Alignment = ParagraphAlignment.Center;
+                //builder.Font.Bold = false;
+                //builder.Font.Name = "宋体";
+                //builder.Font.Name = "Times New Roman";
+                //builder.Font.Size = 9;
+                //builder.ParagraphFormat.Alignment = ParagraphAlignment.Center;
+                builder.ParagraphFormat.Style = builder.ParagraphFormat.Style.Styles["tips"];
                 builder.Writeln("图" + (imgNo + 1) + ":" + fileName);
+                builder.ParagraphFormat.ClearFormatting();
 
             }
             catch (Exception ex)
@@ -84,57 +89,72 @@ namespace C2.Model.MindMaps
 
         }
 
-        private void WriteTitleToDocx(string title, Document docx, int layer, string serialNumber)
+        private void WriteTitleToDocx(string title, Document docx, int layer, string serialNumber, StyleCollection styles)
         {
             DocumentBuilder builder = new DocumentBuilder(docx);
             builder.MoveToDocumentEnd();
             switch (layer)
             {
                 case 1:
-                    builder.Font.Size = 20;
-                    builder.Font.Bold = true;
-                    builder.Font.Name = "黑体";
-                    builder.ParagraphFormat.Alignment = ParagraphAlignment.Center;
-                    builder.ParagraphFormat.LineSpacing = 18;
+                    //builder.Font.Size = 20;
+                    //builder.Font.Bold = true;
+                    //builder.Font.Name = "黑体";
+                    //builder.ParagraphFormat.Alignment = ParagraphAlignment.Center;
+                    //builder.ParagraphFormat.LineSpacing = 18;
+                    builder.ParagraphFormat.Style.Styles.AddCopy(styles["Heading 1"]);
+                    builder.ParagraphFormat.Style.Styles.AddCopy(styles["Heading 2"]);
+                    builder.ParagraphFormat.Style.Styles.AddCopy(styles["Heading 3"]);
+                    builder.ParagraphFormat.Style.Styles.AddCopy(styles["Heading 4"]);
+                    builder.ParagraphFormat.Style.Styles.AddCopy(styles["IAO正文"]);
+                    builder.ParagraphFormat.Style.Styles.AddCopy(styles["tips"]);
+                    builder.ParagraphFormat.Style = builder.ParagraphFormat.Style.Styles["Heading 1"];
                     builder.Writeln(title);
                     break;
                 case 2:
-                    builder.Font.Size = 16;
-                    builder.Font.Bold = true;
-                    builder.ParagraphFormat.Alignment = ParagraphAlignment.Left;
-                    builder.ParagraphFormat.LineSpacing = 18;
-                    builder.ParagraphFormat.FirstLineIndent = 0;
-                    builder.Font.Name = "黑体";
+                    //builder.Font.Size = 16;
+                    //builder.Font.Bold = true;
+                    //builder.ParagraphFormat.Alignment = ParagraphAlignment.Justify;
+                    //builder.ParagraphFormat.LineSpacing = 18;
+                    //builder.ParagraphFormat.FirstLineIndent = 0;
+                    //builder.Font.Name = "黑体";
+                    
+                    builder.ParagraphFormat.Style = builder.ParagraphFormat.Style.Styles["Heading 2"];
                     builder.Writeln(string.Format("{0} {1}", serialNumber, title));
                     break;
                 case 3:
-                    builder.Font.Size = 14;
-                    builder.Font.Bold = true;
-                    builder.Font.Name = "宋体";
-                    builder.Font.Name = "TimesNewRoma";
-                    builder.ParagraphFormat.FirstLineIndent = 0;
-                    builder.ParagraphFormat.Alignment = ParagraphAlignment.Left;
-                    builder.ParagraphFormat.LineSpacing = 18;
+                    //builder.Font.Size = 14;
+                    //builder.Font.Bold = true;
+                    //builder.Font.Name = "宋体";
+                    //builder.Font.Name = "Times New Roman";
+                    //builder.ParagraphFormat.FirstLineIndent = 0;
+                    //builder.ParagraphFormat.Alignment = ParagraphAlignment.Justify;
+                    //builder.ParagraphFormat.LineSpacing = 18;
+                    
+                    builder.ParagraphFormat.Style = builder.ParagraphFormat.Style.Styles["Heading 3"];
                     builder.Writeln(string.Format("{0} {1}", serialNumber, title));
                     break;
                 case 4:
-                    builder.Font.Size = 10.5;
-                    builder.Font.Bold = true;
-                    builder.Font.Name = "宋体";
-                    builder.Font.Name = "TimesNewRoma";
-                    builder.ParagraphFormat.FirstLineIndent = 0;
-                    builder.ParagraphFormat.LineSpacing = 18;
-                    builder.ParagraphFormat.Alignment = ParagraphAlignment.Left;
+                    //builder.Font.Size = 10.5;
+                    //builder.Font.Bold = true;
+                    //builder.Font.Name = "宋体";
+                    //builder.Font.Name = "Times New Roman";
+                    //builder.ParagraphFormat.FirstLineIndent = 0;
+                    //builder.ParagraphFormat.LineSpacing = 18;
+                    //builder.ParagraphFormat.Alignment = ParagraphAlignment.Justify;
+                   
+                    builder.ParagraphFormat.Style = builder.ParagraphFormat.Style.Styles["Heading 4"];
                     builder.Writeln(string.Format("{0} {1}", serialNumber, title));
                     break;
                 default:
-                    builder.Font.Size = 10.5;
-                    builder.Font.Bold = false;
-                    builder.Font.Name = "宋体";
-                    builder.Font.Name = "TimesNewRoma";
-                    builder.ParagraphFormat.LineSpacing = 18;
-                    builder.ParagraphFormat.Alignment = ParagraphAlignment.Left;
-                    builder.ParagraphFormat.FirstLineIndent = 21;
+                    //builder.Font.Size = 10.5;
+                    //builder.Font.Bold = false;
+                    //builder.Font.Name = "宋体";
+                    //builder.Font.Name = "Times New Roman";
+                    //builder.ParagraphFormat.LineSpacing = 18;
+                    //builder.ParagraphFormat.Alignment = ParagraphAlignment.Justify;
+                    //builder.ParagraphFormat.FirstLineIndent = 21;
+                    builder.ParagraphFormat.Style.Styles.AddCopy(styles["IAO正文"]);
+                    builder.ParagraphFormat.Style = builder.ParagraphFormat.Style.Styles["IAO正文"];
                     builder.Writeln(string.Format("{0} {1}", serialNumber, title));
                     break;
             }
@@ -166,13 +186,13 @@ namespace C2.Model.MindMaps
                     byte[] bs = File.ReadAllBytes(path);
                     using (Stream stream = new MemoryStream(bs))
                     {
+                        builder.ParagraphFormat.Style = builder.ParagraphFormat.Style.Styles["tips"];
                         Image image = GetImage(path);
                         Shape shape = builder.InsertOleObject(stream, "Package", true, image);
                         OlePackage olePackage = shape.OleFormat.OlePackage;
                         string name = Path.GetFileName(path);
                         olePackage.FileName = string.Format("{0}.{1}{2}",layer,i, Path.GetExtension(path));
                         i++;
-                        olePackage.DisplayName = "1.txt";
                     }
                 }
                 catch(Exception ex)
@@ -212,51 +232,65 @@ namespace C2.Model.MindMaps
             var length = (UInt32)name.Length;
             int chinese = 0;
             int l = 0;
-            StringBuilder sb = new StringBuilder();
-            string abbreviate = "文件名超长";
+            StringBuilder ab = new StringBuilder();
+            StringBuilder left = new StringBuilder();
+            string firstName = "文件名超长";
             for (int i = 0; i < name.Length; i++)
             {
                 char j = name[i];
                 ushort s = j;
                 if (s >= 0x4E00 && s <= 0x9FA5)
                 {
-                    sb.Append(name[i]);
+                    ab.Append(name[i]);
                     l += 2;
                     chinese++;
-                    if (l == 18 || l == 17)
-                        abbreviate = sb.ToString();
+                    if (l > 20)
+                    {
+                        firstName = ab.ToString();
+                        left.Append(name[i]);
+                    }
                 }
                 else 
                 {
-                    sb.Append(name[i]);
+                    ab.Append(name[i]);
                     l += 1;
-                    if (l == 18 || l == 17)
-                        abbreviate = sb.ToString();
+                    if (l > 20)
+                    {
+                        firstName = ab.ToString();
+                        left.Append(name[i]);
+                    }
                 }
             }
+            string leftname = left.ToString();
             int wordLength = (int)length + chinese;//一个字符4.5个像素
             double addLength = 45;
             if (wordLength >= 20) 
             {
-                addLength = 90;
-                name = abbreviate + "...";
-            }                
-            if(wordLength < 3)
+                addLength = 92;
+            }
+            if (wordLength < 3)
+            {
+                firstName = name;
                 addLength = 22;
+            }
             if(3 <= wordLength && wordLength < 21)
-                addLength = wordLength * 4.3;
+            {
+                firstName = name;
+                addLength = wordLength * 4.6;
+            }
+             
             int Width = Img.Width;
             int Height = Img.Height;
             //获取图片水平和垂直的分辨率
             float dpiX = Img.HorizontalResolution;
             float dpiY = Img.VerticalResolution;
             //创建一个位图文件
-            Bitmap BitmapResult = new Bitmap(Width + (int)addLength, Height + 30, PixelFormat.Format24bppRgb);
+            Bitmap BitmapResult = new Bitmap(Width + (int)addLength, Height + 40, PixelFormat.Format24bppRgb);
             //设置位图文件的水平和垂直分辨率  与Img一致
             BitmapResult.SetResolution(dpiX, dpiY);
             //在位图文件上填充一个矩形框
             Graphics Grp = Graphics.FromImage(BitmapResult);
-            Rectangle Rec = new Rectangle(0, 0, Width + (int)addLength, Height + 30);
+            Rectangle Rec = new Rectangle(0, 0, Width + (int)addLength, Height + 40);
             //定义一个白色的画刷
             SolidBrush mySolidBrush = new SolidBrush(Color.White);
             //Grp.Clear(Color.White);
@@ -270,9 +304,14 @@ namespace C2.Model.MindMaps
             //返回位图文件
 
             
-            System.Drawing.Font font = new System.Drawing.Font("宋体", 9);
+            System.Drawing.Font font = new System.Drawing.Font("宋体", 9);//插入第一行文件名
             SolidBrush sbrush = new SolidBrush(Color.Black);
-            Grp.DrawString(name, font, sbrush, new PointF(0, 40));
+            Grp.DrawString(firstName, font, sbrush, new PointF(0, 40));
+            if(leftname != null) //插入第二行文件名
+            {
+                Grp.DrawString(leftname, font, sbrush, new PointF(0, 55));
+            }
+            
             Grp.Dispose();
             GC.Collect();
             return BitmapResult;
@@ -290,15 +329,15 @@ namespace C2.Model.MindMaps
         {
             return topic.FindWidgets<PictureWidget>(e => File.Exists(e.ImageUrl) && (e.Data.Width > 128 || e.Data.Height > 128));
         }
-        private void WriteToDocx(Topic topic, Document docx, string serialNumber = "1")
+        private void WriteToDocx(Topic topic, Document docx, StyleCollection styles, string serialNumber = "1")
         {
             
             //写入标题
-            WriteTitleToDocx(topic.Text, docx, topic.GetDepth(topic), serialNumber);
+            WriteTitleToDocx(topic.Text, docx, topic.GetDepth(topic), serialNumber, styles);
 
             //在标题后写入内容
 
-            WriteNoteToDocx(GetTopicNote(topic), docx);
+            WriteNoteToDocx(GetTopicNote(topic), docx, styles);
 
             //在内容后插入图片
             foreach (var pictureWidget in GetTopicPictures(topic))
@@ -316,7 +355,7 @@ namespace C2.Model.MindMaps
                 string nSerialNumber = (i + 1).ToString();
                 if (!topic.IsRoot)
                     nSerialNumber = string.Format("{0}.{1}", serialNumber, (i + 1).ToString());
-                WriteToDocx(topic.Children[i], docx, nSerialNumber);
+                WriteToDocx(topic.Children[i], docx, styles, nSerialNumber);
             }
 
         }
@@ -326,7 +365,12 @@ namespace C2.Model.MindMaps
             {
                 imgNo = 0;//word中图片编号初始化
                 Document docx = new Document();
-                WriteToDocx(topic, docx);
+
+                string examplePath = Path.Combine(Application.StartupPath, "Resources", "Templates", "DocxExample.dotx");//获得模板样式
+                Document doct = new Document(examplePath);
+                StyleCollection styles = doct.Styles;
+
+                WriteToDocx(topic, docx,styles);
                 try
                 {
                     docx.Save(fileName);
@@ -341,9 +385,9 @@ namespace C2.Model.MindMaps
                         MessageBox.Show("以下附件不存在或大小超过500M:"+sb.ToString(), "提示");
                     }
                 }
-                catch
+                catch(Exception ex)
                 {
-
+                    MessageBox.Show("错误:" + ex.Message);
                 }
 
             }
