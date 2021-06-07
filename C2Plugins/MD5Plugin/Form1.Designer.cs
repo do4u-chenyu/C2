8eac5dc99f1199bf8b27e473b7e60b924bf7618a
xxlisa <1261921617@qq.com>
2021-06-07 11:07:54 +0800

    分辨率修改

--- C2Plugins/MD5Plugin/Form1.Designer.cs
+++ C2Plugins/MD5Plugin/Form1.Designer.cs
@@ -196,8 +196,7 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.ClientSize = new System.Drawing.Size(1369, 684);
             this.Controls.Add(this.radioButton7);
