351b5d5cce633458ee24749c48d254f6f43c8e7b
lu-qi123 <kfc.skywalker@163.com>
2021-07-02 10:28:57 +0800

    CB任务预留功能扩展

--- C2Main/Dialogs/CastleBravo/AddCBTask.Designer.cs
+++ C2Main/Dialogs/CastleBravo/AddCBTask.Designer.cs
@@ -35,13 +35,16 @@
             this.browserButton = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.taskComboBox = new System.Windows.Forms.ComboBox();
+            this.modeComboBox = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(36, 32);
+            this.label1.Location = new System.Drawing.Point(21, 58);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(65, 19);
             this.label1.TabIndex = 10003;
@@ -51,7 +54,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label2.Location = new System.Drawing.Point(22, 80);
+            this.label2.Location = new System.Drawing.Point(7, 101);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(79, 19);
             this.label2.TabIndex = 10004;
@@ -59,24 +62,24 @@
             // 
             // taskNameTextBox
             // 
-            this.taskNameTextBox.Location = new System.Drawing.Point(107, 31);
+            this.taskNameTextBox.Location = new System.Drawing.Point(92, 56);
             this.taskNameTextBox.Name = "taskNameTextBox";
-            this.taskNameTextBox.Size = new System.Drawing.Size(244, 21);
+            this.taskNameTextBox.Size = new System.Drawing.Size(231, 21);
             this.taskNameTextBox.TabIndex = 10005;
             // 
             // filePathTextBox
             // 
-            this.filePathTextBox.Location = new System.Drawing.Point(107, 80);
+            this.filePathTextBox.Location = new System.Drawing.Point(92, 100);
             this.filePathTextBox.Name = "filePathTextBox";
-            this.filePathTextBox.Size = new System.Drawing.Size(244, 21);
+            this.filePathTextBox.Size = new System.Drawing.Size(231, 21);
             this.filePathTextBox.TabIndex = 10006;
             // 
             // browserButton
             // 
             this.browserButton.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.browserButton.Location = new System.Drawing.Point(367, 79);
+            this.browserButton.Location = new System.Drawing.Point(336, 100);
             this.browserButton.Name = "browserButton";
-            this.browserButton.Size = new System.Drawing.Size(54, 24);
+            this.browserButton.Size = new System.Drawing.Size(90, 24);
             this.browserButton.TabIndex = 10007;
             this.browserButton.Text = "+浏览";
             this.browserButton.UseVisualStyleBackColor = true;
@@ -86,7 +89,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.ForeColor = System.Drawing.Color.Red;
-            this.label3.Location = new System.Drawing.Point(107, 108);
+            this.label3.Location = new System.Drawing.Point(92, 129);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(203, 12);
             this.label3.TabIndex = 10008;
@@ -96,12 +99,63 @@
             // 
             this.ofd.Filter = "数据文件|*.txt;*.bcp;*.csv;*.tsv|所有文件|*.*";
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(7, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 19);
+            this.label4.TabIndex = 10009;
+            this.label4.Text = "任务类型：";
+            // 
+            // taskComboBox
+            // 
+            this.taskComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.taskComboBox.FormattingEnabled = true;
+            this.taskComboBox.ItemHeight = 12;
+            this.taskComboBox.Items.AddRange(new object[] {
+            "Hash逆向分析",
+            "目标侦察(施工中...)"});
+            this.taskComboBox.Location = new System.Drawing.Point(92, 13);
+            this.taskComboBox.Name = "taskComboBox";
+            this.taskComboBox.Size = new System.Drawing.Size(231, 20);
+            this.taskComboBox.TabIndex = 10010;
+            this.taskComboBox.SelectedIndexChanged += new System.EventHandler(this.TaskComboBox_SelectedIndexChanged);
+            // 
+            // modeComboBox
+            // 
+            this.modeComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.modeComboBox.DropDownWidth = 105;
+            this.modeComboBox.FormattingEnabled = true;
+            this.modeComboBox.ItemHeight = 12;
+            this.modeComboBox.Items.AddRange(new object[] {
+            "自动",
+            "MD5",
+            "MD5(SHA1($pwd))",
+            "MD5(MD5($pwd))",
+            "MD5(MD5(MD5($pwd)))",
+            "MD5(SHA256($pwd))",
+            "MD5(SHA512($pwd))",
+            "SHA1",
+            "SHA1(MD5($pwd))",
+            "SHA256",
+            "SHA512",
+            "宝塔面板"});
+            this.modeComboBox.Location = new System.Drawing.Point(336, 56);
+            this.modeComboBox.Name = "modeComboBox";
+            this.modeComboBox.Size = new System.Drawing.Size(90, 20);
+            this.modeComboBox.TabIndex = 10011;
+            // 
             // AddCBTask
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(433, 200);
+            this.Controls.Add(this.modeComboBox);
+            this.Controls.Add(this.taskComboBox);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.browserButton);
             this.Controls.Add(this.filePathTextBox);
@@ -110,12 +164,16 @@
             this.Controls.Add(this.label1);
             this.Name = "AddCBTask";
             this.Text = "喝彩城堡-新建任务";
+            this.Load += new System.EventHandler(this.AddCBTask_Load);
             this.Controls.SetChildIndex(this.label1, 0);
             this.Controls.SetChildIndex(this.label2, 0);
             this.Controls.SetChildIndex(this.taskNameTextBox, 0);
             this.Controls.SetChildIndex(this.filePathTextBox, 0);
             this.Controls.SetChildIndex(this.browserButton, 0);
             this.Controls.SetChildIndex(this.label3, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
+            this.Controls.SetChildIndex(this.taskComboBox, 0);
+            this.Controls.SetChildIndex(this.modeComboBox, 0);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -130,5 +188,8 @@
         private System.Windows.Forms.Button browserButton;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox taskComboBox;
+        private System.Windows.Forms.ComboBox modeComboBox;
     }
 }
--- C2Main/Dialogs/CastleBravo/AddCBTask.cs
+++ C2Main/Dialogs/CastleBravo/AddCBTask.cs
@@ -128,5 +128,23 @@ namespace C2.Dialogs.CastleBravo
 
             return true;
         }
+
+        private void AddCBTask_Load(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        private void Reset(int i = 0)
+        {
+            this.taskComboBox.SelectedIndex = i;
+            this.modeComboBox.SelectedIndex = 0;
+            this.modeComboBox.Visible = this.taskComboBox.SelectedIndex == 0;
+            this.label3.Visible = this.taskComboBox.SelectedIndex == 0;
+        }
+
+        private void TaskComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Reset(this.taskComboBox.SelectedIndex);
+        }
     }
 }
