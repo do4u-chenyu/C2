97dfd2be2461609abc25da94e3b19c3a8af94192
MCDictionary <735946247@qq.com>
2021-07-12 09:02:43 +0800

    word导入bug修正

--- C2Main/Business/Model/ImportWordFile.cs
+++ C2Main/Business/Model/ImportWordFile.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using Aspose.Words;
 using C2.Core;
 using C2.Forms;
@@ -24,51 +25,64 @@ namespace C2.Business.Model
         }
         public void Import(string path)
         {
+            if (!File.Exists(path))
+            {
+                MessageBox.Show("文件不存在");
+                return;
+            }
             CreateC2(LoadWord(path),path);
         }
         private List<List<string>> LoadWord(string path)
         {
-            List<List<string>> titles = new List<List<string>>();
             
-            Document doc = new Document(path);
-            // Get all paragraphs from the document.
-            NodeCollection paragraphs = doc.GetChildNodes(NodeType.Paragraph, true);
-            foreach (Paragraph paragraph in paragraphs)
+            List<List<string>> titles = new List<List<string>>();
+            try
             {
-                if (paragraph.ParagraphFormat.Style.StyleIdentifier == StyleIdentifier.Heading1)
+                Document doc = new Document(path);
+                NodeCollection paragraphs = doc.GetChildNodes(NodeType.Paragraph, true);
+                foreach (Paragraph paragraph in paragraphs)
                 {
-                    List<string> titleInfo = new List<string>();
-                    titleInfo.Add(paragraph.Range.Text);
-                    titleInfo.Add("1");
-                    titles.Add(titleInfo);
+                    if (paragraph.ParagraphFormat.Style.StyleIdentifier == StyleIdentifier.Heading1)
+                    {
+                        List<string> titleInfo = new List<string>();
+                        titleInfo.Add(paragraph.Range.Text);
+                        titleInfo.Add("1");
+                        titles.Add(titleInfo);
+                    }
+                    if (paragraph.ParagraphFormat.Style.StyleIdentifier == StyleIdentifier.Heading2)
+                    {
+                        List<string> titleInfo = new List<string>();
+                        titleInfo.Add(paragraph.Range.Text);
+                        titleInfo.Add("2");
+                        titles.Add(titleInfo);
+                    }
+                    if (paragraph.ParagraphFormat.Style.StyleIdentifier == StyleIdentifier.Heading3)
+                    {
+                        List<string> titleInfo = new List<string>();
+                        titleInfo.Add(paragraph.Range.Text);
+                        titleInfo.Add("3");
+                        titles.Add(titleInfo);
+                    }
+                    if (paragraph.ParagraphFormat.Style.StyleIdentifier == StyleIdentifier.Heading4)
+                    {
+                        List<string> titleInfo = new List<string>();
+                        titleInfo.Add(paragraph.Range.Text);
+                        titleInfo.Add("4");
+                        titles.Add(titleInfo);
+                    }
+
                 }
-                if (paragraph.ParagraphFormat.Style.StyleIdentifier == StyleIdentifier.Heading2)
-                {
-                    List<string> titleInfo = new List<string>();
-                    titleInfo.Add(paragraph.Range.Text);
-                    titleInfo.Add("2");
-                    titles.Add(titleInfo);
-                }
-                if (paragraph.ParagraphFormat.Style.StyleIdentifier == StyleIdentifier.Heading3)
-                {
-                    List<string> titleInfo = new List<string>();
-                    titleInfo.Add(paragraph.Range.Text);
-                    titleInfo.Add("3");
-                    titles.Add(titleInfo);
-                }
-                if (paragraph.ParagraphFormat.Style.StyleIdentifier == StyleIdentifier.Heading4)
-                {
-                    List<string> titleInfo = new List<string>();
-                    titleInfo.Add(paragraph.Range.Text);
-                    titleInfo.Add("4");
-                    titles.Add(titleInfo);
-                }
-                
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show(ex.Message, "ERROR");
             }
             return titles;
         }
         private void CreateC2(List<List<string>> titles,string path)
         {
+            if (titles.Count == 0)
+                return;
             string name = Path.GetFileNameWithoutExtension(path);
             C2.Model.Documents.Document doc = Global.GetMainForm().CreateNewMapForWord(name);
             doc.FileName = name;
@@ -79,7 +93,14 @@ namespace C2.Business.Model
             root.Children.Remove(root.GetChildByText("子主题 1"));
             if (titles[0][1] == "1")
                 root.Text = titles[0][0];
-            CreatTopic(titles,root,0);
+            try
+            {
+                CreatTopic(titles, root, 0);
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show(ex.Message,"ERROR");
+            }
         }
         private void CreatTopic(List<List<string>> titles, Topic lastTopic, int j) 
         {
