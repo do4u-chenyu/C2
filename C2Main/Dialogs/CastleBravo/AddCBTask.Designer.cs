fceea253ad4eac8c9da4130268d4481a2db9ecb3
lu-qi123 <kfc.skywalker@163.com>
2021-07-19 11:50:55 +0800

    CB模块支持粘贴模式，施工中

--- C2Main/Dialogs/CastleBravo/AddCBTask.Designer.cs
+++ C2Main/Dialogs/CastleBravo/AddCBTask.Designer.cs
@@ -38,13 +38,15 @@
             this.label4 = new System.Windows.Forms.Label();
             this.taskComboBox = new System.Windows.Forms.ComboBox();
             this.modeComboBox = new System.Windows.Forms.ComboBox();
+            this.md5TextBox = new System.Windows.Forms.TextBox();
+            this.pasteModeCB = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(21, 58);
+            this.label1.Location = new System.Drawing.Point(25, 58);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(65, 19);
             this.label1.TabIndex = 10003;
@@ -54,7 +56,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label2.Location = new System.Drawing.Point(7, 101);
+            this.label2.Location = new System.Drawing.Point(11, 101);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(79, 19);
             this.label2.TabIndex = 10004;
@@ -62,22 +64,22 @@
             // 
             // taskNameTextBox
             // 
-            this.taskNameTextBox.Location = new System.Drawing.Point(92, 56);
+            this.taskNameTextBox.Location = new System.Drawing.Point(96, 56);
             this.taskNameTextBox.Name = "taskNameTextBox";
-            this.taskNameTextBox.Size = new System.Drawing.Size(231, 21);
+            this.taskNameTextBox.Size = new System.Drawing.Size(259, 21);
             this.taskNameTextBox.TabIndex = 10005;
             // 
             // filePathTextBox
             // 
-            this.filePathTextBox.Location = new System.Drawing.Point(92, 100);
+            this.filePathTextBox.Location = new System.Drawing.Point(96, 100);
             this.filePathTextBox.Name = "filePathTextBox";
-            this.filePathTextBox.Size = new System.Drawing.Size(231, 21);
+            this.filePathTextBox.Size = new System.Drawing.Size(259, 21);
             this.filePathTextBox.TabIndex = 10006;
             // 
             // browserButton
             // 
             this.browserButton.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.browserButton.Location = new System.Drawing.Point(336, 100);
+            this.browserButton.Location = new System.Drawing.Point(366, 100);
             this.browserButton.Name = "browserButton";
             this.browserButton.Size = new System.Drawing.Size(90, 24);
             this.browserButton.TabIndex = 10007;
@@ -89,7 +91,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.ForeColor = System.Drawing.Color.Red;
-            this.label3.Location = new System.Drawing.Point(92, 129);
+            this.label3.Location = new System.Drawing.Point(94, 129);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(203, 12);
             this.label3.TabIndex = 10008;
@@ -103,7 +105,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label4.Location = new System.Drawing.Point(7, 13);
+            this.label4.Location = new System.Drawing.Point(11, 13);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(79, 19);
             this.label4.TabIndex = 10009;
@@ -117,9 +119,9 @@
             this.taskComboBox.Items.AddRange(new object[] {
             "Hash逆向分析",
             "目标侦察(施工中...)"});
-            this.taskComboBox.Location = new System.Drawing.Point(92, 13);
+            this.taskComboBox.Location = new System.Drawing.Point(96, 13);
             this.taskComboBox.Name = "taskComboBox";
-            this.taskComboBox.Size = new System.Drawing.Size(231, 20);
+            this.taskComboBox.Size = new System.Drawing.Size(259, 20);
             this.taskComboBox.TabIndex = 10010;
             this.taskComboBox.SelectedIndexChanged += new System.EventHandler(this.TaskComboBox_SelectedIndexChanged);
             // 
@@ -127,6 +129,7 @@
             // 
             this.modeComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.modeComboBox.DropDownWidth = 105;
+            this.modeComboBox.Font = new System.Drawing.Font("宋体", 9F);
             this.modeComboBox.FormattingEnabled = true;
             this.modeComboBox.ItemHeight = 12;
             this.modeComboBox.Items.AddRange(new object[] {
@@ -142,17 +145,43 @@
             "SHA256",
             "SHA512",
             "宝塔面板"});
-            this.modeComboBox.Location = new System.Drawing.Point(336, 56);
+            this.modeComboBox.Location = new System.Drawing.Point(366, 57);
             this.modeComboBox.Name = "modeComboBox";
             this.modeComboBox.Size = new System.Drawing.Size(90, 20);
             this.modeComboBox.TabIndex = 10011;
+            // 
+            // md5TextBox
+            // 
+            this.md5TextBox.Font = new System.Drawing.Font("微软雅黑", 8F);
+            this.md5TextBox.Location = new System.Drawing.Point(96, 149);
+            this.md5TextBox.Multiline = true;
+            this.md5TextBox.Name = "md5TextBox";
+            this.md5TextBox.ReadOnly = true;
+            this.md5TextBox.Size = new System.Drawing.Size(259, 117);
+            this.md5TextBox.TabIndex = 10012;
+            this.md5TextBox.Text = "粘贴模式可以直接Ctrl+V";
+            this.md5TextBox.WordWrap = false;
+            // 
+            // pasteModeCB
+            // 
+            this.pasteModeCB.AutoSize = true;
+            this.pasteModeCB.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold);
+            this.pasteModeCB.Location = new System.Drawing.Point(6, 149);
+            this.pasteModeCB.Name = "pasteModeCB";
+            this.pasteModeCB.Size = new System.Drawing.Size(75, 21);
+            this.pasteModeCB.TabIndex = 10013;
+            this.pasteModeCB.Text = "粘贴模式";
+            this.pasteModeCB.UseVisualStyleBackColor = true;
+            this.pasteModeCB.CheckedChanged += new System.EventHandler(this.PasteModeCB_CheckedChanged);
             // 
             // AddCBTask
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(433, 200);
+            this.ClientSize = new System.Drawing.Size(470, 318);
+            this.Controls.Add(this.pasteModeCB);
+            this.Controls.Add(this.md5TextBox);
             this.Controls.Add(this.modeComboBox);
             this.Controls.Add(this.taskComboBox);
             this.Controls.Add(this.label4);
@@ -174,6 +203,8 @@
             this.Controls.SetChildIndex(this.label4, 0);
             this.Controls.SetChildIndex(this.taskComboBox, 0);
             this.Controls.SetChildIndex(this.modeComboBox, 0);
+            this.Controls.SetChildIndex(this.md5TextBox, 0);
+            this.Controls.SetChildIndex(this.pasteModeCB, 0);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -191,5 +222,7 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.ComboBox taskComboBox;
         private System.Windows.Forms.ComboBox modeComboBox;
+        private System.Windows.Forms.TextBox md5TextBox;
+        private System.Windows.Forms.CheckBox pasteModeCB;
     }
 }
