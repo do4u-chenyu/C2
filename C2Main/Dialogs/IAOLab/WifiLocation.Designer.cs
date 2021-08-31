023fafba1563888987c88e75fb2263231814f141
AnTi-anti <weilei.gakki@gmail.com>
2021-08-31 10:18:48 +0800

    新增定位工具地址查询功能

--- C2Main/Dialogs/IAOLab/WifiLocation.Designer.cs
+++ C2Main/Dialogs/IAOLab/WifiLocation.Designer.cs
@@ -41,14 +41,18 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
             this.SuspendLayout();
             // 
             // inputAndResult
@@ -70,7 +74,7 @@
             this.inputLabel.Location = new System.Drawing.Point(5, 13);
             this.inputLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.inputLabel.Name = "inputLabel";
-            this.inputLabel.Size = new System.Drawing.Size(191, 19);
+            this.inputLabel.Size = new System.Drawing.Size(151, 15);
             this.inputLabel.TabIndex = 1;
             this.inputLabel.Text = "请在下方输入MAC地址";
             // 
@@ -113,7 +117,7 @@
             this.label2.Location = new System.Drawing.Point(3, 25);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 19);
+            this.label2.Size = new System.Drawing.Size(0, 15);
             this.label2.TabIndex = 6;
             // 
             // tipLable
@@ -122,7 +126,7 @@
             this.tipLable.ForeColor = System.Drawing.SystemColors.AppWorkspace;
             this.tipLable.Location = new System.Drawing.Point(6, 61);
             this.tipLable.Name = "tipLable";
-            this.tipLable.Size = new System.Drawing.Size(680, 38);
+            this.tipLable.Size = new System.Drawing.Size(542, 30);
             this.tipLable.TabIndex = 7;
             this.tipLable.Text = "单次输入格式：04a1518006c2 或 04-a1-51-80-06-c2 或 04:a1:51:80:06:c2\r\n批量查询格式：多个mac间用换行分割，最" +
     "大支持2000条";
@@ -135,7 +139,7 @@
             this.label1.ForeColor = System.Drawing.Color.SkyBlue;
             this.label1.Location = new System.Drawing.Point(9, 60);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(78, 23);
+            this.label1.Size = new System.Drawing.Size(65, 20);
             this.label1.TabIndex = 9;
             this.label1.Text = "查询进度";
             // 
@@ -161,6 +165,7 @@
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
             this.tabControl1.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.tabControl1.Location = new System.Drawing.Point(0, -1);
             this.tabControl1.Name = "tabControl1";
@@ -173,10 +178,10 @@
             this.tabPage1.Controls.Add(this.label2);
             this.tabPage1.Controls.Add(this.inputLabel);
             this.tabPage1.Controls.Add(this.tipLable);
-            this.tabPage1.Location = new System.Drawing.Point(4, 28);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(593, 101);
+            this.tabPage1.Size = new System.Drawing.Size(593, 105);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Wifi查询";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -185,24 +190,25 @@
             // 
             this.tabPage2.Controls.Add(this.label4);
             this.tabPage2.Controls.Add(this.label3);
-            this.tabPage2.Location = new System.Drawing.Point(4, 28);
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(593, 101);
+            this.tabPage2.Size = new System.Drawing.Size(593, 105);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "基站查询";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // label4
+            // tabPage3
             // 
-            this.label4.AutoSize = true;
-            this.label4.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.label4.Location = new System.Drawing.Point(6, 61);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(581, 38);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "单次输入格式：4600051162c01(2G/3G) 或 46001590a8089407(4G)\r\n 或 37b900018bd0(电信2G) 最大支持200" +
-    "0条";
+            this.tabPage3.Controls.Add(this.label7);
+            this.tabPage3.Controls.Add(this.label8);
+            this.tabPage3.Location = new System.Drawing.Point(4, 24);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(593, 105);
+            this.tabPage3.TabIndex = 1;
+            this.tabPage3.Text = "地址查询";
+            this.tabPage3.UseVisualStyleBackColor = true;
             // 
             // label3
             // 
@@ -211,9 +217,20 @@
             this.label3.Location = new System.Drawing.Point(5, 13);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(180, 19);
+            this.label3.Size = new System.Drawing.Size(142, 15);
             this.label3.TabIndex = 2;
             this.label3.Text = "请在下方输入基站号";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label4.Location = new System.Drawing.Point(6, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(463, 30);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "单次输入格式：4600051162c01(2G/3G) 或 46001590a8089407(4G)\r\n 或 37b900018bd0(电信2G) 最大支持200" +
+    "0条";
             // 
             // label5
             // 
@@ -222,7 +239,7 @@
             this.label5.Location = new System.Drawing.Point(9, 24);
             this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(199, 19);
+            this.label5.Size = new System.Drawing.Size(157, 15);
             this.label5.TabIndex = 13;
             this.label5.Text = "请在下方输入银行卡号";
             // 
@@ -233,10 +250,31 @@
             this.label6.ForeColor = System.Drawing.SystemColors.AppWorkspace;
             this.label6.Location = new System.Drawing.Point(10, 94);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(680, 38);
+            this.label6.Size = new System.Drawing.Size(542, 30);
             this.label6.TabIndex = 14;
             this.label6.Text = "单次输入格式：04a1518006c2 或 04-a1-51-80-06-c2 或 04:a1:51:80:06:c2\r\n批量查询格式：多个mac间用换行分割，最" +
     "大支持5000条";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("宋体", 11F);
+            this.label7.Location = new System.Drawing.Point(5, 13);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(142, 15);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "请在下方输入待查询地址";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label8.Location = new System.Drawing.Point(6, 61);
+            this.label8.Name = "label4";
+            this.label8.Size = new System.Drawing.Size(463, 30);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "输入格式:南京市鼓楼区汉口路22号南京大学(xx市+具体地址)";
             // 
             // WifiLocation
             // 
@@ -262,6 +300,8 @@
             this.tabPage1.PerformLayout();
             this.tabPage2.ResumeLayout(false);
             this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -282,9 +322,12 @@
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
