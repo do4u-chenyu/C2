3d22e7382e8fe1937e8e43d0f534d908942bb94e
lu-qi123 <kfc.skywalker@163.com>
2021-09-07 16:09:44 +0800

    错误提示信息修正

--- C2Main/Dialogs/SearchToolkit/SearchToolkitForm.cs
+++ C2Main/Dialogs/SearchToolkit/SearchToolkitForm.cs
@@ -233,9 +233,9 @@ namespace C2.SearchToolkit
 
             validateMessage = String.Empty;
             // 从后往前验证
-            validateMessage = ValidateSearchAgentIP() ? validateMessage : "全文机【IP】格式不对";
-            validateMessage = ValidateInterfaceIP()   ? validateMessage : "界面机【IP】格式不对";
-            validateMessage = ValidateBastionIP()     ? validateMessage : "堡垒机【IP:Port】格式不对";
+            validateMessage = ValidateSearchAgentIP() ? validateMessage : "全文机【IP:Port】格式不对，标准端口时Port可不填";
+            validateMessage = ValidateInterfaceIP()   ? validateMessage : "界面机【IP:Port】格式不对，标准端口时Port可不填";
+            validateMessage = ValidateBastionIP()     ? validateMessage : "堡垒机【IP:Port】格式不对，标准端口时Port可不填";
             validateMessage = ValidatePassword()      ? validateMessage : "堡垒机 【密码】  不能为空, 不能超过128个字符";
             validateMessage = ValidateUsername()      ? validateMessage : "堡垒机 【用户名】不能为空, 不能超过128个字符";
             validateMessage = ValidateTaskName()      ? validateMessage : "任务名称 不能为空,不能超过128个字符,不能含有特殊字符";
