ccb0f327c823733a9b65411a8d63e3ba706f7505
AnTi-anti <weilei.gakki@gmail.com>
2021-09-08 16:55:53 +0800

    新增HI实验室涉政文本识别接口

--- C2Main/Controls/C1/Left/IAOButton.cs
+++ C2Main/Controls/C1/Left/IAOButton.cs
@@ -6,6 +6,7 @@ using C2.Business.HIBU.QRCode;
 using C2.Business.HIBU.FaceAgeGender;
 using C2.Business.HIBU.LanguageDetect;
 using C2.Business.HIBU.DrugTextRecognition;
+using C2.Business.HIBU.PoliticsTextRecognition;
 using C2.Dialogs.IAOLab;
 using C2.Globalization;
 using C2.IAOLab.Plugins;
@@ -102,7 +103,11 @@ namespace C2.Controls.Left
                     break;
                 case "涉赌文本识别":
                     this.leftPictureBox.Image = global::C2.Properties.Resources.DrugTextRecognition;
-                    toolTip1.SetToolTip(this.rightPictureBox, HelpUtil.LanguageDetectFormHelpInfo);
+                    toolTip1.SetToolTip(this.rightPictureBox, HelpUtil.DrugTextRecognitionFormHelpInfo);
+                    break;
+                case "涉政文本识别":
+                    this.leftPictureBox.Image = global::C2.Properties.Resources.PoliticsTextRecognition;
+                    toolTip1.SetToolTip(this.rightPictureBox, HelpUtil.PoliticsTextRecognitionFormHelpInfo);
                     break;
             }
 
@@ -203,6 +208,9 @@ namespace C2.Controls.Left
                     break;
                 case "涉赌文本识别":
                     new DrugTextRecognitionForm().ShowDialog();
+                    break;
+                case "涉政文本识别":
+                    new PoliticsTextRecognitionForm().ShowDialog();
                     break;
                 default:
                     openToolForm?.Invoke();
--- C2Main/Properties/Resources.Designer.cs
+++ C2Main/Properties/Resources.Designer.cs
@@ -2006,6 +2006,16 @@ namespace C2.Properties {
         /// <summary>
         ///   查找 System.Drawing.Bitmap 类型的本地化资源。
         /// </summary>
+        public static System.Drawing.Bitmap PoliticsTextRecognition {
+            get {
+                object obj = ResourceManager.GetObject("PoliticsTextRecognition", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
         public static System.Drawing.Bitmap PostgreSQL {
             get {
                 object obj = ResourceManager.GetObject("PostgreSQL", resourceCulture);
