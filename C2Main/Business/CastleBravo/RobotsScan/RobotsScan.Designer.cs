ab7dfea5505c07b45f9cfec6a6294abc4513d200
XXGYC <1037538120@qq.com>
2021-09-13 18:42:18 +0800

    完成网站指纹扫描功能

--- C2Main/Business/CastleBravo/RobotsScan/RobotsScan.Designer.cs
+++ C2Main/Business/CastleBravo/RobotsScan/RobotsScan.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace C2.Business.CastleBravo.RobotsScan
 {
-    partial class MainWin
+    partial class RobotsScan
     {
         /// <summary>
         /// Required designer variable.
@@ -29,13 +29,13 @@ namespace C2.Business.CastleBravo.RobotsScan
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWin));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RobotsScan));
             this.label2 = new System.Windows.Forms.Label();
-            this.OutTextBox = new System.Windows.Forms.RichTextBox();
+            this.resultBox = new System.Windows.Forms.RichTextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.button2 = new System.Windows.Forms.Button();
-            this.InputTextBox = new System.Windows.Forms.RichTextBox();
+            this.inputBox = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -43,29 +43,29 @@ namespace C2.Business.CastleBravo.RobotsScan
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label2.Location = new System.Drawing.Point(504, 3);
+            this.label2.Location = new System.Drawing.Point(12, 90);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(149, 20);
             this.label2.TabIndex = 94;
             this.label2.Text = "内网靶场中对应的网站";
             // 
-            // OutTextBox
+            // resultBox
             // 
-            this.OutTextBox.Location = new System.Drawing.Point(499, 33);
-            this.OutTextBox.Name = "OutTextBox";
-            this.OutTextBox.Size = new System.Drawing.Size(301, 272);
-            this.OutTextBox.TabIndex = 93;
-            this.OutTextBox.Text = "";
+            this.resultBox.Location = new System.Drawing.Point(17, 113);
+            this.resultBox.Name = "resultBox";
+            this.resultBox.Size = new System.Drawing.Size(300, 272);
+            this.resultBox.TabIndex = 2;
+            this.resultBox.Text = "";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(12, 3);
+            this.label1.Location = new System.Drawing.Point(12, 21);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(174, 20);
+            this.label1.Size = new System.Drawing.Size(297, 20);
             this.label1.TabIndex = 92;
-            this.label1.Text = "请输入要查找的Robots.txt";
+            this.label1.Text = "请输入域名 (例如：https://www.baidu.com/)";
             // 
             // panel1
             // 
@@ -74,42 +74,43 @@ namespace C2.Business.CastleBravo.RobotsScan
             this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.panel1.Location = new System.Drawing.Point(0, 391);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 59);
+            this.panel1.Size = new System.Drawing.Size(335, 59);
             this.panel1.TabIndex = 91;
             // 
             // button2
             // 
-            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(185)))), ((int)(((byte)(94)))));
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(225)))), ((int)(((byte)(225)))));
             this.button2.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button2.ForeColor = System.Drawing.Color.Honeydew;
-            this.button2.Location = new System.Drawing.Point(603, 18);
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(219, 18);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(116, 29);
+            this.button2.Size = new System.Drawing.Size(90, 25);
             this.button2.TabIndex = 0;
             this.button2.Text = "查找";
             this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.Search_Click);
             // 
-            // InputTextBox
+            // inputBox
             // 
-            this.InputTextBox.Location = new System.Drawing.Point(0, 33);
-            this.InputTextBox.Name = "InputTextBox";
-            this.InputTextBox.Size = new System.Drawing.Size(301, 272);
-            this.InputTextBox.TabIndex = 90;
-            this.InputTextBox.Text = "";
+            this.inputBox.Location = new System.Drawing.Point(17, 53);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(300, 21);
+            this.inputBox.TabIndex = 1;
             // 
-            // Form1
+            // RobotsScan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(335, 450);
+            this.Controls.Add(this.inputBox);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.OutTextBox);
+            this.Controls.Add(this.resultBox);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.InputTextBox);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "Form1";
+            this.MaximizeBox = false;
+            this.Name = "RobotsScan";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Robots模板匹配";
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -120,10 +121,10 @@ namespace C2.Business.CastleBravo.RobotsScan
         #endregion
 
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.RichTextBox OutTextBox;
+        private System.Windows.Forms.RichTextBox resultBox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.RichTextBox InputTextBox;
+        private System.Windows.Forms.TextBox inputBox;
     }
 }
---
+++ C2Main/Business/CastleBravo/RobotsScan/RobotsScan.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace C2.Business.CastleBravo.RobotsScan
+{
+    public partial class RobotsScan : Form
+    {
+        public RobotsScan()
+        {
+            InitializeComponent();
+        }
+
+        //读取json文件
+        public static JObject Readjson()
+        {
+            string jsonfile = "C://Users//FH//Desktop//Robots.json"; //JSON文件路径
+
+            using (System.IO.StreamReader file = System.IO.File.OpenText(jsonfile))
+            {
+                using (JsonTextReader reader = new JsonTextReader(file))
+                {
+                    JObject o = (JObject)JToken.ReadFrom(reader);
+
+                    //var value = o.ToString();
+                    return o;
+                }
+            }
+        }
+
+        private string RobotsContent(string url)
+        {
+            if (url.Length >= 4)
+            {
+                try
+                {
+                    //创建一个http请求
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                    //请求方式
+                    request.Method = "GET";
+                    request.Timeout = 5 * 1000;
+                    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                    Stream s = response.GetResponseStream();
+                    StreamReader sRead = new StreamReader(s);
+                    string postContent = sRead.ReadToEnd();
+                    sRead.Close();
+                    return postContent;
+                }
+                catch
+                {
+                    MessageBox.Show("请检查输入网站是否可以打开，请检查输入格式为http://域名/");
+                    return null;
+                }
+
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+
+        private void Search_Click(object sender, EventArgs e)
+        {
+            List<string> s = new List<string>();
+
+            JObject val = Readjson();
+            string t = string.Join(",", s.ToArray());
+            string data = val.ToString();
+            data = "[" + data + "]";
+            JArray ja = (JArray)JsonConvert.DeserializeObject(data);
+            //string pred = ja[0]["bbs.shouyoufabu.com"].ToString().Replace("[", "").Replace("]", "").Replace(@"""", "").Replace("\n", "").Replace("\r", "").Replace(" ", ""); //此处在ja[0]后加(website里的值)提取出键对应的值
+            //string[] pred = ja[0]["bbs.shouyoufabu.com"].ToString().Replace("[", "").Replace("]", "").Split(','); //此处在ja[0]后加(website里的值)提取出键对应的值
+
+            string jsoncontent = ja[0].ToString(); //将json文件内容输出为字符串
+
+            string RegexStr = @"""(.*)""\:";   //在@“”类型的字符串里面输入双引号要写成两个双引号并列
+            MatchCollection webname = Regex.Matches(jsoncontent, RegexStr, RegexOptions.IgnoreCase);  //利用正则取出全部模板中的模板名称
+            List<string> website = new List<string>();
+            foreach (Match w in webname)
+            {
+                website.Add(w.Groups[0].Value.Replace(":", "").Replace(@"""", ""));     //website列表中存放网站模板的名称
+
+            }
+            string webname1 = string.Join(",", website.ToArray());  //website列表转化为字符串进行打印
+
+
+            string url = inputBox.Text + "robots.txt";
+            string robots = RobotsContent(url);
+
+
+            //string[] inputtext = robots.Replace(@"""", "").Split(Environment.NewLine.ToCharArray());  //将输入的robots.txt的文件内容放进inputtext列表
+
+            if (robots == "User-agent: *\nDisallow:\n" || robots == "User-agent: *" || robots == "User-agent: *\nDisallow:/\n")
+            {
+                MessageBox.Show("网站robots.txt过于简单，无法匹配");
+            }
+            else if (robots != null)
+            {
+                string[] inputtext = robots.Split(Environment.NewLine.ToCharArray());  //将输入的robots.txt的文件内容放进inputtext列表
+                List<string> inputtext1 = inputtext.ToList<string>();
+
+                List<string> result = new List<string>();  //存放最终结果
+
+                for (int k = inputtext1.Count - 1; k >= 0; k--)
+                {
+                    if (inputtext1[k].Contains("#") || inputtext1[k] == "" || inputtext1[k].Contains("Sitemap"))
+                    {
+                        inputtext1.Remove(inputtext1[k]);
+                    }
+                }
+                foreach (string item1 in website)  //取出模板内容  
+                {
+                    for (int i = 0; i < inputtext1.Count; i++)    //输入字符串的内容
+                    {
+                        //if(inputtext1[i])
+                        string[] input1 = inputtext1[i].Split('/');
+                        int j = input1.ToList<string>().FindLastIndex(str => str != "");
+
+                        if (i == inputtext1.Count - 1 && ja[0][item1].ToString().Replace("[", "").Replace("]", "").Replace(@"""", "").Replace("\n", "").Replace("\r", "").Contains(input1[j]))
+                        {
+                            result.Add(item1);
+                            break;
+                        }
+                        else if (ja[0][item1].ToString().Replace("[", "").Replace("]", "").Replace(@"""", "").Replace("\n", "").Replace("\r", "").Contains(input1[j]))
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+                string sresult = string.Join(",", result.ToArray());  //website列表转化为字符串进行打印
+
+                //resultBox.Text = sresult;
+
+                /*
+                 * 调用RobotsContent查结果,要么有：返回匹配结果，要么没有,"未匹配到已有的网站模板";有的话调用Search_Click主逻辑
+                 */
+                if (sresult == string.Empty)
+                {
+                    resultBox.Text = "对不起，未匹配到已有的网站模板";
+                }
+                else
+                {
+                    resultBox.Text = sresult.Replace(",", "\n");
+
+                }
+            }
+
+
+        }
+    }
+}
