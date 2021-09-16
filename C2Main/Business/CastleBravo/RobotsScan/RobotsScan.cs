0af92323653a3477340f4e8ad5ca49b02eb0844f
XXGYC <1037538120@qq.com>
2021-09-16 11:12:42 +0800

    完善提示信息

--- C2Main/Business/CastleBravo/RobotsScan/RobotsScan.cs
+++ C2Main/Business/CastleBravo/RobotsScan/RobotsScan.cs
@@ -29,7 +29,7 @@ namespace C2.Business.CastleBravo.RobotsScan
         //读取json文件
         public static JObject ReadJson()
         {
-            string jsonfile = Path.Combine(Application.StartupPath, "Resources/Templates/WebRobots.json"); //JSON文件路径
+            string jsonfile = Path.Combine(Application.StartupPath, "Resources/Templates/WebRobots.json"); 
             using (System.IO.StreamReader file = System.IO.File.OpenText(jsonfile))
             {
                 using (JsonTextReader reader = new JsonTextReader(file))
@@ -39,7 +39,7 @@ namespace C2.Business.CastleBravo.RobotsScan
                 }
             }
         }
-
+        //获取网站robots.txt
         private string RobotsContent(string url)
         {
             if (url.Length >= 4)
@@ -48,7 +48,7 @@ namespace C2.Business.CastleBravo.RobotsScan
                 {
                     HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
                     request.Method = "GET";
-                    request.Timeout = 5 * 1000;
+                    request.Timeout = 8 * 1000;
                     HttpWebResponse response = (HttpWebResponse)request.GetResponse();
                     Stream s = response.GetResponseStream();
                     StreamReader sRead = new StreamReader(s);
@@ -56,27 +56,47 @@ namespace C2.Business.CastleBravo.RobotsScan
                     sRead.Close();
                     return postContent;
                 }
-                catch(Exception e)
+                catch (Exception e)
                 {
                     if (e.Message.Contains("SSL"))
                     {
-                        System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-                        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-                        request.Method = "GET";
-                        request.Timeout = 5 * 1000;
-                        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-                        Stream s = response.GetResponseStream();
-                        StreamReader sRead = new StreamReader(s);
-                        string postContent = sRead.ReadToEnd();
-                        sRead.Close();
-                        return postContent;
+                        try
+                        {
+                            System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 
+                                                                             | SecurityProtocolType.Tls11 
+                                                                             | SecurityProtocolType.Ssl3
+                                                                             | SecurityProtocolType.Tls;
+                            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                            request.Method = "GET";
+                            request.Timeout = 8 * 1000;
+                            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                            Stream s = response.GetResponseStream();
+                            StreamReader sRead = new StreamReader(s);
+                            string postContent = sRead.ReadToEnd();
+                            sRead.Close();
+                            return postContent;
+                        }
+                        catch
+                        {
+                            MessageBox.Show("无法获得该网站的robots.txt");
+                            return null;
+                        } 
+                    }
+                    else if (e.Message.Contains("操作超时"))
+                    {
+                        MessageBox.Show("网络连接超时，请检查输入网站是否可以打开");
+                        return null;
+                    }
+                    else if (e.Message.Contains("404"))
+                    {
+                        MessageBox.Show("无法获得该网站的robots.txt文件");
+                        return null;
                     }
                     else
                     {
-                        MessageBox.Show("请检查输入网站是否可以打开，请检查输入格式为http://域名/");
+                        MessageBox.Show("请检查输入格式为http://域名/");
                         return null;
                     }
-                    
                 }
             }
             else
@@ -87,7 +107,7 @@ namespace C2.Business.CastleBravo.RobotsScan
 
         protected override bool OnOKButtonClick()
         {         
-            JObject obj = JObject.Parse(ReadJson().ToString());  //obj[]
+            JObject obj = JObject.Parse(ReadJson().ToString());  
             List<string> website = new List<string>();
             foreach (var x in obj)
             {
@@ -95,11 +115,7 @@ namespace C2.Business.CastleBravo.RobotsScan
             }
             string url = inputBox.Text + "robots.txt";
             string robots = RobotsContent(url);
-
-            
-
-
-            if (robots == "User-agent: *\r\nDisallow: " || robots == "User-agent: *" || robots == "User-agent: *\r\nDisallow:\r\n") 
+            if (robots == "User-agent: *\r\nDisallow: " || robots == "User-agent: *" || robots == "User-agent: *\r\nDisallow:\r\n" || robots == "User-agent: * \nDisallow: ") 
             {
                 MessageBox.Show("网站robots.txt过于简单，无法匹配");
             }
@@ -119,7 +135,6 @@ namespace C2.Business.CastleBravo.RobotsScan
                 {
                     for (int i = 0; i < inputtext1.Count; i++)  
                     {
-                        //if(inputtext1[i])
                         string[] input1 = inputtext1[i].Split('/');
                         int j = input1.ToList<string>().FindLastIndex(str => str != "");
 
@@ -138,10 +153,14 @@ namespace C2.Business.CastleBravo.RobotsScan
                         }
                     }
                 }
-                string sresult = string.Join(",", result.ToArray());          
+                string sresult = string.Join(",", result.ToArray());
                 if (sresult == string.Empty)
                 {
                     resultBox.Text = "对不起，未匹配到已有的网站模板";
+                }
+                else if (result.Count >= 6)
+                {
+                    MessageBox.Show("网站robots.txt过于简单，无法匹配");
                 }
                 else
                 {
