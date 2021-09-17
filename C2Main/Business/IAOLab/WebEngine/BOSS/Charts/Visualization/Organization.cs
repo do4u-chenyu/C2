941da94ac6cba66a8fe4b62341ba3c62ddf94698
hzhx7707 <1416305262@qq.com>
2021-09-17 10:04:47 +0800

    组织架构图报错提示bug修改

--- C2Main/Business/IAOLab/WebEngine/BOSS/Charts/Visualization/Organization.cs
+++ C2Main/Business/IAOLab/WebEngine/BOSS/Charts/Visualization/Organization.cs
@@ -37,8 +37,31 @@ namespace C2.Business.IAOLab.WebEngine.Boss.Charts.Visualization
                     }
                 }
             }
+            string[] colName = new string[chartOptions.Length];
+            string[][] colList = new string[chartOptions.Length][];
+            for (int i = 0; i < chartOptions.Length; i++)
+            {
+                colList[i] = new string[dataTable.Rows.Count];
+                colName[i] = dataTable.Columns[int.Parse(chartOptions[i])].ColumnName;
+                colList[i] = dataTable.AsEnumerable().Select(peo => peo.Field<string>(colName[i])).ToArray();
+            }
+            int k = 0;
+            int rootIndex = -1;
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                if (colList[1].Except(colList[0]).ToList().Count == 1 && colList[1][i] == colList[1].Except(colList[0]).ToList()[0])
+                {
+                    k = k + 1;
+                    rootIndex = i;
+                }
+            }
+            if (k != 1)
+            {
+                MessageBox.Show("输入的数据根节点个数不正确，请重新输入");
+                return;
+            }
             //option.dataset = Common.FormatDatas;
-            List<string> returnList = Trans(dataTable, chartOptions);
+            List<string> returnList = Trans(dataTable, chartOptions, rootIndex);
             string data = returnList[0];
             string tooltipHeight = returnList[1];
 
@@ -68,7 +91,7 @@ namespace C2.Business.IAOLab.WebEngine.Boss.Charts.Visualization
             _initScript = option.ToString();
         }
 
-        public List<string> Trans(DataTable dataTable, string[] chartOptions)
+        public List<string> Trans(DataTable dataTable, string[] chartOptions, int index)
         {
             List<Dictionary<string, object>> testData = new List<Dictionary<string, object>>();
 
@@ -112,154 +135,135 @@ namespace C2.Business.IAOLab.WebEngine.Boss.Charts.Visualization
                 colList[i] = dataTable.AsEnumerable().Select(peo => peo.Field<string>(colName[i])).ToArray();
             }
 
-            int k = 0;
-            int rootIndex = -1;
             for (int i = 0; i < dataTable.Rows.Count; i++)
             {
-                if (colList[1].Except(colList[0]).ToList().Count == 1 && colList[1][i] == colList[1].Except(colList[0]).ToList()[0])
+                Dictionary<string, object> node = new Dictionary<string, object>();
+                object labelDictionary = new Dictionary<string, object>();
+                object toolDictionary = new Dictionary<string, object>();
+                object infoDictionary = new Dictionary<string, object>();
+                object firstDictionary = new Dictionary<string, object>();
+                object secondDictionary = new Dictionary<string, object>();
+                object radius = new List<int>() { 5, 5, 5, 5 };
+                object radius1 = new List<int>() { 5, 5, 0, 0 };
+                object radius2 = new List<int>() { 0, 0, 5, 5 };
+
+                (labelDictionary as Dictionary<string, object>).Add("backgroundColor", "#F4F4F4");
+                (labelDictionary as Dictionary<string, object>).Add("borderRadius", radius);
+                (firstDictionary as Dictionary<string, object>).Add("backgroundColor", "#078E34");
+                (firstDictionary as Dictionary<string, object>).Add("color", "#fff");
+                (firstDictionary as Dictionary<string, object>).Add("align", "center");
+                (firstDictionary as Dictionary<string, object>).Add("fontsize", 20);
+
+                string secondList = "}\n{second|";
+                List<int> lengthList = new List<int>();
+                string toolFormatter = colName[0] + ":" + colList[0][i];
+                string secondFormatter = "  ";
+
+                if (chartOptions.Length == 2)
                 {
-                    k = k + 1;
-                    rootIndex = i;
+                    lengthList.Add(0);
+                    //secondList = "";
                 }
-            }
-
-            if (k != 1)
-            {
-                MessageBox.Show("输入的数据根节点个数不正确，请重新输入");
-                return new List<string>() { "" };
-            }
-            else
-            {
-                for (int i = 0; i < dataTable.Rows.Count; i++)
+                else if (chartOptions.Length > 2)
                 {
-                    Dictionary<string, object> node = new Dictionary<string, object>();
-                    object labelDictionary = new Dictionary<string, object>();
-                    object toolDictionary = new Dictionary<string, object>();
-                    object infoDictionary = new Dictionary<string, object>();
-                    object firstDictionary = new Dictionary<string, object>();
-                    object secondDictionary = new Dictionary<string, object>();
-                    object radius = new List<int>() { 5, 5, 5, 5 };
-                    object radius1 = new List<int>() { 5, 5, 0, 0 };
-                    object radius2 = new List<int>() { 0, 0, 5, 5 };
-
-                    (labelDictionary as Dictionary<string, object>).Add("backgroundColor", "#F4F4F4");
-                    (labelDictionary as Dictionary<string, object>).Add("borderRadius", radius);
-                    (firstDictionary as Dictionary<string, object>).Add("backgroundColor", "#078E34");
-                    (firstDictionary as Dictionary<string, object>).Add("color", "#fff");
-                    (firstDictionary as Dictionary<string, object>).Add("align", "center");
-                    (firstDictionary as Dictionary<string, object>).Add("fontsize", 20);
-
-                    string secondList = "}\n{second|";
-                    List<int> lengthList = new List<int>();
-                    string toolFormatter = colName[0] + ":" + colList[0][i];
-                    string secondFormatter = "  ";
-
-                    if (chartOptions.Length == 2)
+                    for (int j = 2; j < chartOptions.Length; j++)
                     {
-                        lengthList.Add(0);
-                        //secondList = "";
-                    }
-                    else if (chartOptions.Length > 2)
-                    {
-                        for (int j = 2; j < chartOptions.Length; j++)
+                        int infoLength = 8;
+                        secondFormatter = secondFormatter + "  " + colName[j] + ":" + colList[j][i];
+                        if (colList[j][i] == "")
                         {
-                            int infoLength = 8;
-                            secondFormatter = secondFormatter + "  " + colName[j] + ":" + colList[j][i];
-                            if (colList[j][i] == "")
-                            {
-                                infoLength = colList[j][i].Length;
-                                secondList = secondList + colList[j][i];
-                            }
-                            else if (colList[j][i].Length <= infoLength)
-                            {
-                                secondList = secondList + colName[j] + ":" + colList[j][i];
-                                infoLength = colList[j][i].Length + colName[j].Length;
-                            }
-                            else if (colList[j][i].Length > infoLength)
-                            {
-                                string infoValue = colList[j][i].Substring(0, infoLength) + "...";
-                                secondList = secondList + "  " + colName[j] + ":" + infoValue;
-                                infoLength = infoValue.Length + colName[j].Length - 1;
-                            }
-                            if (j < chartOptions.Length - 1)
-                            {
-                                secondList = secondList + "\n";
-                            }
-                            lengthList.Add(infoLength);
+                            infoLength = colList[j][i].Length;
+                            secondList = secondList + colList[j][i];
                         }
-                        if (inPut_Length > defaultLength)
+                        else if (colList[j][i].Length <= infoLength)
                         {
-                            secondList = secondList + "\n" + "...";
-                            for (int j = 0; j < inPut_Length - defaultLength; j++)
-                            {
-                                secondFormatter = secondFormatter + "  " + colName_Rest[j] + ":" + colList_Rest[j][i];
-                            }
+                            secondList = secondList + colName[j] + ":" + colList[j][i];
+                            infoLength = colList[j][i].Length + colName[j].Length;
+                        }
+                        else if (colList[j][i].Length > infoLength)
+                        {
+                            string infoValue = colList[j][i].Substring(0, infoLength) + "...";
+                            secondList = secondList + "  " + colName[j] + ":" + infoValue;
+                            infoLength = infoValue.Length + colName[j].Length - 1;
+                        }
+                        if (j < chartOptions.Length - 1)
+                        {
+                            secondList = secondList + "\n";
+                        }
+                        lengthList.Add(infoLength);
+                    }
+                    if (inPut_Length > defaultLength)
+                    {
+                        secondList = secondList + "\n" + "...";
+                        for (int j = 0; j < inPut_Length - defaultLength; j++)
+                        {
+                            secondFormatter = secondFormatter + "  " + colName_Rest[j] + ":" + colList_Rest[j][i];
                         }
                     }
-                    toolFormatter = toolFormatter + secondFormatter;
-                    if (colList[0][i].Length > 8)
-                    {
-                        colList[0][i] = colList[0][i].Substring(0, 8) + "...";
-                    }
-                    List<int> allLength = new List<int>() { lengthList.Max(), colList[0][i].Length, 8 };
-                    int labelWidth = allLength.Max() * 12;
-                    (firstDictionary as Dictionary<string, object>).Add("width", labelWidth);
+                }
+                toolFormatter = toolFormatter + secondFormatter;
+                if (colList[0][i].Length > 8)
+                {
+                    colList[0][i] = colList[0][i].Substring(0, 8) + "...";
+                }
+                List<int> allLength = new List<int>() { lengthList.Max(), colList[0][i].Length, 8 };
+                int labelWidth = allLength.Max() * 12;
+                (firstDictionary as Dictionary<string, object>).Add("width", labelWidth);
 
-                    if (lengthList.Max() != 0)
-                    {
-                        (labelDictionary as Dictionary<string, object>).Add("formatter", "{first|" + colList[0][i] + secondList + "}");
-                        (firstDictionary as Dictionary<string, object>).Add("height", 50);
-                        (firstDictionary as Dictionary<string, object>).Add("borderRadius", radius1);
-                        (secondDictionary as Dictionary<string, object>).Add("color", "#888");
-                        (secondDictionary as Dictionary<string, object>).Add("align", "center");
-                        (secondDictionary as Dictionary<string, object>).Add("width", labelWidth);
-                        (secondDictionary as Dictionary<string, object>).Add("height", infoHeight);
-                        (secondDictionary as Dictionary<string, object>).Add("borderRadius", radius2);
-                        (secondDictionary as Dictionary<string, object>).Add("fontsize", 20);
-                        (infoDictionary as Dictionary<string, object>).Add("second", secondDictionary);
-                    }
-                    else
-                    {
-                        (labelDictionary as Dictionary<string, object>).Add("formatter", "{first|" + colList[0][i] + "}");
-                        (firstDictionary as Dictionary<string, object>).Add("height", 50 + infoHeight * (chartOptions.Length - 2 + initialHeight));
-                        (firstDictionary as Dictionary<string, object>).Add("borderRadius", radius);
-                    }
+                if (lengthList.Max() != 0)
+                {
+                    (labelDictionary as Dictionary<string, object>).Add("formatter", "{first|" + colList[0][i] + secondList + "}");
+                    (firstDictionary as Dictionary<string, object>).Add("height", 50);
+                    (firstDictionary as Dictionary<string, object>).Add("borderRadius", radius1);
+                    (secondDictionary as Dictionary<string, object>).Add("color", "#888");
+                    (secondDictionary as Dictionary<string, object>).Add("align", "center");
+                    (secondDictionary as Dictionary<string, object>).Add("width", labelWidth);
+                    (secondDictionary as Dictionary<string, object>).Add("height", infoHeight);
+                    (secondDictionary as Dictionary<string, object>).Add("borderRadius", radius2);
+                    (secondDictionary as Dictionary<string, object>).Add("fontsize", 20);
+                    (infoDictionary as Dictionary<string, object>).Add("second", secondDictionary);
+                }
+                else
+                {
+                    (labelDictionary as Dictionary<string, object>).Add("formatter", "{first|" + colList[0][i] + "}");
+                    (firstDictionary as Dictionary<string, object>).Add("height", 50 + infoHeight * (chartOptions.Length - 2 + initialHeight));
+                    (firstDictionary as Dictionary<string, object>).Add("borderRadius", radius);
+                }
 
                 (toolDictionary as Dictionary<string, object>).Add("formatter", toolFormatter);
-                    (infoDictionary as Dictionary<string, object>).Add("first", firstDictionary);
-                    (labelDictionary as Dictionary<string, object>).Add("rich", infoDictionary);
+                (infoDictionary as Dictionary<string, object>).Add("first", firstDictionary);
+                (labelDictionary as Dictionary<string, object>).Add("rich", infoDictionary);
 
-                    node.Add("label", labelDictionary);
-                    node.Add("tooltip", toolDictionary);
+                node.Add("label", labelDictionary);
+                node.Add("tooltip", toolDictionary);
 
-                    node.Add("user", colList[0][i]);
-                    node.Add("up", colList[1][i]);
-                    List<Dictionary<string, object>> nullList2 = new List<Dictionary<string, object>>();
-                    node.Add("children", nullList2);
-                    testData.Add(node);
-                }
+                node.Add("user", colList[0][i]);
+                node.Add("up", colList[1][i]);
+                List<Dictionary<string, object>> nullList2 = new List<Dictionary<string, object>>();
+                node.Add("children", nullList2);
+                testData.Add(node);
+            }
 
-                for (int i = 0; i < testData.Count; i++)
+            for (int i = 0; i < testData.Count; i++)
+            {
+                for (int j = 0; j < testData.Count; j++)
                 {
-                    for (int j = 0; j < testData.Count; j++)
+                    if (testData[j]["up"].ToString() == testData[i]["user"].ToString())
                     {
-                        if (testData[j]["up"].ToString() == testData[i]["user"].ToString())
-                        {
-                            (testData[i]["children"] as List<Dictionary<string, object>>).Add(testData[j]);
-                        }
+                        (testData[i]["children"] as List<Dictionary<string, object>>).Add(testData[j]);
                     }
                 }
-                for (int i = 0; i < testData.Count; i++)
-                {
-                    testData[i].Remove("user");
-                    testData[i].Remove("up");
-                }
-
-                JavaScriptSerializer jss = new JavaScriptSerializer();
-                string strDATAJSON = jss.Serialize(testData[rootIndex]);
-                List<string> returnList = new List<string>() { '[' + strDATAJSON + ']', toolHeight.ToString()};
-                return returnList;
             }
+            for (int i = 0; i < testData.Count; i++)
+            {
+                testData[i].Remove("user");
+                testData[i].Remove("up");
+            }
+
+            JavaScriptSerializer jss = new JavaScriptSerializer();
+            string strDATAJSON = jss.Serialize(testData[index]);
+            List<string> returnList = new List<string>() { '[' + strDATAJSON + ']', toolHeight.ToString() };
+            return returnList;
         }
     }
 }
