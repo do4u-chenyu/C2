3ea826fadb7fae5b757eeeeccd505fddc2cca17d
wen6ly <1395198665@qq.com>
2021-09-23 09:36:42 +0800

    修改美化版安装包生成脚本

--- tools/generate.py
+++ tools/generate.py
@@ -41,8 +41,10 @@ def main(file_path: str) -> None:
             return
     print("没有找到vs编译环境, 请将vs目录添加到tools/git-hook-scripts/vs_path")
 
-def C2_uninstall(product_id):
-    uninstall_cmd = "msiexec/x" + product_id
+product_path = os.path.abspath(os.path.dirname(os.getcwd())) + "\IAO解决方案\Release"
+
+def C2_uninstall():
+    uninstall_cmd = "wmic product where Name=\"IAO解决方案\" call uninstall"
     print(uninstall_cmd)
     if os.system(uninstall_cmd) == 0:
         return 0;
@@ -50,7 +52,6 @@ def C2_uninstall(product_id):
         return -1;
 
 def C2_install():
-    product_path = os.path.abspath(os.path.dirname(os.getcwd())) + "\IAO解决方案\Release"
     install_cmd = r"chdir /d {} & msiexec /i IAO解决方案C2.msi /qr".format(product_path)
     print(install_cmd)
     if os.system(install_cmd) == 0:
@@ -58,11 +59,11 @@ def C2_install():
     else:
         return -1;
 
-def beaut_product():
-    beaut_path = "C:\Program Files (x86)\Inno Setup 5"
-    beaut_cmd = "chdir /d {} & compil32 /cc \"".format(beaut_path) + os.path.abspath(os.path.dirname(os.getcwd())) + "\C2打包程序\gen_beauty_package.iss\""
-    print(beaut_cmd)
-    if os.system(beaut_cmd) == 0:
+def beauty_product():
+    beauty_path = "C:\Program Files (x86)\Inno Setup 5"
+    beauty_cmd = "chdir /d {} & compil32 /cc \"".format(beauty_path) + os.path.abspath(os.path.dirname(os.getcwd())) + "\C2打包程序\gen_beauty_package.iss\""
+    print(beauty_cmd)
+    if os.system(beauty_cmd) == 0:
         return 0;
     else:
         return -1;
@@ -71,24 +72,26 @@ if __name__ == '__main__':
     file_path = "git-hook-scripts/vs_path"
     print(abspath(file_path))
     main(file_path)
-    tmp = os.popen("wmic product where Name=\"IAO解决方案\" get IdentifyingNumber|head -n2|tail -n1").read()
-    print(tmp)
-    if tmp.strip() == '':
+    tmp = len(os.popen("wmic product where Name=\"IAO解决方案\"").read())
+    if tmp == 4:
         if C2_install() == 0:
             print("新版IAO解决方案安装成功")
         else:
             print("新版IAO解决方案安装失败")
     else:
-        if C2_uninstall(tmp) == 0:
+        if C2_uninstall() == 0:
             print("旧版IAO解决方案卸载成功")
+            if C2_install() == 0:
+                print("新版IAO解决方案安装成功")
+            else:
+                print("新版IAO解决方案安装失败")
         else:
             print("旧版IAO解决方案卸载失败")
-        if C2_install() == 0:
-            print("新版IAO解决方案安装成功")
-        else:
-            print("新版IAO解决方案安装失败")
 
-    if beaut_product() == 0:
-        print("美化版安装包生成成功在" + os.path.abspath(os.path.dirname(os.getcwd())) + "\C2打包程序\output文件夹下")
+    if C2_install() == 0:
+        if beauty_product() == 0:
+            print("美化版安装包生成成功，位于 " + os.path.abspath(os.path.dirname(os.getcwd())) + "\C2打包程序\output")
+        else:
+            print("美化版安装包生成失败")
     else:
-        print("美化版安装包生成失败")
+        print("无法生成美化版安装包")
