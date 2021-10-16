66fe2d1a0f344b7d3ed55482fc1e9a3c41d11c12
lu-qi123 <kfc.skywalker@163.com>
2021-10-16 16:07:31 +0800

    antsword webshell施工中

--- C2Main/Business/CastleBravo/WebShellTool/Client/AntSwordClient.cs
+++ C2Main/Business/CastleBravo/WebShellTool/Client/AntSwordClient.cs
@@ -4,8 +4,54 @@
     {
         public AntSwordClient(string password, string clientSetting)
             :base(password, clientSetting)
-        { }
+        {
+            this.prefix = password + "=";
+        }
 
+        private string SPL
+        {
+            get { return string.Empty; }
+        }
+
+        private string SPR
+        {
+            get { return string.Empty; }
+        }
+        
+        private string ValueAB
+        {
+            get { return string.Empty; }
+        }
+
+        private string TmpDir
+        {
+            get { return string.Empty; }
+        }
+
+        private string Param1K
+        {
+            get { return string.Empty; }
+        }
+
+        //private string IndexTemplate;
+        //private string PhpinfoTemplate;
+        //private string ReadDictTemplate;
+        //private string ShellTemplate;
+
+        private string PhpinfoStart()
+        {
+            return string.Empty;
+        }
+
+        private void PhpinfoEnd()
+        {
+            
+        }
+
+        public override string PHPInfo()
+        {
+            return string.Empty;
+        }
 
 
     }
