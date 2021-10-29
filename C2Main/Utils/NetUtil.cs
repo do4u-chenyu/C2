e1b780d6aea432e9972ddb6ca9e007771db56889
lu-qi123 <kfc.skywalker@163.com>
2021-10-29 09:31:43 +0800

    webshell新增查询IP归属地功能

--- C2Main/Utils/NetUtil.cs
+++ C2Main/Utils/NetUtil.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.IO;
 using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using C2.Core;
 
 namespace C2.Utils
 {
@@ -17,6 +21,58 @@ namespace C2.Utils
                 return url;
             else
                 return "http://" + url.TrimStart();
+        }
+
+        public static string IPQuery_126Net(string ip)
+        {
+            ip = ip.ToLower().Trim();
+            if (ip.IsNullOrEmpty())
+                return ip;
+            if (ip.StartsWith("127.0.0."))
+                return "本机回环地址";
+            if (ip.Contains("0.0.0.0"))
+                return "空地址";
+            if (ip.StartsWith("192.168."))
+                return "内网IP"; 
+            if (ip.StartsWith("10."))
+                return "内网IP";
+            if (ip.StartsWith("172.16."))  // 172.16 - 172.31都是,赶时间先凑合
+                return "内网IP";
+            if (ip.Contains("%"))
+                return "暂不支持IPV6";
+
+            string url = "http://ip.ws.126.net/ipquery?ip=" + ip;
+            string result = "";
+            WebRequest wrt = null;
+            WebResponse wrp = null;
+            try
+            {
+                wrt = WebRequest.Create(url);
+                wrt.Credentials = CredentialCache.DefaultCredentials;
+
+                wrp = wrt.GetResponse();
+                StreamReader sr = new StreamReader(wrp.GetResponseStream(), Encoding.Default);
+                //获取到的数据格式：var lo="江苏省", lc="镇江市"; var localAddress={city:"镇江市", province:"江苏省"}
+                string html = sr.ReadToEnd();
+                string pattern = "{city:\"(?<city>.*?)\", province:\"(?<province>.*?)\"}";
+                Regex regex = new Regex(pattern, RegexOptions.None);
+                Match match = regex.Match(html);
+                string city = match.Groups["city"].Value;
+                string province = match.Groups["province"].Value;
+                result = city.Equals(province) ? city : (province + city);
+            }
+            catch (Exception)
+            {
+            }
+            finally
+            {
+                if (wrp != null)
+                    wrp.Close();
+                if (wrt != null)
+                    wrt.Abort();
+            }
+            return result;
+
         }
     }
 }
