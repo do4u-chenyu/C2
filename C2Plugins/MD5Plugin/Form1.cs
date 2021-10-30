6fb8b9d7d392f75762e21a96c50346b50eaffa5b
lu-qi123 <kfc.skywalker@163.com>
2021-10-30 23:29:32 +0800

    md5插件新增 ntlm 加密算法

--- C2Plugins/MD5Plugin/Form1.cs
+++ C2Plugins/MD5Plugin/Form1.cs
@@ -478,15 +478,7 @@ namespace MD5Plugin
             }
             else
             {
-                byte[] bytValue = Encoding.UTF8.GetBytes(str);
-                SHA512 sha512 = new SHA512CryptoServiceProvider();
-                byte[] retVal = sha512.ComputeHash(bytValue);
-                StringBuilder sb = new StringBuilder();
-                for (int i = 0; i < retVal.Length; i++)
-                {
-                    sb.Append(retVal[i].ToString("x2"));
-                }
-                outputTextBox.Text = sb.ToString();
+                outputTextBox.Text = NTLM.ComputeHashHexString(str);
             }
         }
         //编码或者加密功能
