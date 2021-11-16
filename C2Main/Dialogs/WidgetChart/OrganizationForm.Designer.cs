e037cdf2aa7cc7aed16a7d437926cc919046d85a
hzhx7707 <1416305262@qq.com>
2021-11-16 17:27:50 +0800

    组织架构视图窗口标题修改

--- C2Main/Dialogs/WidgetChart/OrganizationForm.Designer.cs
+++ C2Main/Dialogs/WidgetChart/OrganizationForm.Designer.cs
@@ -53,14 +53,14 @@ namespace C2.Dialogs.WidgetChart
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(326, 154);
+            this.panel1.Size = new System.Drawing.Size(369, 167);
             this.panel1.TabIndex = 10003;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("宋体", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label4.Location = new System.Drawing.Point(256, 107);
+            this.label4.Location = new System.Drawing.Point(299, 125);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(68, 15);
             this.label4.TabIndex = 10089;
@@ -70,7 +70,7 @@ namespace C2.Dialogs.WidgetChart
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("宋体", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label3.Location = new System.Drawing.Point(12, 107);
+            this.label3.Location = new System.Drawing.Point(14, 125);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(52, 15);
             this.label3.TabIndex = 10088;
@@ -79,10 +79,10 @@ namespace C2.Dialogs.WidgetChart
             // infoList
             // 
             this.infoList.DataSource = null;
-            this.infoList.Location = new System.Drawing.Point(70, 104);
+            this.infoList.Location = new System.Drawing.Point(82, 121);
             this.infoList.Margin = new System.Windows.Forms.Padding(1);
             this.infoList.Name = "infoList";
-            this.infoList.Size = new System.Drawing.Size(187, 23);
+            this.infoList.Size = new System.Drawing.Size(218, 23);
             this.infoList.TabIndex = 10087;
             // 
             // comboBox2
@@ -90,16 +90,16 @@ namespace C2.Dialogs.WidgetChart
             this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox2.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(70, 59);
+            this.comboBox2.Location = new System.Drawing.Point(82, 69);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(187, 22);
+            this.comboBox2.Size = new System.Drawing.Size(217, 22);
             this.comboBox2.TabIndex = 10086;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("宋体", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Location = new System.Drawing.Point(14, 69);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(52, 15);
             this.label2.TabIndex = 10085;
@@ -109,7 +109,7 @@ namespace C2.Dialogs.WidgetChart
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("宋体", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Location = new System.Drawing.Point(14, 19);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(52, 15);
             this.label1.TabIndex = 10084;
@@ -120,20 +120,23 @@ namespace C2.Dialogs.WidgetChart
             this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(70, 14);
+            this.comboBox1.Location = new System.Drawing.Point(82, 16);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(187, 22);
+            this.comboBox1.Size = new System.Drawing.Size(217, 22);
             this.comboBox1.TabIndex = 10083;
             // 
             // OrganizationForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.ClientSize = new System.Drawing.Size(326, 199);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(369, 212);
             this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.Name = "OrganizationForm";
-            this.Text = "OrganizationForm";
+            this.ShowInTaskbar = true;
+            this.Text = "可视化展示";
             this.Controls.SetChildIndex(this.panel1, 0);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
