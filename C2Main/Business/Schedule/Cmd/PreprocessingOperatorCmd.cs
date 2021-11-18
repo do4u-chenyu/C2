afb05534f33d882f895539b0ca4afaffc27d5a4f
273048269@qq.com <273048269@qq.com>
2021-11-18 17:52:36 +0800

    初步实现群聊数据预处理算子功能

--- C2Main/Business/Schedule/Cmd/PreprocessingOperatorCmd.cs
+++ C2Main/Business/Schedule/Cmd/PreprocessingOperatorCmd.cs
@@ -2,6 +2,7 @@
 using C2.Utils;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,11 +21,44 @@ namespace C2.Business.Schedule.Cmd
         public List<string> GenCmd()
         {
             List<string> cmds = new List<string>();
-            string inputFilePath = inputFilePaths.First();//输入文件
-            string preType = TransInputLine(option.GetOption("pretype"));//取平均值字段
+            string inputFilePath = GetInputFilePath(inputFilePaths.First());//输入文件
+            string preType = option.GetOption("pretype");//取平均值字段
 
-            //cmds.Add(string.Format("sbin\\preprocess.exe {0} {1} {2}", inputFilePath, this.outputFilePath, preType));
+            cmds.Add(string.Format("sbin\\preprocessing.exe {0} {1} {2}", inputFilePath, this.outputFilePath, preType));
             return cmds;
+        }
+
+        private string GetInputFilePath(string path)
+        {
+            /*
+             * 如果输入文件：
+             *     字段个数为1，且（第一列名为说明），那么跳到指定文件夹路径读所有文件；
+             *     字段个数小于5，不处理；
+             */
+
+            FileStream fs_dir = null;
+            StreamReader reader = null;
+            string inputFilePath = path;
+            try
+            {
+                fs_dir = new FileStream(path, FileMode.Open, FileAccess.Read);
+                reader = new StreamReader(fs_dir);
+                string[] columnList = reader.ReadLine().TrimEnd(new char[] { '\r', '\n' }).Split('\t');
+                if (columnList.Length == 1 && columnList[0] == "说明")
+                    inputFilePath = reader.ReadLine().TrimEnd(new char[] { '\r', '\n' });
+                else if (columnList.Length < 5)
+                    inputFilePath = string.Empty;
+            }
+            catch { }
+            finally
+            {
+                if (reader != null)
+                    reader.Close();
+                if (fs_dir != null)
+                    fs_dir.Close();
+            }
+
+            return inputFilePath;
         }
     }
 }
--- tools/preprocessing/DealData.cs
+++ tools/preprocessing/DealData.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +13,10 @@ namespace preprocessing
         private string inputFilePath;
         private string outputFilePath;
         private string preType;
+        private DataTable dataTable;
+        bool deleteAdGroup;
+        bool deleteLongText;
+        bool deletePic;
 
 
         public DealData(string[] param)
@@ -18,22 +24,138 @@ namespace preprocessing
             this.inputFilePath = param[0];
             this.outputFilePath = param[1];
             this.preType = param[2];
+
+            //this.inputFilePath = "C:\\Users\\RedHat\\Desktop\\ql\\GROUPCODE_680692357_QQ.tsv";
+            //this.outputFilePath = "C:\\Users\\RedHat\\Desktop\\ql\\1.txt";
+            //this.preType = "3";
+
+
+            this.dataTable = new DataTable();
+
+            string pretype = DecimalToBinary(int.Parse(preType));
+            this.deleteAdGroup = pretype[0].ToString() == "1";
+            this.deleteLongText = pretype[1].ToString() == "1";
+            this.deletePic = pretype[2].ToString() == "1";
+        }
+
+        private string DecimalToBinary(int decimalNum)
+        {
+            string binaryNum = Convert.ToString(decimalNum, 2);
+            if (binaryNum.Length < 3)
+            {
+                for (int i = 0; i < 3 - binaryNum.Length; i++)
+                {
+                    binaryNum = '0' + binaryNum;
+                }
+            }
+            return binaryNum;
         }
 
         public void Deal()
         {
-            /*
-             * 0、如果输入文件：
-             *        字段个数小于5，且（第一列名为说明），那么跳到指定文件夹路径读所有文件；
-             *        字段个数小于5，不处理；
-             *        大于5，取第五列（群聊内容列）
-             * 
-             * 1、解析类型，去广告群、去长文本、去图片，分三个方法实现
-             * 2、结果写入指定输出路径文件（多个文件的话合并）
-             * 
-             */
-
+            SaveResultToLocal(GenDataTable(inputFilePath));
             return;
+        }
+
+        private DataTable GenDataTable(string path)
+        {
+            DataTable dataTable = new DataTable(Path.GetFileNameWithoutExtension(path));
+
+            string[] columnArray = new string[] { "APPID", "QQNUM", "CAPTURE_TIME", "GROUPCODE", "CONTENT", "DATATYPE", "IP", "IPAREAID", "IP_CITY", "IP_PROVINCE", "DotIP", "Position" };
+
+            // 可能有同名列，这里需要重命名一下
+            Dictionary<string, int> induplicatedName = new Dictionary<string, int>() { };
+            foreach (string col in columnArray)
+            {
+                if (!induplicatedName.ContainsKey(col))
+                {
+                    induplicatedName.Add(col, 0);
+                    dataTable.Columns.Add(col);
+                }
+                else
+                {
+                    induplicatedName[col] += 1;
+                    dataTable.Columns.Add(col + "_" + induplicatedName[col]);
+                }
+            }
+
+            int lineCount = 0;
+            FileStream fs_dir = null;
+            StreamReader reader = null;
+            try
+            {
+                fs_dir = new FileStream(path, FileMode.Open, FileAccess.Read);
+                reader = new StreamReader(fs_dir);
+                string lineStr;
+                while ((lineStr = reader.ReadLine()) != null)
+                {
+                    lineCount++;
+                    if (lineCount == 1 || lineStr == string.Empty)
+                        continue;
+
+                    string[] rowList = lineStr.TrimEnd(new char[] { '\r', '\n' }).Split('\t');
+                    if((deleteLongText && rowList[4].Length > 200) || (deletePic && ContainPic(rowList[4])))
+                        continue;
+
+                    List<string> tmpRowList = new List<string>();
+                    for (int j = 0; j < columnArray.Length; j++)
+                    {
+                        string cellValue = j < rowList.Length ? rowList[j] : "";
+                        tmpRowList.Add(cellValue);
+                    }
+                    dataTable.Rows.Add(tmpRowList.ToArray());
+                }
+            }
+            catch { }
+            finally
+            {
+                if (reader != null)
+                    reader.Close();
+                if (fs_dir != null)
+                    fs_dir.Close();
+            }
+
+            return dataTable;
+        }
+
+        private bool ContainPic(string content)
+        {
+            string[] ends = new string[] { ".pngA", ".gifA", ".jpgA", ".png", ".gif", ".jpg" };
+            foreach(string end in ends)
+            {
+                if (content.EndsWith(end))
+                    return true;
+            } 
+            return false;
+        }
+
+        private void SaveResultToLocal(DataTable dataTable)
+        {
+            StreamWriter sw = null;
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(outputFilePath, FileMode.Create, FileAccess.Write);
+                sw = new StreamWriter(fs, Encoding.UTF8);
+                List<string> colList = new List<string>();
+                for (int i = 0; i < dataTable.Columns.Count; i++)
+                    colList.Add(dataTable.Columns[i].ColumnName);
+
+                sw.WriteLine(string.Join("\t", colList));
+
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    sw.WriteLine(string.Join("\t", row.ItemArray));  
+                }
+            }
+            catch { }
+            finally
+            {
+                if (sw != null)
+                    sw.Close();
+                if (fs != null)
+                    fs.Close();
+            }
         }
     }
 }
