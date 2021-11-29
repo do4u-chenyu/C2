1a8725ac8334dc5d49f3a193db1ec23cdb677213
lu-qi123 <kfc.skywalker@163.com>
2021-11-29 23:02:23 +0800

    MD5小修改

--- C2Plugins/MD5Plugin/EncryptDecrypt/MD5128Plugin.Designer.cs
+++ C2Plugins/MD5Plugin/EncryptDecrypt/MD5128Plugin.Designer.cs
@@ -28,20 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(414, 235);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "移步 喝彩城堡";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(414, 212);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "解密实为撞库";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(414, 189);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "MD5本身不可逆";
             // 
             // MD5128Plugin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.DoubleBuffered = true;
             this.Name = "MD5128Plugin";
+            this.Controls.SetChildIndex(this.inputTextBox, 0);
+            this.Controls.SetChildIndex(this.outputTextBox, 0);
+            this.Controls.SetChildIndex(this.buttonEncode, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.label3, 0);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-       
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
