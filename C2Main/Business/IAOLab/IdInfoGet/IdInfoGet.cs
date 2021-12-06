faec352ce8374d4c7cf66cfbf47aa399c5bf3c9d
XXGYC <1037538120@qq.com>
2021-12-06 19:56:27 +0800

    调整身份证查询代码

--- C2Main/Business/IAOLab/IdInfoGet/IdInfoGet.cs
+++ C2Main/Business/IAOLab/IdInfoGet/IdInfoGet.cs
@@ -3,12 +3,16 @@ using System.IO;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using C2.Utils;
 
 namespace C2.IAOLab.IdInfoGet
 {
-    public class IdInfoGet {
+    public class IdInfoGet
+    {
         private static IdInfoGet instance;
-        public static IdInfoGet GetInstance() {
+        public static IdInfoGet GetInstance()
+        {
             if (instance == null)
                 instance = new IdInfoGet();
             return instance;
@@ -16,80 +20,74 @@ namespace C2.IAOLab.IdInfoGet
 
         public string IdSearch(string input)
         {
-            foreach (var i in input)
+            input = input.Trim();  
+            string idStr = "(^[0-9]{15}$)|(^[1-9][0-9]{5}(19|20)[0-9]{9}[0-9Xx]$)";  
+            if (!Regex.IsMatch(input, idStr))
+                return string.Format("{0}\t{1}\n", input, "身份证号错误");
+            input = CovertTo18(input);
+            string jsonFile = Path.Combine(Application.StartupPath, "Resources/Templates/AreaNum.json");  
+            StreamReader file = File.OpenText(jsonFile);
+            JsonTextReader reader = new JsonTextReader(file);
+            JObject o = (JObject)JToken.ReadFrom(reader);
+            List<string> location = new List<string>();
+            foreach (var i in o)
+                location.Add(i.Key);
+            List<string> result = new List<string>();
+            List<string> municipality = new List<string>() { "11", "12", "31", "50" };   //直辖市编号
+            string areaCode = input.Substring(0, 6);
+            string birth = input.Substring(6, 8);
+            string province = areaCode.Substring(0, 2);
+            string city = areaCode.Substring(0, 4);
+            string year = birth.Substring(0, 4);
+            string month = birth.Substring(4, 2);
+            string day = birth.Substring(6, 2);
+            if (!location.Contains(province + "0000"))   
+                return string.Format("{0}\t{1}\n", input, "身份证号错误");
+            if (!location.Contains(city + "00") && !municipality.Contains(province))  
+                result.Add(o[province + "0000"].ToString());
+            else if (!location.Contains(areaCode)) 
             {
-                if (!char.IsDigit(i))
-                    return string.Empty;
-            }
-            string input1 = input.Trim();
-            if(C15to18(input1) ==string.Empty)
-                return string.Format("{0}{1}{2}{3}",input1,"\t","身份证号错误","\n");
-            List<string> result;
-            result = GetResult(C15to18(input1));
-            return string.Format("{0}{1}{2}{3}{4}{5}{6}{7}{8}{9}", input1,
-                "\t", C15to18(input1), "\t", result[0], "\t", result[1], "\t", result[2], "\n");
-        }
-
-        private static string C15to18(string input){
-            if (input.Length == 15)
-            {
-                char[] strJY = { '1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2' };
-                int[] intJQ = { 7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2, 1 };
-                int intTemp = 0;
-                string strTemp;
-                if (int.Parse(input.Substring(6, 2)) <= 20)
-                    strTemp = input.Substring(0, 6) + "20" + input.Substring(6);
+                if (municipality.Contains(province))
+                    result.Add(o[province + "0000"].ToString());
                 else
-                    strTemp = input.Substring(0, 6) + "19" + input.Substring(6);
-                for (int i = 0; i <= strTemp.Length - 1; i++)
-                    intTemp += int.Parse(strTemp.Substring(i, 1)) * intJQ[i];
-                intTemp %= 11;
-                return strTemp + strJY[intTemp];
+                    result.Add(o[province + "0000"].ToString() + o[city + "00"].ToString());
             }
             else
-                return input.Length == 18 ? input : string.Empty; 
-        }
-
-        private static List<string> GetResult(string input)
-        {
-            string jsonfile = Path.Combine(Application.StartupPath, "Resources/Templates/AreaNum.json");
-            using (StreamReader file = File.OpenText(jsonfile))
             {
-                using (JsonTextReader reader = new JsonTextReader(file))
-                {
-                    JObject o = (JObject)JToken.ReadFrom(reader);
-                    List<string> location = new List<string>();
-                    foreach (var i in o)
-                        location.Add(i.Key);
-                    List<string> result = new List<string>();
-                    List<string> municipality = new List<string>() { "11", "12", "31", "50" };
-                    string locNum = input.Substring(0, 6);
-                    string birth = input.Substring(6, 8);
-                    int gender = int.Parse(input.Substring(16, 1));
-                    if(!location.Contains(locNum.Substring(0, 2) + "0000"))   
-                        result.Add("归属地错误");
-                    else if (!location.Contains(locNum.Substring(0, 4) + "00")  && !municipality.Contains(locNum.Substring(0, 2)))  
-                        result.Add(o[locNum.Substring(0, 2) + "0000"].ToString());
-                    else if (!location.Contains(locNum))    
-                    {
-                        if (municipality.Contains(locNum.Substring(0, 2)))
-                            result.Add(o[locNum.Substring(0, 2) + "0000"].ToString());
-                        else
-                            result.Add(o[locNum.Substring(0, 2) + "0000"].ToString() + o[locNum.Substring(0, 4) + "00"].ToString());
-                    }
-                    else         
-                    {
-                        if (municipality.Contains(locNum.Substring(0, 2)))
-                            result.Add(o[locNum.Substring(0, 2) + "0000"].ToString() + o[locNum].ToString());
-                        else
-                            result.Add(o[locNum.Substring(0, 2) + "0000"].ToString() + o[locNum.Substring(0, 4) + "00"].ToString() + o[locNum].ToString());
-                    }
-                    result.Add(birth.Substring(0, 4) + "年" + birth.Substring(4, 2) + "月" + birth.Substring(6, 2) + "日");
-                    result.Add(gender % 2 != 0 ? "男" : "女");
-                    return result;
-                }
+                if (municipality.Contains(province))
+                    result.Add(o[province + "0000"].ToString() + o[areaCode].ToString());
+                else
+                    result.Add(o[province + "0000"].ToString() + o[city + "00"].ToString() + o[areaCode].ToString());
             }
-        }      
+            result.Add(year + "年" + month + "月" + day + "日");
+            int gender = ConvertUtil.TryParseInt(input.Substring(16, 1), 1);
+            result.Add(gender % 2 != 0 ? "男" : "女");
+            return string.Format("{0}\t{1}\t{2}\t{3}\t{4}\n",
+                input,      //原始数据
+                CovertTo18(input),   //转18位
+                result[0],    //归属地
+                result[1],    //生日
+                result[2]);   //性别
+        }
+        private static string CovertTo18(string input)
+        {
+            if (input.Length == 18)
+                return input;
+            if (input.Length != 15)
+                return string.Empty;
+            else
+            {
+                char[] JY = { '1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2' };
+                int[] JQ = { 7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2, 1 };
+                int sum = 0;
+                string strTemp;
+                int yy = ConvertUtil.TryParseInt(input.Substring(6, 2));
+                strTemp = input.Substring(0, 6) + (yy <= 20 ? "20" : "19") + input.Substring(6);
+                for (int i = 0; i < strTemp.Length; i++)
+                    sum += int.Parse(strTemp.Substring(i, 1)) * JQ[i];
+                return strTemp + JY[sum % 11];
+            }
+        }
     }
 }
 
