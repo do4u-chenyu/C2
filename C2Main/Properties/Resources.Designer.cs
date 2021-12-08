1a90fb6522f9ffe145b218b42617a841b3506f82
XXGYC <1037538120@qq.com>
2021-12-08 15:24:07 +0800

    调整身份证查询代码

--- C2Main/Properties/Resources.Designer.cs
+++ C2Main/Properties/Resources.Designer.cs
@@ -47,8 +47,8 @@ namespace C2.Properties {
         }
         
         /// <summary>
-        ///   重写当前线程的 CurrentUICulture 属性
-        ///   重写当前线程的 CurrentUICulture 属性。
+        ///   重写当前线程的 CurrentUICulture 属性，对
+        ///   使用此强类型资源类的所有资源查找执行重写。
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
         public static global::System.Globalization.CultureInfo Culture {
@@ -177,6 +177,57 @@ namespace C2.Properties {
             get {
                 object obj = ResourceManager.GetObject("Apk", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查找类似 110000:北京市
+        ///110101:东城区
+        ///110102:西城区
+        ///110105:朝阳区
+        ///110106:丰台区
+        ///110107:石景山区
+        ///110108:海淀区
+        ///110109:门头沟区
+        ///110111:房山区
+        ///110112:通州区
+        ///110113:顺义区
+        ///110114:昌平区
+        ///110115:大兴区
+        ///110116:怀柔区
+        ///110117:平谷区
+        ///110118:密云区
+        ///110119:延庆区
+        ///120000:天津市
+        ///120101:和平区
+        ///120102:河东区
+        ///120103:河西区
+        ///120104:南开区
+        ///120105:河北区
+        ///120106:红桥区
+        ///120110:东丽区
+        ///120111:西青区
+        ///120112:津南区
+        ///120113:北辰区
+        ///120114:武清区
+        ///120115:宝坻区
+        ///120116:滨海新区
+        ///120117:宁河区
+        ///120118:静海区
+        ///120119:蓟州区
+        ///130000:河北省
+        ///130100:石家庄市
+        ///130102:长安区
+        ///130104:桥西区
+        ///130105:新华区
+        ///130107:井陉矿区
+        ///130108:裕华区
+        ///130109:藁城区
+        ///130 [字符串的其余部分被截断]&quot;; 的本地化字符串。
+        /// </summary>
+        public static string AreaNum {
+            get {
+                return ResourceManager.GetString("AreaNum", resourceCulture);
             }
         }
         
