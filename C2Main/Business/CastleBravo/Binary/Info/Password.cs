6e35185c6e5a328c8b87a5894f22ca233fe72edb
lu-qi123 <kfc.skywalker@163.com>
2021-12-24 17:47:12 +0800

    冰鞋解密施工中

---
+++ C2Main/Business/CastleBravo/Binary/Info/Password.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C2.Business.CastleBravo.Binary.Info
+{
+    class Password
+    {
+        private static Password instance = null;
+
+        public static Password GetInstance()
+        {
+            if (instance == null)
+                instance = new Password();
+            return instance;
+        }
+
+        private Password()
+        {
+
+        }
+    }
+}
