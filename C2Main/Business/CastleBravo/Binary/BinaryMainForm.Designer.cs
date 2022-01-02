714a9633867bb4e926ffea968593d30715f1e155
lu-qi123 <kfc.skywalker@163.com>
2022-01-02 15:12:13 +0800

    二进制专项小修改

--- C2Main/Business/CastleBravo/Binary/BinaryMainForm.Designer.cs
+++ C2Main/Business/CastleBravo/Binary/BinaryMainForm.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BinaryMainForm));
-            this.label1 = new System.Windows.Forms.Label();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.StringsTabPage = new System.Windows.Forms.TabPage();
             this.label6 = new System.Windows.Forms.Label();
@@ -62,8 +61,6 @@
             this.progressBar = new System.Windows.Forms.ProgressBar();
             this.BehinderDTextBox = new System.Windows.Forms.TextBox();
             this.label10 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.BehinderETabPage = new System.Windows.Forms.TabPage();
             this.label17 = new System.Windows.Forms.Label();
             this.label16 = new System.Windows.Forms.Label();
@@ -80,16 +77,6 @@
             this.BehinderDTabPage.SuspendLayout();
             this.BehinderETabPage.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("宋体", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(269, 204);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(159, 33);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "施工中...";
             // 
             // tabControl1
             // 
@@ -113,7 +100,6 @@
             this.StringsTabPage.Controls.Add(this.ResultTB);
             this.StringsTabPage.Controls.Add(this.label5);
             this.StringsTabPage.Controls.Add(this.label4);
-            this.StringsTabPage.Controls.Add(this.label1);
             this.StringsTabPage.Location = new System.Drawing.Point(4, 26);
             this.StringsTabPage.Name = "StringsTabPage";
             this.StringsTabPage.Padding = new System.Windows.Forms.Padding(3);
@@ -138,7 +124,7 @@
             this.FileButton.Name = "FileButton";
             this.FileButton.Size = new System.Drawing.Size(63, 23);
             this.FileButton.TabIndex = 6;
-            this.FileButton.Text = "+分析";
+            this.FileButton.Text = "+打开";
             this.FileButton.UseVisualStyleBackColor = true;
             this.FileButton.Click += new System.EventHandler(this.FileButton_Click);
             // 
@@ -312,8 +298,6 @@
             this.BehinderDTabPage.Controls.Add(this.progressBar);
             this.BehinderDTabPage.Controls.Add(this.BehinderDTextBox);
             this.BehinderDTabPage.Controls.Add(this.label10);
-            this.BehinderDTabPage.Controls.Add(this.label8);
-            this.BehinderDTabPage.Controls.Add(this.label3);
             this.BehinderDTabPage.Location = new System.Drawing.Point(4, 26);
             this.BehinderDTabPage.Name = "BehinderDTabPage";
             this.BehinderDTabPage.Padding = new System.Windows.Forms.Padding(3);
@@ -437,24 +421,6 @@
             this.label10.TabIndex = 6;
             this.label10.Text = "破解三代冰蝎(Behinder)的加密流量报文(AES128)";
             // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(8, 10);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(0, 17);
-            this.label8.TabIndex = 5;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("宋体", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label3.Location = new System.Drawing.Point(255, 250);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(159, 33);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "施工中...";
-            // 
             // BehinderETabPage
             // 
             this.BehinderETabPage.Controls.Add(this.label18);
@@ -545,7 +511,7 @@
             // 
             // openFileDialog1
             // 
-            this.openFileDialog1.Filter = "二进制文件|*.exe;*.so;*.dll;*.lib;*.a;*.dat;*.bin|所有文件|*.*";
+            this.openFileDialog1.Filter = "二进制文件|*.exe;*.so;*.dll;*.lib;*.a;*.dat;*.bin;*.*";
             // 
             // label18
             // 
@@ -585,17 +551,14 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage StringsTabPage;
         private System.Windows.Forms.TabPage XiseTabPage;
         private System.Windows.Forms.TabPage BehinderDTabPage;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Label label8;
         private System.Windows.Forms.TextBox ResultTB;
         private System.Windows.Forms.Button FileButton;
         private System.Windows.Forms.TextBox FileTB;
