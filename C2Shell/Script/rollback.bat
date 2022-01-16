a43e3afa751f86d05e0fb4fcfce4dc8e89227a8c
lu-qi123 <kfc.skywalker@163.com>
2022-01-16 15:31:55 +0800

    删除C2Shell和在线更新功能，同步修改安装脚本，打包脚本，名称改成 分析师单兵作战装备

--- C2Shell/Script/rollback.bat
+++
@@ -1,5 +0,0 @@
-tasklist|findstr C2.exe
-cd /d %~dp0
-if  not exist .\backup\C2.exe  (EXIT) 
-if  not exist .\C2.exe  (EXIT) 
-if  %errorlevel%==1 (del .\C2.exe & copy .\update\backup\C2.exe  .\  & EXIT )
