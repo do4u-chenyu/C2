7cc0d592244eb4b5940479e4b1ff416eb05958f7
lu-qi123 <kfc.skywalker@163.com>
2022-02-02 22:15:17 +0800

    小修改

--- C2Main/Controls/C1/Left/ManualButton.cs
+++ C2Main/Controls/C1/Left/ManualButton.cs
@@ -14,7 +14,7 @@ namespace C2.Controls.C1.Left
             this.desc = "战术手册";
             this.contextMenuStrip.Items.Remove(DeleteToolStripMenuItem);
             this.toolTip1.SetToolTip(this.lelfPictureBox, "经典分析战术");
-            this.lelfPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.lelfPictureBox.SizeMode = PictureBoxSizeMode.Zoom;
             this.lelfPictureBox.Image = global::C2.Properties.Resources.战术手册;
         }
 
