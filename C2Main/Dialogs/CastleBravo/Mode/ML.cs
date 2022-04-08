db05de7c5b44cca36df81f459ac98b51d8c2c54a
lu-qi123 <kfc.skywalker@163.com>
2022-04-08 19:38:49 +0800

    MD5加盐破解继续施工中

--- C2Main/Dialogs/CastleBravo/Mode/ML.cs
+++ C2Main/Dialogs/CastleBravo/Mode/ML.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using C2.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,38 +9,80 @@ namespace C2.Dialogs.CastleBravo.Mode
 {
     class ML
     {
-        public static string MD5_Pass_Salt(string pass, string salt, string u) { return string.Empty; }
-        public static string MD5_Salt_Pass(string pass, string salt, string u) { return string.Empty; }
+        // 模式01: MD5($Pass.$Salt)
+        public static string MD5_Pass_Salt(string pass, string salt, string u) 
+        {
+            return ST.GenerateMD5(pass + salt);
+        }
+        // 模式02: MD5($Salt.$Pass)
+        public static string MD5_Salt_Pass(string pass, string salt, string u) 
+        {
+            return ST.GenerateMD5(salt + pass);
+        }
+        // 模式03: MD5($Salt.$Pass.$Salt)
         public static string MD5_Salt_Pass_Salt(string pass, string salt, string u) { return string.Empty; }
+        // 模式04: MD5(MD5($Pass).$Salt)
         public static string MD5_MD5_Pass_Salt(string pass, string salt, string u) { return string.Empty; }
+        // 模式05: MD5($Salt.MD5($Pass))
         public static string MD5_Salt_MD5_Pass(string pass, string salt, string u) { return string.Empty; }
+        // 模式06: MD5(MD5($Pass.$Salt))
         public static string MD5_MD5_Pass_Salt_2(string pass, string salt, string u) { return string.Empty; }
+        // 模式07: MD5(MD5($Salt.$Pass))
         public static string MD5_MD5_Salt_Pass(string pass, string salt, string u) { return string.Empty; }
+        // 模式08: MD5(MD5($Pass).MD5($Salt))
         public static string MD5_MD5_Pass_MD5_Salt(string pass, string salt, string u) { return string.Empty; }
+        // 模式09: MD5(MD5($Salt).MD5($Pass))
         public static string MD5_MD5_Salt_MD5_Pass(string pass, string salt, string u) { return string.Empty; }
+        // 模式10: MD5($Salt.MD5($Salt.$Pass))
         public static string MD5_Salt_MD5_Salt_Pass(string pass, string salt, string u) { return string.Empty; }
+        // 模式11: MD5($Salt.MD5($Pass.$Salt))
         public static string MD5_Salt_MD5_Pass_Salt(string pass, string salt, string u) { return string.Empty; }
+        // 模式12: MD5(MD5($Salt.$Pass).$Salt)
         public static string MD5_MD5_Salt_Pass_Salt(string pass, string salt, string u) { return string.Empty; }
+        // 模式13: MD5(MD5($Pass.$Salt).$Salt)
         public static string MD5_MD5_Pass_Salt_Salt(string pass, string salt, string u) { return string.Empty; }
+        // 模式14: MD5($U.$Pass.$Salt)
         public static string MD5_U_Pass_Salt(string pass, string salt, string u) { return string.Empty; }
-        public static string MD5_Salt_U_Pass(string pass, string salt, string u) { return string.Empty; }
-        public static string MD5_Pass_U_Salt(string pass, string salt, string u) { return string.Empty; }
+        // 模式15: MD5($U.$Salt.$Pass)
         public static string MD5_U_Salt_Pass(string pass, string salt, string u) { return string.Empty; }
-        public static string MD5_U_Pass_MD5_Salt(string pass, string salt, string u) { return string.Empty; }
-        public static string MD5_U_MD5_Pass_Salt(string pass, string salt, string u) { return string.Empty; }
-        public static string MD5_MD5_U_Pass_Salt(string pass, string salt, string u) { return string.Empty; }
-        public static string MD5_MD5_U_Pass_Salt_2(string pass, string salt, string u) { return string.Empty; }
-        public static string MD5_MD5_U_Pass_Salt_3(string pass, string salt, string u) { return string.Empty; }
-        public static string MD5_MD5_U_MD5_Pass_Salt(string pass, string salt, string u) { return string.Empty; }
-        public static string MD5_MD5_U_MD5_Pass_MD5_Salt(string pass, string salt, string u) { return string.Empty; }
-        public static string MD5_MD5_U_Pass_MD5_Salt(string pass, string salt, string u) { return string.Empty; }
-        public static string MD5_U_MD5_Pass_MD5_Salt(string pass, string salt, string u) { return string.Empty; }
-        public static string MD5_U_MD5_Pass_Salt_2(string pass, string salt, string u) { return string.Empty; }
-        public static string MD5_Pass_Salt_U(string pass, string salt, string u) { return string.Empty; }
+
+        // 模式16: MD5($Salt.$U.$Pass)
+        public static string MD5_Salt_U_Pass(string pass, string salt, string u) { return string.Empty; }
+        
+        // 模式17: MD5($Salt.$Pass.$U)
         public static string MD5_Salt_Pass_U(string pass, string salt, string u) { return string.Empty; }
+        // 模式18: MD5($Pass.$Salt.$U)
+        public static string MD5_Pass_Salt_U(string pass, string salt, string u) { return string.Empty; }
+        // 模式19: MD5($Pass.$U.$Salt)
+        public static string MD5_Pass_U_Salt(string pass, string salt, string u) { return string.Empty; }
+
+        // 模式20: MD5($U.$Pass.MD5($Salt))
+        public static string MD5_U_Pass_MD5_Salt(string pass, string salt, string u) { return string.Empty; }
+        // 模式21: MD5($U.MD5($Pass).$Salt)
+        public static string MD5_U_MD5_Pass_Salt(string pass, string salt, string u) { return string.Empty; }
+        // 模式22: MD5(MD5($U).$Pass.$Salt)
+        public static string MD5_MD5_U_Pass_Salt(string pass, string salt, string u) { return string.Empty; }
+        // 模式23: MD5(MD5($U.$Pass).$Salt)
+        public static string MD5_MD5_U_Pass_Salt_2(string pass, string salt, string u) { return string.Empty; }
+        // 模式24: MD5(MD5($U.$Pass.$Salt))
+        public static string MD5_MD5_U_Pass_Salt_3(string pass, string salt, string u) { return string.Empty; }
+        // 模式25: MD5(MD5($U.MD5($Pass)).$Salt)
+        public static string MD5_MD5_U_MD5_Pass_Salt(string pass, string salt, string u) { return string.Empty; }
+        // 模式26: MD5(MD5($U).MD5($Pass).MD5($Salt))
+        public static string MD5_MD5_U_MD5_Pass_MD5_Salt(string pass, string salt, string u) { return string.Empty; }
+        // 模式27: MD5(MD5($U).$Pass.MD5($Salt))
+        public static string MD5_MD5_U_Pass_MD5_Salt(string pass, string salt, string u) { return string.Empty; }
+        // 模式28: MD5($U.MD5($Pass).MD5($Salt))
+        public static string MD5_U_MD5_Pass_MD5_Salt(string pass, string salt, string u) { return string.Empty; }
+        // 模式29: MD5($U.MD5($Pass.$Salt))
+        public static string MD5_U_MD5_Pass_Salt_2(string pass, string salt, string u) { return string.Empty; }
+        // 模式30: MD5(MD5($Salt).$Pass)
         public static string MD5_MD5_Salt_Pass_2(string pass, string salt, string u) { return string.Empty; }
+        // 模式31: MD5(SHA1($Pass))
         public static string MD5_SHA1(string pass, string salt, string u) { return string.Empty; }
+        // 模式32: MD5(SHA256($Pass))
         public static string MD5_SHA256(string pass, string salt, string u) { return string.Empty; }
+        // 模式33: MD5(SHA512($Pass))
         public static string MD5_SHA512(string pass, string salt, string u) { return string.Empty; }
     }
 }
