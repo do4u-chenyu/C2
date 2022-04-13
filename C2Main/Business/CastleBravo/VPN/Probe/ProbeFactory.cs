efef9f6d958ebf5bde48bc34488b9a2d782876f5
刘晓凤 <lxfjiangnan@163.com>
2022-04-13 10:05:20 +0800

    随机探针施工

---
+++ C2Main/Business/CastleBravo/VPN/Probe/ProbeFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C2.Business.CastleBravo.VPN.Probe
+{
+    class ProbeFactory
+    {
+        public static void GenRandomProbe()
+        {
+            Random random = new Random();
+        }
+
+    }
+}
