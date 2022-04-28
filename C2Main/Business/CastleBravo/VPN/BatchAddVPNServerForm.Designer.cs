ab524552622e6dfcbd5e956940f5dc583caaf218
lu-qi123 <kfc.skywalker@163.com>
2022-04-28 16:16:35 +0800

    批量添加VPN增加支持订阅地址的接口

--- C2Main/Business/CastleBravo/VPN/BatchAddVPNServerForm.Designer.cs
+++ C2Main/Business/CastleBravo/VPN/BatchAddVPNServerForm.Designer.cs
@@ -37,6 +37,11 @@ namespace C2.Business.CastleBravo.VPN
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.behinder3 = new System.Windows.Forms.RadioButton();
+            this.trojan = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label3
@@ -44,12 +49,12 @@ namespace C2.Business.CastleBravo.VPN
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label3.ForeColor = System.Drawing.Color.Red;
-            this.label3.Location = new System.Drawing.Point(4, 98);
+            this.label3.Location = new System.Drawing.Point(33, 82);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(413, 18);
+            this.label3.Size = new System.Drawing.Size(503, 18);
             this.label3.TabIndex = 10029;
-            this.label3.Text = "* 格式:一行一个,支持ss,ssr,vmess,vless,trojan";
+            this.label3.Text = "* 格式:一行一个,支持ss,ssr,vmess,vless,trojan和订阅地址";
             // 
             // wsTextBox
             // 
@@ -80,7 +85,7 @@ namespace C2.Business.CastleBravo.VPN
             // browserButton
             // 
             this.browserButton.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.browserButton.Location = new System.Drawing.Point(548, 45);
+            this.browserButton.Location = new System.Drawing.Point(548, 36);
             this.browserButton.Margin = new System.Windows.Forms.Padding(4);
             this.browserButton.Name = "browserButton";
             this.browserButton.Size = new System.Drawing.Size(88, 34);
@@ -93,7 +98,7 @@ namespace C2.Business.CastleBravo.VPN
             // 
             this.filePathTextBox.BackColor = System.Drawing.Color.White;
             this.filePathTextBox.Font = new System.Drawing.Font("微软雅黑", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.filePathTextBox.Location = new System.Drawing.Point(144, 44);
+            this.filePathTextBox.Location = new System.Drawing.Point(144, 35);
             this.filePathTextBox.Margin = new System.Windows.Forms.Padding(4);
             this.filePathTextBox.Name = "filePathTextBox";
             this.filePathTextBox.ReadOnly = true;
@@ -104,7 +109,7 @@ namespace C2.Business.CastleBravo.VPN
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("微软雅黑", 9.75F, System.Drawing.FontStyle.Bold);
-            this.label2.Location = new System.Drawing.Point(24, 46);
+            this.label2.Location = new System.Drawing.Point(24, 37);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(112, 27);
@@ -135,15 +140,67 @@ namespace C2.Business.CastleBravo.VPN
             this.label5.TabIndex = 10031;
             this.label5.Text = "大约10W-20W行";
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(24, 116);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 27);
+            this.label4.TabIndex = 10032;
+            this.label4.Text = "地址类型：";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.groupBox1.Controls.Add(this.behinder3);
+            this.groupBox1.Controls.Add(this.trojan);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.groupBox1.Location = new System.Drawing.Point(144, 99);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(394, 54);
+            this.groupBox1.TabIndex = 10033;
+            this.groupBox1.TabStop = false;
+            // 
+            // behinder3
+            // 
+            this.behinder3.AutoSize = true;
+            this.behinder3.Location = new System.Drawing.Point(223, 21);
+            this.behinder3.Margin = new System.Windows.Forms.Padding(4);
+            this.behinder3.Name = "behinder3";
+            this.behinder3.Size = new System.Drawing.Size(105, 22);
+            this.behinder3.TabIndex = 10021;
+            this.behinder3.Text = "订阅地址";
+            this.behinder3.UseVisualStyleBackColor = true;
+            // 
+            // trojan
+            // 
+            this.trojan.AutoSize = true;
+            this.trojan.Checked = true;
+            this.trojan.Location = new System.Drawing.Point(9, 21);
+            this.trojan.Margin = new System.Windows.Forms.Padding(4);
+            this.trojan.Name = "trojan";
+            this.trojan.Size = new System.Drawing.Size(177, 22);
+            this.trojan.TabIndex = 10020;
+            this.trojan.TabStop = true;
+            this.trojan.Text = "服务器(分享)地址";
+            this.trojan.UseVisualStyleBackColor = true;
+            // 
             // BatchAddVPNServerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(656, 488);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.wsTextBox);
             this.Controls.Add(this.pasteModeCB);
             this.Controls.Add(this.browserButton);
@@ -158,9 +215,13 @@ namespace C2.Business.CastleBravo.VPN
             this.Controls.SetChildIndex(this.browserButton, 0);
             this.Controls.SetChildIndex(this.pasteModeCB, 0);
             this.Controls.SetChildIndex(this.wsTextBox, 0);
-            this.Controls.SetChildIndex(this.label3, 0);
             this.Controls.SetChildIndex(this.label5, 0);
             this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.groupBox1, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
+            this.Controls.SetChildIndex(this.label3, 0);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -175,5 +236,9 @@ namespace C2.Business.CastleBravo.VPN
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton behinder3;
+        private System.Windows.Forms.RadioButton trojan;
     }
 }
