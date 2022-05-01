1565773aa5bf883efc10f3aac00809097792ab90
刘晓凤 <lxfjiangnan@163.com>
2022-05-01 11:33:50 +0800

    随机探针配置窗体设置

---
+++ C2Main/Business/CastleBravo/VPN/ProbeForm.random.cs
@@ -0,0 +1,31 @@
+﻿using C2.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C2.Business.CastleBravo.VPN
+{
+    partial class ProbeForm : StandardDialog
+    {
+        public ProbeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label0_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
