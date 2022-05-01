dee1dad5435f1f0620e0ebc1975b2d10a2dbf494
刘晓凤 <lxfjiangnan@163.com>
2022-05-01 15:30:42 +0800

    随机探针窗体微调

--- C2Main/Business/CastleBravo/VPN/RandomProbeForm.Designer.cs
+++ C2Main/Business/CastleBravo/VPN/RandomProbeForm.Designer.cs
@@ -42,7 +42,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(17, 22);
+            this.label1.Location = new System.Drawing.Point(17, 10);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(65, 12);
             this.label1.TabIndex = 10003;
@@ -51,7 +51,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(17, 53);
+            this.label2.Location = new System.Drawing.Point(17, 41);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(59, 12);
             this.label2.TabIndex = 10004;
@@ -60,7 +60,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(197, 22);
+            this.label3.Location = new System.Drawing.Point(197, 10);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(59, 12);
             this.label3.TabIndex = 10005;
@@ -69,7 +69,7 @@
             // timeoutBox
             // 
             this.timeoutBox.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.timeoutBox.Location = new System.Drawing.Point(87, 18);
+            this.timeoutBox.Location = new System.Drawing.Point(87, 6);
             this.timeoutBox.Name = "timeoutBox";
             this.timeoutBox.Size = new System.Drawing.Size(89, 21);
             this.timeoutBox.TabIndex = 10006;
@@ -78,7 +78,7 @@
             // lengthBox
             // 
             this.lengthBox.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.lengthBox.Location = new System.Drawing.Point(87, 49);
+            this.lengthBox.Location = new System.Drawing.Point(87, 37);
             this.lengthBox.Name = "lengthBox";
             this.lengthBox.Size = new System.Drawing.Size(281, 21);
             this.lengthBox.TabIndex = 10008;
@@ -86,21 +86,20 @@
             // 
             // probeContextBox
             // 
-            this.probeContextBox.BackColor = System.Drawing.Color.White;
+            this.probeContextBox.BackColor = System.Drawing.SystemColors.Menu;
             this.probeContextBox.Font = new System.Drawing.Font("微软雅黑", 8F);
-            this.probeContextBox.Location = new System.Drawing.Point(9, 128);
+            this.probeContextBox.Location = new System.Drawing.Point(5, 116);
             this.probeContextBox.MaxLength = 16777216;
             this.probeContextBox.Multiline = true;
             this.probeContextBox.Name = "probeContextBox";
-            this.probeContextBox.ReadOnly = true;
-            this.probeContextBox.Size = new System.Drawing.Size(379, 165);
+            this.probeContextBox.Size = new System.Drawing.Size(385, 172);
             this.probeContextBox.TabIndex = 10029;
             this.probeContextBox.WordWrap = false;
             // 
             // numBox
             // 
             this.numBox.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.numBox.Location = new System.Drawing.Point(278, 18);
+            this.numBox.Location = new System.Drawing.Point(278, 6);
             this.numBox.Name = "numBox";
             this.numBox.Size = new System.Drawing.Size(89, 21);
             this.numBox.TabIndex = 10030;
@@ -109,7 +108,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(17, 113);
+            this.label4.Location = new System.Drawing.Point(17, 101);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(161, 12);
             this.label4.TabIndex = 10031;
@@ -119,10 +118,10 @@
             // 
             this.infoBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.infoBox.ForeColor = System.Drawing.Color.Red;
-            this.infoBox.Location = new System.Drawing.Point(17, 79);
+            this.infoBox.Location = new System.Drawing.Point(17, 67);
             this.infoBox.Multiline = true;
             this.infoBox.Name = "infoBox";
-            this.infoBox.Size = new System.Drawing.Size(378, 31);
+            this.infoBox.Size = new System.Drawing.Size(368, 31);
             this.infoBox.TabIndex = 10033;
             this.infoBox.Text = "* 说明:\'-\'表示取连续区间,\',\'表示离散区间。如测试探针长度区间是1到6，8，10到12可写为1-6，8，10-12";
             // 
@@ -131,7 +130,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(397, 344);
+            this.ClientSize = new System.Drawing.Size(394, 339);
             this.Controls.Add(this.infoBox);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.numBox);
