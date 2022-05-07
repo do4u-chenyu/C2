478bef4bf7981641437d79742d044ab800a69572
An Ri <weilei.gakki@gmail.com>
2022-05-07 15:44:53 +0800

    盗洞模型代码删减和优化

--- C2Main/Resources/Script/IAO_Search_gamble/batchquery_hackDD_accountPass_C2_20211126.py
+++ C2Main/Resources/Script/IAO_Search_gamble/batchquery_hackDD_accountPass_C2_20211126.py
@@ -1,55 +1,42 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Oct 29 09:08:44 2018
-  version 0621:
-      增加根据登陆控件和控件值生成keyword功能，实现函数：prdoucKey
-  version 0628:
-      增加查询：
-          去除登陆控件值；
-          挑选 只包含admin的url 
-  version 0708:
-      增加线程时间检测功能
-      增加文件》50M即压缩一次功能
-  version 0803:
-       修改了脚本临时文件命名
-       修改了脚本压缩加密方式
-  version 0224:
-       增加机场查询脚本
-@author: Administrator
-"""
-from Queue import Queue
-from threading import Thread
+# -*- encoding: utf-8 -*-
+# @File : batchquery_hackDD_accountPass_C2_20211126.py
+# @Author : 安替
+# @Time : 2022/5/7 11:38
+# @Software : PyCharm
+# Python版本：3.6.3
+
 from subprocess import Popen,PIPE
-import time
-import urllib
 import re
 import io 
 import sys
 import datetime
 import os
-import itertools
 import logging
 import urllib2
-from urllib import unquote
 from optparse import OptionParser
 reload(sys)
 sys.setdefaultencoding('utf-8')
-#################
+
 
 class BatchQuery:
-    def __init__(self,data_path,startTime,endTime,all_items):
+    def __init__(self,data_path, startTime, endTime, all_items):
         self.data_path = data_path
         self.startTime = startTime
         self.endTime   = endTime
         self.all_items = all_items
 
-    def queryclient(self,keyWords,queryType):
+    def queryclient(self, keyWords, queryType):
+        """
+        :param keyWords
+        :param queryType
+        :return data
+        """
         batch = []
         cont_flag = False
         cont_end_flag = True
         content = ''
         end_item = '_QUERY_MATCHTERMS'
-        with open(os.path.join(self.data_path,'result1.log'),'a+') as f:
+        with open(os.path.join(self.data_path, 'result.log'), 'a+') as f:
             cmd = [
                 '/home/search/sbin/queryclient',
                 '--server', '127.0.0.1',
@@ -65,7 +52,7 @@ class BatchQuery:
             LOGGER.info('QUERYTIME:{0}_{1}\n wait...'.format(self.startTime,self.endTime)) 
             #req = Popen(['/home/search/sbin/queryclient','--server','127.0.0.1','--port', '9870','--querystring', keyWords,'--start', startTime,'--end', endTime,'--contextlen','10000','--maxcount', '10000'],stdout=PIPE)
             for line in req.stdout:
-                if queryType == 'airport':
+                if queryType == 'hackDD':
                     f.write(line)
                 line  = line.replace('\x1a','')
                 if line == '\n' and cont_flag and cont_end_flag:
@@ -81,7 +68,7 @@ class BatchQuery:
                     k, v = map(lambda x: x.strip(), line.split(':', 1))
                     if k == end_item:
                         # cont_end_flag = True
-                        batch.append(('_QUERY_CONTENT',content))
+                        batch.append(('_QUERY_CONTENT', content))
                         content = ''
                         cont_end_flag = True
                     elif k == '_QUERY_CONTENT':
@@ -104,7 +91,7 @@ class BatchQuery:
             req = urllib2.Request(file_url)
             response = urllib2.urlopen(req, timeout=2)
             content = response.read().decode()
-            pload = content.split('\n')[-1].replace('\t','').replace('\r','').replace('\n','').strip()
+            pload = content.split('\n')[-1].replace('\t', '').replace('\r', '').replace('\n', '').strip()
             method = content.split(' ')[0]
             if(len(pload)>100 and len(pload)<1000):
                 return pload.strip(), method
@@ -114,69 +101,18 @@ class BatchQuery:
         except Exception, e:
             return 'http文件获取失败','http文件获取失败'
 
-    def get_airport_user(self, content):
-        user_key     = ['email']
-        password_key = ['passwd','password']
-        user_str = ''
-        pass_str = ''
-        for part in re.split(r'\.\.\.',content):
-            for line in re.split('[\r\n&;]+',part):
-                try:
-                    [key, value] = line.split("=")
-                    if len(value.strip()) < 1:
-                        continue
-                    if key in user_key:
-                        user_str = value
-                    elif key in password_key:
-                        pass_str = value
-                except:
-                    pass
-        return user_str,pass_str
-
-    def find_keyword_position(self,kw, d):
-        lk = []
-        for k, v in d.items():
-            if kw in v:
-                lk.append(k)
-
-        lv = kw.strip().split(' ')
-        return lv, lk
-
-    def position_match(self,data, KEY_WORDS, pload, d):
-        lv, lk = self.find_keyword_position(KEY_WORDS, d)
-        for i in lk:
-            flag = 0
-            if (i == 'PAYLOAD'):
-                k = pload
-            else:
-                k = data.get(i, '')
-            for j in lv:
-                if j not in k:
-                    flag = 1
-                    break
-            if (flag == 0):
-                return data, pload
-        pload = 'http文件获取失败'
-        return data, pload
-
-    
 
     def handle_res(self):
-        ##df = pd.read_csv(r'C:\Users\Administrator\Desktop\ws_out.txt', sep='\t', dtype=str, error_bad_lines=False)
-        ##df1 = df[['_HOST', '_RELATIVEURL', 'PAYLOAD']]
-        ##df2 = df1.drop_duplicates()
-        # print(df2)
-        ##df2.to_csv('qw_res.txt', sep='\t', index=False, header=False)
         tmplist = []
         out_file = 'ws_out.txt'
         result_file = 'qw_res.txt'
         with open(os.path.join(self.data_path, out_file), "r") as f:
             with open(os.path.join(self.data_path,result_file), "w") as f1:
                 lines = f.readlines()
-                for line in lines[1:]:  ##跳过第一行title
+                for line in lines[1:]:  #跳过第一行title
                     line = line.strip('\n')
-                    ##data = "{0}\t{1}{\t}{2}\n".format(line.split('\t')[8],line.split('\t')[9],line.split('\t')[16])
                     try:
+                        #AUTH_ACCOUNT、CAPTURE_TIME、STRSRC_IP、_HOST、_RELATIVEURL、PAYLOAD
                         data = line.split('\t')[0] + '\t' + line.split('\t')[2] + '\t'+ line.split('\t')[3] + '\t'+ line.split('\t')[7] + '\t' + line.split('\t')[8] + '\t' + line.split('\t')[13] + '\n'
                     except:
                         data = ""
@@ -188,12 +124,11 @@ class BatchQuery:
         f.close()
 
         qw_result = io.open(os.path.join(self.data_path,result_file), mode='r', encoding='utf-8').readlines()
-        data1 = io.open(os.path.join(self.data_path,'request_post.txt'), mode='wb+')
+        data1 = io.open(os.path.join(self.data_path, 'request_post.txt'), mode='wb+')
         data1.write('AUTH_ACCOUNT' + '\t' + 'CAPTURE_TIME' + '\t' + 'STRSRC_IP' + '\t' + 'url' + '\t' + 'password' + '\n')
         for line in qw_result:
             l = line.strip().split('\t')
-            # print(l)
-            if (len(l) < 6):
+            if (len(l) < 6):#写的很隐晦，大胆猜测如果每行数据有缺失值，直接跳过
                 continue
             url = l[3] + l[4]
             if 'http://' not in url:
@@ -205,7 +140,7 @@ class BatchQuery:
         data1.close()
 
     def run_query(self):
-        QUREY_TYPE = 'airport_'
+        QUREY_TYPE = 'hackDD'
         out_file = 'ws_out.txt'
         with open(os.path.join(self.data_path, out_file), 'a+') as f:
             f.write('\t'.join(ALL_ITEMS + ['PAYLOAD', 'METHOD', 'KEY_WORDS']) + '\n')
@@ -223,9 +158,7 @@ class BatchQuery:
                                 f.write('\t'.join([data.get(item, '') for item in self.all_items]) +'\t'+ pload+'\t'+method+'\t'+KEY_WORDS+ '\n')
                             except:
                                 pass
-
-
-##日志文件打印
+#日志文件打印
 def init_logger(logname,filename,logger_level = logging.INFO):
     logger = logging.getLogger(logname)
     logger.setLevel(logger_level)
@@ -257,7 +190,7 @@ def zip_result(DATA_PATH,ZIP_PATH):
 
 def main():
     LOGGER.info('START BatchQuery QUERY BATCH....')
-    ap = BatchQuery(DATA_PATH,startTime,endTime,ALL_ITEMS)
+    ap = BatchQuery(DATA_PATH, startTime, endTime, ALL_ITEMS)
     ap.run_query()
     ap.handle_res()
 
@@ -270,7 +203,6 @@ def main():
 
 
 if __name__ == '__main__':
-    ##Program description
     usage = 'python bathquery_db_password.py --start [start_time] --end [end_time] --areacode [areacode]'
     dataformat = '<time>: yyyyMMddhhmmss eg:20180901000000'
     areaformat = '<areacode> xxxxxx eg:530000'
@@ -278,22 +210,21 @@ if __name__ == '__main__':
     parser.add_option('--start',dest = 'startTime',help = dataformat)
     parser.add_option('--end',dest = 'endTime',help = dataformat)
     parser.add_option('--areacode',dest = 'areacode',help = areaformat)
-    ##get input Time  parameter
+
     option,args = parser.parse_args()
     startTime   = option.startTime
     endTime     = option.endTime
     areacode    = option.areacode
-    ##set default Time[ one year]
+
     NowTime = datetime.datetime.now()
     OneYear = datetime.timedelta(days = 15)
     defaultStart = (NowTime - OneYear).strftime("%Y%m%d%H%M%S")
     defaultEnd   = NowTime.strftime("%Y%m%d%H%M%S")
 
-    #ALL_ITEMS= ['AUTH_ACCOUNT', 'AUTH_TYPE', 'CAPTURE_TIME', 'STRSRC_IP', 'SRC_PORT', 'STRDST_IP', 'DST_PORT','_HOST', '_RELATIVEURL','_REFERER', '_MAINFILE', '_QUERY_CONTENT']
-    ALL_ITEMS= ['AUTH_ACCOUNT', 'AUTH_TYPE', 'CAPTURE_TIME', 'STRSRC_IP', 'SRC_PORT', 'STRDST_IP', 'DST_PORT','_HOST', '_RELATIVEURL','_REFERER','_COOKIE','_USERAGENT','_MAINFILE']
+    ALL_ITEMS = ['AUTH_ACCOUNT', 'AUTH_TYPE', 'CAPTURE_TIME', 'STRSRC_IP', 'SRC_PORT', 'STRDST_IP', 'DST_PORT','_HOST', '_RELATIVEURL','_REFERER','_COOKIE','_USERAGENT','_MAINFILE']
     DATA_PATH = './_queryResult_hackDD_' + defaultEnd
-    kwl = [
-    '_RELATIVEURL:aspx System.Convert System.Text FromBase64String Response.Write NOT UmVzcG9uc2UuV3JpdGUoImhhb3JlbmdlLmNvbVFRMzE3Mjc1NzM4Iik7',
+
+    kwl = ['_RELATIVEURL:aspx System.Convert System.Text FromBase64String Response.Write NOT UmVzcG9uc2UuV3JpdGUoImhhb3JlbmdlLmNvbVFRMzE3Mjc1NzM4Iik7',
     '_RELATIVEURL:aspx System.Convert System.Text FromBase64String Request.Item',
     '_RELATIVEURL:aspx System.Convert System.Text Request.Item HexAsciiConvert',
     '.php @ini_set display_errors @set_time_limit @is_writable',
@@ -312,8 +243,11 @@ if __name__ == '__main__':
     '_RELATIVEURL:php eval _SERVER',
     '_RELATIVEURL:php _TEXT:eval _TEXT:_SERVER NOT DOCUMENT_ROOT',
     '_RELATIVEURL:asp Execute Server.ScriptTimeout Resume byVal IsNumeric Response.Write']
+
+    #kwl =['eval System.Text.Encoding.GetEncoding', 'edoced_46esab z0=', 'system _POST', 'array_map @ev', 'eval _SERVER', 'Function  To IsNumeric Else Response.Write Response.End z1=', 'BaSE64 %40eval%2F .27']
+
     init_path(DATA_PATH)
-    LOGGER = init_logger('queryclient_logger',os.path.join(DATA_PATH,'running.log'))
+    LOGGER = init_logger('queryclient_logger', os.path.join(DATA_PATH, 'running.log'))
     if startTime is None and endTime is None:
         startTime = defaultStart
         endTime   = defaultEnd
