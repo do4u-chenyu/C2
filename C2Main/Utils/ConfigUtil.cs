44f1e5aca243a288ef4fb615fde4dada85a87b67
hzhx7707 <1416305262@qq.com>
2022-06-15 11:39:52 +0800

    实验楼顺序调整，机场模型全文提取脚本优化

--- C2Main/Utils/ConfigUtil.cs
+++ C2Main/Utils/ConfigUtil.cs
@@ -9,7 +9,7 @@ namespace C2.Utils
 {
     class ConfigUtil
     {
-        public static string DefaultIAOLab = "BigAPK, APK, Visualization, Wifi, InformationSearch, Tude, PostAndGet, PwdGenerator, GoldEyes";
+        public static string DefaultIAOLab = "BigAPK, APK, Visualization, Wifi, InformationSearch, Tude, PostAndGet, GoldEyes, PwdGenerator";
 
         public static string TryGetAppSettingsByKey(string key, string defaultValue = "")
         {
