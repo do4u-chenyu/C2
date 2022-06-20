8cdf78fc4da7db7b170040006bbcf0ceb434d8e0
An Ri <weilei.gakki@gmail.com>
2022-06-20 10:26:15 +0800

    银行卡查询修正错误提示信息

--- C2Main/Business/IAOLab/BankTool/BankTool.cs
+++ C2Main/Business/IAOLab/BankTool/BankTool.cs
@@ -37,23 +37,18 @@ namespace C2.IAOLab.BankTool
         public string GetBankTool(string bankCardNum)
         {
             Thread.Sleep(500);
-            //string strURL = "http://www.teldata2018.com/cha/kapost.php?ka="+ bankCard.Replace(" ", string.Empty);
-            //string strURL = "http://www.guabu.com/bank/?cardid=" + bankCard.Replace(OpUtil.StringBlank, string.Empty);//新接口，不好用，查得慢，输入中文会返回锟斤拷
             string url = "http://113.31.114.239:53373/api/spider/bank_info";
             Dictionary<string, string> pairs = new Dictionary<string, string> { { "bankcard", bankCardNum } };
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             request.Timeout = 200000;
             string content = JsonConvert.SerializeObject(pairs);
             byte[] data = Encoding.UTF8.GetBytes(content);
-
             request.Method = "POST";
             request.ContentType = "application/json";
             //request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36";
             //request.Headers.Set("cookie", "td_cookie=1206126369; t=ed9584c3ee86740383a6dd94af963b16; r=461; UM_distinctid=17e2f0d4f7950c-0b8d03a6e92b87-3b39580e-1fa400-17e2f0d4f7a530; ASPSESSIONIDSSRDRBDB=CAJABDJAFEBEAAAHIOFCLBBC; CNZZDATA1279885053=887337253-1641463516-null%7C1643249436; security_session_verify=d256ef371a6fb7ffda35fec7c01e5c64");
-
-            
-            string nameAndType = string.Empty;
-            string city = string.Empty;
+            string nameAndType;
+            string city;
             try
             {
                 using (var stream = request.GetRequestStream())
@@ -69,9 +64,9 @@ namespace C2.IAOLab.BankTool
                 nameAndType = gList["type"].ToString();
                 city = gList["city"].ToString().Replace("-", string.Empty).Replace(" ", string.Empty);
             }
-            catch
+            catch(Exception ex)
             {
-                return "网络连接中断";
+                return ex.Message;
             }
             /*
              * 458123038888888
@@ -80,8 +75,6 @@ namespace C2.IAOLab.BankTool
              * 95559
              * <a href="http://www.bankcomm.com" target="_blank">http://www.bankcomm.com</a>
              */
-
-            
             string cardName = string.Empty;
             string cardType = string.Empty;
             
@@ -107,68 +100,7 @@ namespace C2.IAOLab.BankTool
                 cardInfo[2] = cardInfo[2] == string.Empty ? "未知" : cardInfo[2];
                 return string.Join("\t", cardInfo);
             }
-
             return "查询失败";
-
-            /*
-            postContent = string.Join("", postContent.Split('\r', '\n', '\t'));
-            postContent = postContent.Replace("<br />", "\t");
-            String[] postContentArry = postContent.Split('\t', '?');
-
-            if (postContentArry.Length >= 9)
-                return String.Format("{0}\t{1}\t{2}",
-                    postContentArry[1].Replace("银行名称：", ""),  //  卡号
-                    postContentArry[3].Replace("银行卡种：", ""),  //  地址
-                    postContentArry[5].Replace("银行归属地：", "")); //  blabla
-            if (postContentArry.Length == 2)
-                return postContentArry[1];
-            return "查询失败";
-            */
         }
-
-        /*
-        public string GetChinese(string str)
-        {
-            //声明存储结果的字符串
-            string chineseString = string.Empty;
-            //将传入参数中的中文字符添加到结果字符串中
-            for (int i = 0; i < str.Length; i++)
-            {
-                if (str[i] >= 0x4E00 && str[i] <= 0x9FA5) //汉字
-                {
-                    chineseString += str[i];
-                }
-            }
-            //返回保留中文的处理结果
-            return chineseString;
-        }  
-        public List<string> subString(string data, string start, string end)
-        {
-
-            List<string> info = new List<string>();
-            try
-            {
-                int i1 = data.IndexOf(start);
-                int i2 = data.IndexOf(end, i1);
-                string tmp = data.Substring(i1 + start.Length, i2 - i1 - start.Length);
-                info.Add(tmp);
-                while (i1 != -1 && i1 < data.Length - 1)
-                {
-                    i1 = data.IndexOf(start, i1 + 1);
-                    if (i1 == -1)
-                        break;
-                    i2 = data.IndexOf(end, i1 + 1);
-                    tmp = data.Substring(i1 + start.Length, i2 - i1 - start.Length);
-                    info.Add(tmp);
-                }
-                return info;
-            }
-            catch (Exception)
-            {
-                info.Add("查询失败");
-                return info;
-            }
-        }
-        */
     }
 }
